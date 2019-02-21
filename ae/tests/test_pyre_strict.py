8d9712ad82209c2e053c00a3831610a37edba732
Till Varoquaux <till@fb.com>
2019-02-20 23:04:39 -0800

    move runtime into async_state_machine
    Summary: It doesn't make much sense to have `pipeline` sit in a library by itself. Because `pipeline` is built on top of runtime codegen it would pretty hard to typecheck with `pyre-strict` so I added the option to opt out for just one file instead of creating a new `automl_library`.
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D14066508
    
    fbshipit-source-id: aea28dc67f88dae1cd6633cbe3e89abbcf5abe02

--- ae/tests/test_pyre_strict.py
+++ ae/tests/test_pyre_strict.py
@@ -9,7 +9,7 @@ from ae.lazarus.ae.utils.common import testutils
 def check_pyre_strict(t: unittest.TestCase, m: ModuleInfo) -> None:
     with open(m.file) as fd:
         for line in fd:
-            if line == "# pyre-strict\n":
+            if line == "# pyre-strict\n" or line == "# no-strict-types\n":
                 return
     raise Exception(f"{m.path}'s header should contain '# pyre-strict'")
 
