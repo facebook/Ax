9465efe460e42e950a573cf9a50d76fa067b821b
Lili Dworkin <lilidworkin@fb.com>
2019-03-12 13:28:24 -0700

    Allow saving JSON to a different directory
    Summary: Rename variable to make it clear that you can save your json experiment to any directory. Also use "w+" to create file if it doesn't exist (won't create the directory(ies) though)
    
    Reviewed By: amurthy1
    
    Differential Revision: D14418549
    
    fbshipit-source-id: 5221f54e645609f5198918193a8d09c514156b7c

--- ae/storage/json_store/load.py
+++ ae/storage/json_store/load.py
@@ -5,12 +5,12 @@ from ae.lazarus.ae.core.experiment import Experiment
 from ae.lazarus.ae.storage.json_store.decoder import object_from_json
 
 
-def load_experiment(filename: str) -> Experiment:
+def load_experiment(filepath: str) -> Experiment:
     """Load experiment from file.
 
     1) Read file.
     2) Convert dictionary to AE experiment instance.
     """
-    with open(filename, "r") as file:
+    with open(filepath, "r") as file:
         json_experiment = json.loads(file.read())
         return object_from_json(json_experiment)
--- ae/storage/json_store/save.py
+++ ae/storage/json_store/save.py
@@ -6,15 +6,15 @@ from ae.lazarus.ae.core.experiment import Experiment
 from ae.lazarus.ae.storage.json_store.encoder import object_to_json
 
 
-def save_experiment(experiment: Experiment, filename: str) -> None:
+def save_experiment(experiment: Experiment, filepath: str) -> None:
     """Save experiment to file.
 
     1) Convert AE experiment to JSON-serializable dictionary.
     2) Write to file.
     """
-    if not filename.endswith(".json"):
-        raise ValueError("Filename must end in .json")
+    if not filepath.endswith(".json"):
+        raise ValueError("Filepath must end in .json")
 
     json_experiment = object_to_json(experiment)
-    with open(filename, "w") as file:
+    with open(filepath, "w+") as file:
         file.write(json.dumps(json_experiment))
