daf271b5901da7152357e5db3d8d7359e76d304e
Lena Kashtelyan <drfreund@fb.com>
2019-03-28 14:57:59 -0700

    Add branin as just a function to branin metric
    Summary: There are multiple places where it's useful to just have branin on it's own (particularly in testing, and I didn't want to replicate it into `fake.py`). Hopefully this is okay to do!
    
    Reviewed By: ldworkin
    
    Differential Revision: D14668266
    
    fbshipit-source-id: 717f73e66c1ac411ebebf2a5ead3c2d34f9296a4

--- ax/metrics/branin.py
+++ ax/metrics/branin.py
@@ -4,12 +4,17 @@ import numpy as np
 from ax.metrics.noisy_function import NoisyFunctionMetric
 
 
+def branin(x1: float, x2: float) -> float:
+    """Branin synthetic function."""
+    y = (x2 - 5.1 / (4 * np.pi ** 2) * x1 ** 2 + 5 * x1 / np.pi - 6) ** 2
+    y += 10 * (1 - 1 / (8 * np.pi)) * np.cos(x1) + 10
+    return y
+
+
 class BraninMetric(NoisyFunctionMetric):
     def f(self, x: np.ndarray) -> float:
         x1, x2 = x
-        y = (x2 - 5.1 / (4 * np.pi ** 2) * x1 ** 2 + 5 * x1 / np.pi - 6) ** 2
-        y += 10 * (1 - 1 / (8 * np.pi)) * np.cos(x1) + 10
-        return y
+        return branin(x1=x1, x2=x2)
 
 
 class NegativeBraninMetric(BraninMetric):
