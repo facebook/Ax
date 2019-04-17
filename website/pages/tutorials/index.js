02545946e7bcd5e4b732acee29d8f6b81d81b7a2
Brian Johnson <brianjo@fb.com>
2019-04-17 16:08:07 -0700

    Added some content to the tutorials landing page. (Will replace with control in the future.)

--- website/pages/tutorials/index.js
+++ website/pages/tutorials/index.js
@@ -25,6 +25,23 @@ class TutorialHome extends React.Component {
                   <header className="postHeader">
                     <h1 className="postHeaderTitle">Welcome to Ax Tutorials</h1>
                   </header>
+	    <body>
+	    <p>Ax is a machine learning system designed to help automate the
+	    process of selecting and tuning ML configurations. 
+            </p><p>
+	    The tutorials here will help you understand how to use
+	    Ax to tune and optimize your neural networks.
+	    </p>
+	    <ul>
+	    <li><a href="tune_cnn.html">Tune a CNN on MNIST</a></li>
+            <li><a href="multi_task.html">Mixed online/offline Bayesian Optimization in Ax</a></li>
+            <li><a href="benchmarking_suite_example.html">The Ax Benchmarking Suite</a></li>
+	    <li><a href="factorial.html">Factorial design with Empirical Bayes and Thompson Sampling</a></li>
+	    <li><a href="managed_loop.html">Ax Managed Loop Example</a></li>
+	    <li><a href="service_api.html">Ax Service-like API</a></li>
+	    <li><a href="tune_cnn.html">Tune a CNN on MNIST</a></li>
+	    </ul>
+	    </body>
                 </div>
         </Container>
       </div>
