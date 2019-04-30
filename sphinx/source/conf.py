f554882a4620c108ced68b6a78d57432aebeb622
Max Balandat <balandat@fb.com>
2019-04-30 16:27:21 -0700

    Add support for parsing sphinx sources to Ax
    Summary: Pull Request resolved: https://github.com/facebook/Ax/pull/48
    
    Reviewed By: sdsingh
    
    Differential Revision: D15155492
    
    Pulled By: Balandat
    
    fbshipit-source-id: 7a6afe8edd060ce98062e927a8bbe95cf9257d73

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "sphinx.ext.todo",
     "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
