66fce5b113491fb33165d1e82cb2c90215d3c647
Shaun Singh <shaundsingh@fb.com>
2019-07-12 12:01:40 -0700

    Add v0 of build scripts
    Summary:
    Add a basic set of scripts for pushing Ax.
    
    This is currently a wee bit manual.
    Steps:
    1. Run `./build_ax.sh`, which opens an interactive docker prompt (`root`)
    2. `cd Ax-master/scripts`, and run `./docker_install.sh LATEST_VERSION`
    3. Once that is finished, ctrl-D to exit docker, and allow the script to finish.
    
    NOTE: Be careful with the final line, `twine upload dist/*`
    
    This cannot be undone, so please comment during testing!
    
    Reviewed By: ldworkin
    
    Differential Revision: D16223693
    
    fbshipit-source-id: 0031c331ee5d1ca97060af62da06962f8f1de419

---
+++ scripts/build_ax.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+# ManyLinux build
+# Mount top-level Ax directory as Ax-master.
+docker container run --mount type=bind,source="$(pwd)/../",target=/Ax-master -it quay.io/pypa/manylinux2010_x86_64
+
+# MANUAL STEP FOR NOW
+# Now, in Docker container, cd Ax-master and MANUALLY RUN ./docker_install.sh
+
+# LOCAL BUILD
+# Requires Python 3.6+3.7 installed locally, and on path
+cd .. 
+pip3.6 install cython numpy
+python3.6 setup.py bdist_wheel
+
+# Build Linux Python3.7
+pip3.7 install cython numpy
+python3.7 setup.py bdist_wheel
+
+# Final PyPI Upload
+twine upload dist/*
+
