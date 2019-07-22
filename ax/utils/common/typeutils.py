75e52d8b4ac91e36dd4bdbba053252fac8d431ee
Lili Dworkin <lilidworkin@fb.com>
2019-07-22 11:21:34 -0700

    Fix flake/travis
    Summary: self explanatory
    
    Reviewed By: sdsingh
    
    Differential Revision: D16419169
    
    fbshipit-source-id: b84eab7e1792cd8e6f37685652b395693ad002bf

--- ax/utils/common/typeutils.py
+++ ax/utils/common/typeutils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
-from typing import Any, List, Optional, Tuple, Type, TypeVar, Union
+from typing import Any, List, Optional, Tuple, Type, TypeVar
 
 import numpy as np
 
