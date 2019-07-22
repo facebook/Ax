63269d70586d8c02d556e2bf38b6ff11c0d4884f
Ben Letham <bletham@fb.com>
2019-07-22 14:19:12 -0700

    Allow passing fixed features to slice plot
    Summary: Important for plotting outputs of models that use a context (like end time) as a feature in the model.
    
    Reviewed By: amurthy1
    
    Differential Revision: D16347106
    
    fbshipit-source-id: 7077dd27484750a11290f1243744696d3c92601f

--- ax/plot/slice.py
+++ ax/plot/slice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
+from copy import deepcopy
 from typing import Any, Dict, Optional
 
 import numpy as np
@@ -24,6 +25,7 @@ def plot_slice(
     relative: bool = False,
     density: int = 50,
     slice_values: Optional[Dict[str, Any]] = None,
+    fixed_features: Optional[ObservationFeatures] = None,
 ) -> AxPlotConfig:
     """Plot predictions for a 1-d slice of the parameter space.
 
@@ -38,7 +40,11 @@ def plot_slice(
         slice_values: A dictionary {name: val} for the fixed values of the
             other parameters. If not provided, then the status quo values will
             be used if there is a status quo, otherwise the mean of numeric
-            parameters or the mode of choice parameters.
+            parameters or the mode of choice parameters. Ignored if
+            fixed_features is specified.
+        fixed_features: An ObservationFeatures object containing the values of
+            features (including non-parameter features like context) to be set
+            in the slice.
     """
     if generator_runs_dict is None:
         generator_runs_dict = {}
@@ -50,14 +56,18 @@ def plot_slice(
         model=model, generator_runs_dict=generator_runs_dict, metric_names={metric_name}
     )
 
+    if fixed_features is not None:
+        slice_values = fixed_features.parameters
+    else:
+        fixed_features = ObservationFeatures(parameters={})
     fixed_values = get_fixed_values(model, slice_values)
 
     prediction_features = []
     for x in grid:
-        parameters = fixed_values.copy()
-        parameters[param_name] = x
-        # Here we assume context is None
-        prediction_features.append(ObservationFeatures(parameters=parameters))
+        predf = deepcopy(fixed_features)
+        predf.parameters = fixed_values.copy()
+        predf.parameters[param_name] = x
+        prediction_features.append(predf)
 
     f, cov = model.predict(prediction_features)
 
