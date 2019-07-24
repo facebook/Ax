5e736f8318b8ae39d5224ebe100b74d42cd794f5
Sam Daulton <samdaulton@instagram.com>
2019-07-23 18:47:31 -0700

    Add dataset-agnostic helper function for data processing
    Summary: Split out logic that is not MNIST specific. This makes this logic reusable for other torchvision datasets
    
    Reviewed By: Balandat
    
    Differential Revision: D16435978
    
    fbshipit-source-id: c7f2a78e8104002613378596d05851b622ffa79d

--- ax/utils/tutorials/cnn_utils.py
+++ ax/utils/tutorials/cnn_utils.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
-from typing import Dict, Tuple
+from itertools import accumulate
+from typing import Dict, List, Tuple
 
 import torch
 import torch.nn as nn
@@ -9,7 +10,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset, Subset
 
 
 class CNN(nn.Module):
@@ -38,6 +39,7 @@ def load_mnist(
     data_path: str = "./data",
     batch_size: int = 128,
     num_workers: int = 0,
+    deterministic_partitions: bool = False,
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
     """
     Load MNIST dataset (download if necessary) and split data into training,
@@ -51,6 +53,8 @@ def load_mnist(
             and `MNIST/processed/test.pt` exist.
         batch_size: how many samples per batch to load
         num_workers: number of workers (subprocesses) for loading data
+        deterministic_partitions: whether to partition data in a deterministic
+            fashion
 
     Returns:
         DataLoader: training data
@@ -69,29 +73,71 @@ def load_mnist(
     )
     # Load test set
     # pyre-fixme[16]: Module `datasets` has no attribute `MNIST`.
-    test_set_all = torchvision.datasets.MNIST(
+    test_set = torchvision.datasets.MNIST(
         root=data_path, train=False, download=True, transform=transform
     )
+    return get_partition_data_loaders(
+        train_valid_set=train_valid_set,
+        test_set=test_set,
+        downsample_pct=downsample_pct,
+        train_pct=train_pct,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        deterministic_partitions=deterministic_partitions,
+    )
 
+
+def get_partition_data_loaders(
+    train_valid_set: Dataset,
+    test_set: Dataset,
+    downsample_pct: float = 0.5,
+    train_pct: float = 0.8,
+    batch_size: int = 128,
+    num_workers: int = 0,
+    deterministic_partitions: bool = False,
+) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """
+    Helper function for partitioning training data into training and validation sets,
+        downsampling data, and initializing DataLoaders for each partition.
+
+    Args:
+        train_valid_set: torch.dataset
+        downsample_pct: the proportion of the dataset to use for training,
+            validation, and test
+        train_pct: the proportion of the downsampled data to use for training
+        batch_size: how many samples per batch to load
+        num_workers: number of workers (subprocesses) for loading data
+        deterministic_partitions: whether to partition data in a deterministic
+            fashion
+
+    Returns:
+        DataLoader: training data
+        DataLoader: validation data
+        DataLoader: test data
+    """
     # Partition into training/validation
+    # pyre-ignore [6]
     downsampled_num_examples = int(downsample_pct * len(train_valid_set))
     n_train_examples = int(train_pct * downsampled_num_examples)
     n_valid_examples = downsampled_num_examples - n_train_examples
-    train_set, valid_set, _ = torch.utils.data.random_split(
-        train_valid_set,
+    train_set, valid_set, _ = split_dataset(
+        dataset=train_valid_set,
         lengths=[
             n_train_examples,
             n_valid_examples,
-            len(train_valid_set) - downsampled_num_examples,
+            len(train_valid_set) - downsampled_num_examples,  # pyre-ignore [6]
         ],
+        deterministic_partitions=deterministic_partitions,
     )
-    downsampled_num_test_examples = int(downsample_pct * len(test_set_all))
-    test_set, _ = torch.utils.data.random_split(
-        test_set_all,
+    # pyre-ignore [6]
+    downsampled_num_test_examples = int(downsample_pct * len(test_set))
+    test_set, _ = split_dataset(
+        test_set,
         lengths=[
             downsampled_num_test_examples,
-            len(test_set_all) - downsampled_num_test_examples,
+            len(test_set) - downsampled_num_test_examples,  # pyre-ignore [6]
         ],
+        deterministic_partitions=deterministic_partitions,
     )
     train_loader = DataLoader(
         train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers
@@ -103,6 +149,31 @@ def load_mnist(
         test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
     return train_loader, valid_loader, test_loader
+
+
+def split_dataset(
+    dataset: Dataset, lengths: List[int], deterministic_partitions: bool = False
+) -> List[Dataset]:
+    """
+    Split a dataset either randomly or deterministically.
+
+    Args:
+        dataset: the dataset to split
+        lengths: the lengths of each partition
+        deterministic_partitions: deterministic_partitions: whether to partition
+            data in a deterministic fashion
+
+    Returns:
+        List[Dataset]: split datasets
+    """
+    if deterministic_partitions:
+        indices = list(range(sum(lengths)))
+    else:
+        indices = torch.randperm(sum(lengths)).tolist()
+    return [
+        Subset(dataset, indices[offset - length : offset])
+        for offset, length in zip(accumulate(lengths), lengths)
+    ]
 
 
 def train(
