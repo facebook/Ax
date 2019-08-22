6fe8f6dfb260d3ad14fed5549533c57f5851f971
Steve Mandala <scubasteve@fb.com>
2019-08-22 11:36:29 -0700

    Clean up the wheels build script
    Summary:
    In order to make our open source requirements happy and make the script
    slightly more usable, this diff adds a header and manages the working directory
    when running the script
    
    Reviewed By: ldworkin
    
    Differential Revision: D16945976
    
    fbshipit-source-id: af743ed758bd3d7f868ad5ac7d0f48fec9aca0a1

--- scripts/wheels_build.ps1
+++ scripts/wheels_build.ps1
@@ -1,20 +1,25 @@
-# Used for building the Python 3.6.3 wheel dist for Ax
-# Note: Requires VS C++ Build Tools
-
-# Usage: ./wheel_build.ps1 -pypath [PATH_TO_PYTHON] [-upload]
-# Note: This script should be run from the repo's parent dir
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# Powershell script, used for building the Python wheel distribution for Ax
+#
+# Notes:
+# * Requires VS C++ Build Tools
+# * This script should be run from the repo's parent dir
+#
+# Usage: ./scripts/wheel_build.ps1 -pypath [PATH_TO_PYTHON] [-upload]
 
 # Python path to use for build, flag to upload file
 param(
-  [string]$pypath = $("C:\Users\scubasteve\AppData\Local\Programs\Python\Python36\python.exe"),
+  [string]$pypath = $("python3"),
   [switch]$upload = $false
 )
 
-# Jump into the Ax repo folder
-pushd Ax
 Set-Alias -Name mypy -Value $pypath
 Write-Host "Py Version:"
 mypy --version
+
+# Jump into the Ax repo folder
+pushd $PSScriptRoot\..
 
 # Install or upgrade all the dependecies
 mypy -m pip install botorch jinja2 pandas scipy simplejson sklearn plotly cython numpy searchtools twine wheel
@@ -30,6 +35,6 @@ If ($upload) {
   echo "Uploading"
   twine upload dist/*
 }
+
 # Done!
 popd
-
