774f71fdda2d1261c35d8e58e95aea76ed238ca0
Shaun Singh <shaundsingh@fb.com>
2019-09-11 11:57:00 -0700

    Fix OOD transform
    Summary:
    There was a lot of discussion on how to handle OOD SQ in the Ax modeling stack, see T40625569.
    However, the definition of OOD SQ added in the OutOfDesignTransform is very incomplete, and needs to actually check search space membership.
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D17262549
    
    fbshipit-source-id: dd968ed2614c23e6bab2c5de4f69a0ebbb8372f1

--- ax/modelbridge/tests/test_out_of_design_transform.py
+++ ax/modelbridge/tests/test_out_of_design_transform.py
@@ -28,25 +28,40 @@ class OutOfDesignTransformTest(TestCase):
                 FixedParameter("c", parameter_type=ParameterType.STRING, value="a"),
             ]
         )
-        self.t = OutOfDesign(
-            search_space=self.search_space,
-            observation_features=None,
-            observation_data=None,
-        )
-
-    def testTransformObservationFeatures(self):
-        # Don't modify points without None.
-        observation_features = [
+        self.in_design_features = [
             ObservationFeatures(parameters={"a": 2.2, "b": "b", "c": "a"})
         ]
-        obs_ft = deepcopy(observation_features)
-        obs_ft = self.t.transform_observation_features(obs_ft)
-        self.assertEqual(obs_ft, observation_features)
-
-        # Strip params from points with any Nones.
-        observation_features = [
+        self.out_of_design_features_1 = [
             ObservationFeatures(parameters={"a": 2.2, "b": "b", "c": None})
         ]
-        obs_ft = deepcopy(observation_features)
-        obs_ft = self.t.transform_observation_features(obs_ft)
+        self.out_of_design_features_2 = [ObservationFeatures(parameters={})]
+
+    def testTransformOutOfDesign(self):
+        t_out_1 = OutOfDesign(
+            search_space=self.search_space,
+            observation_features=self.out_of_design_features_1,
+            observation_data=None,
+        )
+        obs_ft = deepcopy(self.out_of_design_features_1)
+        obs_ft = t_out_1.transform_observation_features(self.out_of_design_features_1)
         self.assertEqual(obs_ft, [ObservationFeatures(parameters={})])
+
+        t_out_2 = OutOfDesign(
+            search_space=self.search_space,
+            observation_features=self.out_of_design_features_2,
+            observation_data=None,
+        )
+        obs_ft = deepcopy(self.out_of_design_features_2)
+        obs_ft = t_out_2.transform_observation_features(self.out_of_design_features_2)
+        self.assertEqual(obs_ft, [ObservationFeatures(parameters={})])
+
+    def testTransformInDesign(self):
+        # Don't modify in design points.
+        t_in = OutOfDesign(
+            search_space=self.search_space,
+            observation_features=self.in_design_features,
+            observation_data=None,
+        )
+        obs_ft = deepcopy(self.in_design_features)
+        obs_ft = t_in.transform_observation_features(obs_ft)
+        self.assertEqual(obs_ft, self.in_design_features)
