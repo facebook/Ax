ea6bb51a35fa43c49acff2320f499e4a52ba9719
Jake Podell <podell@fb.com>
2019-10-01 07:34:40 -0700

    Push from the original folder
    Summary: When we publish a new version, we want to make sure that we aren't wiping the github pages history. We attempt to fix this problem by copying the contents of our new directory into the clone of gh-pages, and pushing from there.
    
    Reviewed By: kkashin
    
    Differential Revision: D17602805
    
    fbshipit-source-id: 5fd647fb9e6c8e0cdab69758ca4ce2d9c8859c11

--- scripts/publish_site.sh
+++ scripts/publish_site.sh
@@ -213,12 +213,13 @@ else
   cd "${WORK_DIR}" || exit
   python3 Ax-master/scripts/update_versions_html.py -p "${WORK_DIR}"
 
-  # Init as Git repo and push to gh-pages
-  cd new-site || exit
-  git init
+  # move contents of newsite to Ax-gh-pages, preserving commit history
+  rm -rfv ./Ax-gh-pages/*
+  rsync -avh ./new-site/ ./Ax-gh-pages/
+  cd Ax-gh-pages
   git add --all
   git commit -m "Publish version ${VERSION} of site"
-  git push --force "https://github.com/facebook/Ax" master:gh-pages
+  git push
 
 fi
 
