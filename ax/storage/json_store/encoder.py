6758d3ff17376a9b0770b5d599b253090e7b9992
Lena Kashtelyan <drfreund@fb.com>
2019-12-06 10:18:57 -0800

    Rename kwarg in `object_to_json` from reserved keyword
    Summary: As per Max's suggestion, renaming kwarg to `object_to_json` from `object` to `obj` to avoid shadowing of reserved keywords
    
    Reviewed By: Balandat
    
    Differential Revision: D18835836
    
    fbshipit-source-id: c956eab960894c7211b93b9503d47ec38cf51715

--- ax/storage/json_store/encoder.py
+++ ax/storage/json_store/encoder.py
@@ -16,7 +16,7 @@ from ax.utils.common.serialization import _is_named_tuple
 from ax.utils.common.typeutils import numpy_type_to_python_type, torch_type_to_str
 
 
-def object_to_json(object: Any) -> Any:
+def object_to_json(obj: Any) -> Any:
     """Convert an Ax object to a JSON-serializable dictionary.
 
     The root node passed to this function should always be an instance of a
@@ -31,55 +31,55 @@ def object_to_json(object: Any) -> Any:
     We then pass each item of the dictionary back into this function to
     recursively convert the entire object.
     """
-    object = numpy_type_to_python_type(object)
-    _type = type(object)
+    obj = numpy_type_to_python_type(obj)
+    _type = type(obj)
 
     # Python built-in types + `typing` module types
     if _type in (str, int, float, bool, type(None)):
-        return object
+        return obj
     elif _type is list:
-        return [object_to_json(x) for x in object]
+        return [object_to_json(x) for x in obj]
     elif _type is tuple:
-        return tuple(object_to_json(x) for x in object)
+        return tuple(object_to_json(x) for x in obj)
     elif _type is dict:
-        return {k: object_to_json(v) for k, v in object.items()}
-    elif _is_named_tuple(object):
+        return {k: object_to_json(v) for k, v in obj.items()}
+    elif _is_named_tuple(obj):
         return {
             "__type": _type.__name__,
-            **{k: object_to_json(v) for k, v in object._asdict().items()},
+            **{k: object_to_json(v) for k, v in obj._asdict().items()},
         }
 
     # Types from libraries, commonly used in Ax (e.g., numpy, pandas, torch)
     elif _type is OrderedDict:
         return {
             "__type": _type.__name__,
-            "value": [(k, object_to_json(v)) for k, v in object.items()],
+            "value": [(k, object_to_json(v)) for k, v in obj.items()],
         }
     elif _type is datetime.datetime:
         return {
             "__type": _type.__name__,
-            "value": datetime.datetime.strftime(object, "%Y-%m-%d %H:%M:%S.%f"),
+            "value": datetime.datetime.strftime(obj, "%Y-%m-%d %H:%M:%S.%f"),
         }
     elif _type is pd.DataFrame:
-        return {"__type": _type.__name__, "value": object.to_json()}
+        return {"__type": _type.__name__, "value": obj.to_json()}
     elif issubclass(_type, enum.Enum):
-        return {"__type": _type.__name__, "name": object.name}
+        return {"__type": _type.__name__, "name": obj.name}
     elif _type is np.ndarray or issubclass(_type, np.ndarray):
-        return {"__type": _type.__name__, "value": object.tolist()}
+        return {"__type": _type.__name__, "value": obj.tolist()}
     elif _type.__module__ == "torch":
         # Torch does not support saving to string, so save to buffer first
-        return {"__type": f"torch_{_type.__name__}", "value": torch_type_to_str(object)}
+        return {"__type": f"torch_{_type.__name__}", "value": torch_type_to_str(obj)}
 
     # Ax types
-    elif isclass(object) and issubclass(object, Transform):
-        # There is no other way to check is object is of type Type[Transform].
+    elif isclass(obj) and issubclass(obj, Transform):
+        # There is no other way to check is obj is of type Type[Transform].
         _type = Type[Transform]
 
     if _type not in ENCODER_REGISTRY:
         err = (
-            f"Object {object} passed to `object_to_json` (of type {_type}) is "
+            f"Object {obj} passed to `object_to_json` (of type {_type}) is "
             f"not registered with a corresponding encoder in ENCODER_REGISTRY."
         )
         raise JSONEncodeError(err)
-    object_dict = ENCODER_REGISTRY[_type](object)
-    return {k: object_to_json(v) for k, v in object_dict.items()}
+    obj_dict = ENCODER_REGISTRY[_type](obj)
+    return {k: object_to_json(v) for k, v in obj_dict.items()}
