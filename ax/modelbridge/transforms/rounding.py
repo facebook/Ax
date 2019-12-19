1f17b58ac6e9d45b7a0ef1d11d3f3b0852b9a066
Jacob E. Jensen <jej@fb.com>
2019-12-19 13:46:57 -0800

    Change IntToFloat rounding obey Integer constraints
    Summary:
    Attempt to randomly round up to 1000 times when IntToFloat untransforms in order to follow constraints on integer parameter ranges.
    
    Github issue 229
    
    Reviewed By: bletham
    
    Differential Revision: D19151928
    
    fbshipit-source-id: 6e9cfb13d92fb9973abd89c843595bc23636f61c

--- ax/modelbridge/transforms/rounding.py
+++ ax/modelbridge/transforms/rounding.py
@@ -6,8 +6,13 @@
 
 import math
 import random
+from copy import copy
+from typing import Set
 
 import numpy as np
+from ax.core.parameter_constraint import OrderConstraint
+from ax.core.search_space import SearchSpace
+from ax.core.types import TParameterization
 
 
 def randomized_round(x: float) -> int:
@@ -39,3 +44,31 @@ def strict_onehot_round(x: np.ndarray) -> np.ndarray:
     x[argmax] = 1
     x[~argmax] = 0
     return randomized_onehot_round(x)
+
+
+def contains_constrained_integer(
+    search_space: SearchSpace, transform_parameters: Set[str]
+) -> bool:
+    """Check if any integer parameters are present in parameter_constraints.
+
+    Order constraints are ignored since strict rounding preserves ordering.
+    """
+    for constraint in search_space.parameter_constraints:
+        if isinstance(constraint, OrderConstraint):
+            continue
+        constraint_params = set(constraint.constraint_dict.keys())
+        if constraint_params.intersection(transform_parameters):
+            return True
+    return False
+
+
+def randomized_round_parameters(
+    parameters: TParameterization, transform_parameters: Set[str]
+) -> TParameterization:
+    rounded_parameters = copy(parameters)
+    for p_name in transform_parameters:
+        # pyre: param is declared to have type `float` but is used as
+        # pyre-fixme[9]: type `Optional[typing.Union[bool, float, str]]`.
+        param: float = parameters.get(p_name)
+        rounded_parameters[p_name] = randomized_round(param)
+    return rounded_parameters
