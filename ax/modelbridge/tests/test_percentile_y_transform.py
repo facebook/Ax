ef0fde38157ba6740d420a98be20e3dba2ec204c
Jacob E. Jensen <jej@fb.com>
2020-02-03 09:05:36 -0800

    Add Winsorizaiton config option to PercentileYTransform
    Summary: Next step for Gaussian Copula - add winsorization using formula specified in paper (we can specify this in the config and fall back to this formula as a default later if we want to generalize empirical CDF transforms).
    
    Reviewed By: sdsingh
    
    Differential Revision: D19628853
    
    fbshipit-source-id: fc2a9859ed89ad56e9dc9b6e85e1d26b38ccbd99

--- ax/modelbridge/tests/test_percentile_y_transform.py
+++ ax/modelbridge/tests/test_percentile_y_transform.py
@@ -54,6 +54,17 @@ class PercentileYTransformTest(TestCase):
                 deepcopy(self.obsd4),
             ],
         )
+        self.t_with_winsorization = PercentileY(
+            search_space=None,
+            observation_features=None,
+            observation_data=[
+                deepcopy(self.obsd1),
+                deepcopy(self.obsd2),
+                deepcopy(self.obsd3),
+                deepcopy(self.obsd4),
+            ],
+            config={"winsorize": True},
+        )
 
     def testInit(self):
         with self.assertRaises(ValueError):
@@ -62,11 +73,25 @@ class PercentileYTransformTest(TestCase):
     def testTransformObservations(self):
         self.assertListEqual(self.t.percentiles["m1"], [0.0, 1.0, 2.0, 3.0])
         self.assertListEqual(self.t.percentiles["m2"], [0.0, 5.0, 25.0, 125.0])
-        observation_data = self.t.transform_observation_data(
-            [deepcopy(self.obsd_extreme)], []
-        )[0]
-        self.assertListEqual(list(observation_data.means), [0.0, 100.0])
-        observation_data = self.t.transform_observation_data(
+        transformed_obsd_mid = self.t.transform_observation_data(
             [deepcopy(self.obsd_mid)], []
         )[0]
-        self.assertListEqual(list(observation_data.means), [50.0, 75.0])
+        self.assertListEqual(list(transformed_obsd_mid.means), [0.5, 0.75])
+        transformed_obsd_extreme = self.t.transform_observation_data(
+            [deepcopy(self.obsd_extreme)], []
+        )[0]
+        self.assertListEqual(list(transformed_obsd_extreme.means), [0.0, 1.0])
+
+    def testTransformObservationsWithWinsorization(self):
+        self.assertListEqual(self.t.percentiles["m1"], [0.0, 1.0, 2.0, 3.0])
+        self.assertListEqual(self.t.percentiles["m2"], [0.0, 5.0, 25.0, 125.0])
+        transformed_obsd_mid = self.t_with_winsorization.transform_observation_data(
+            [deepcopy(self.obsd_mid)], []
+        )[0]
+        self.assertListEqual(list(transformed_obsd_mid.means), [0.5, 0.75])
+        transformed_obsd_extreme = self.t_with_winsorization.transform_observation_data(
+            [deepcopy(self.obsd_extreme)], []
+        )[0]
+        # Approximate assertion for winsorization
+        results = np.array(list(transformed_obsd_extreme.means))
+        self.assertTrue(all(np.isclose(results, np.array([0.0847075, 0.9152924]))))
