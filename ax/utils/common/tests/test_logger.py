c288781769ee582f4e841c09dbb7322142430290
Lena Kashtelyan <drfreund@fb.com>
2020-03-18 12:44:00 -0400

    Fixing `testLoggerWithFile` in Travis
    This should address https://travis-ci.com/github/facebook/Ax/jobs/299393517

--- ax/utils/common/tests/test_logger.py
+++ ax/utils/common/tests/test_logger.py
@@ -22,8 +22,10 @@ class LoggerTest(TestCase):
         logger.warning.assert_called_once_with(self.warning_string)
 
     def testLoggerWithFile(self):
-        with NamedTemporaryFile() as tf:
-            logger = get_logger(__name__, tf.name)
+        with NamedTemporaryFile(mode="w") as tf:
+            logger = get_logger(name=__name__, filepath=tf.name)
             logger.warning(self.warning_string)
-            self.assertIn(self.warning_string, str(tf.readline()))
+            with open(tf.name) as log_file:
+                line = log_file.readline()
+                self.assertIn(self.warning_string, line)
             tf.close()
