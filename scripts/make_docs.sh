7a11a6bf15270744e03bb0a930e78faba1f9b668
Lena Kashtelyan <drfreund@fb.com>
2020-09-30 11:35:23 -0700

    Exit docs + tutorials build on error
    Summary: I believe that since our website build script separately executes our make docs build, the latter should have its own `set -e`
    
    Reviewed By: stevemandala
    
    Differential Revision: D24020814
    
    fbshipit-source-id: 8826a4dfa9a52543a72c4a6c5edf626d443672ed

--- scripts/make_docs.sh
+++ scripts/make_docs.sh
@@ -4,6 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# Exit if any error occurs
+set -e
+
 usage() {
   echo "Usage: $0 [-b] [-o] [-r] [-t] [-k kernel_name]"
   echo ""
