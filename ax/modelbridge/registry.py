42c95fe6a116abf29c78fc817eacc01f085d6846
Shaun Singh <shaundsingh@fb.com>
2020-10-02 11:29:01 -0700

    Do not override torch device default
    Summary: Defaulting to CPU is unnecessary; if we are on CPU this will be the default for an empty/None value, anyways.
    
    Reviewed By: Balandat
    
    Differential Revision: D24083858
    
    fbshipit-source-id: 9e450a1277931bc457fae3177d612224d3b21a41

--- ax/modelbridge/registry.py
+++ ax/modelbridge/registry.py
@@ -110,10 +110,7 @@ Specified_Task_ST_MTGP_trans: List[Type[Transform]] = (
     Cont_X_trans + [Derelativize, StratifiedStandardizeY, TaskEncode]
 )
 
-STANDARD_TORCH_BRIDGE_KWARGS: Dict[str, Any] = {
-    "torch_dtype": torch.double,
-    "torch_device": torch.device("cpu"),
-}
+STANDARD_TORCH_BRIDGE_KWARGS: Dict[str, Any] = {"torch_dtype": torch.double}
 
 
 class ModelSetup(NamedTuple):
