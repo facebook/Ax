16f89fb589cfbacf744ea62307f6d74aebe05b00
Bernie Beckerman <bbeckerman@fb.com>
2021-03-11 16:31:43 -0800

    Implement plots in report_results
    Summary: Implements plotting in report_results for the param sweep scheduler migration.
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D26804686
    
    fbshipit-source-id: 4aebc7f1bb5fc4d77191e467d26e2e1d9be09af1

---
+++ ax/service/utils/report_utils.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from logging import Logger
+from typing import Dict, List, Optional
+
+import numpy as np
+from ax.core.experiment import Experiment
+from ax.core.search_space import SearchSpace
+from ax.core.trial import BaseTrial, Trial
+from ax.modelbridge import ModelBridge
+from ax.modelbridge.generation_strategy import GenerationStrategy
+from ax.modelbridge.random import RandomModelBridge
+from ax.plot.base import AxPlotConfig
+from ax.plot.contour import interact_contour
+from ax.plot.slice import plot_slice
+from ax.plot.trace import optimization_trace_single_method
+from ax.utils.common.logger import get_logger
+from ax.utils.common.typeutils import checked_cast, not_none
+
+
+logger: Logger = get_logger(__name__)
+
+
+def _get_objective_trace_plot(
+    trials: Dict[int, BaseTrial],
+    metric_name: str,
+    model_transitions: List[int],
+    optimization_direction: Optional[str] = None,
+) -> Optional[AxPlotConfig]:
+    best_objectives = np.array(
+        [[checked_cast(Trial, t).objective_mean for t in trials.values()]]
+    )
+    return optimization_trace_single_method(
+        y=best_objectives,
+        title="Best objective found vs. # of iterations",
+        ylabel=metric_name,
+        model_transitions=model_transitions,
+        optimization_direction=optimization_direction,
+        plot_trial_points=True,
+    )
+
+
+def _get_objective_v_param_plot(
+    search_space: SearchSpace,
+    model: ModelBridge,
+    metric_name: str,
+    trials: Dict[int, BaseTrial],
+) -> Optional[AxPlotConfig]:
+    range_params = list(search_space.range_parameters.keys())
+    if len(range_params) == 1:
+        # individual parameter slice plot
+        output_slice_plot = plot_slice(
+            model=not_none(model),
+            param_name=range_params[0],
+            metric_name=metric_name,
+            generator_runs_dict={
+                str(t.index): not_none(checked_cast(Trial, t).generator_run)
+                for t in trials.values()
+            },
+        )
+        return output_slice_plot
+    if len(range_params) > 1:
+        # contour plot
+        output_contour_plot = interact_contour(
+            model=not_none(model),
+            metric_name=metric_name,
+        )
+        return output_contour_plot
+
+
+def get_standard_plots(
+    experiment: Experiment, generation_strategy: GenerationStrategy
+) -> List[AxPlotConfig]:
+    """Extract standard plots for single-objective optimization.
+
+    TODO: Describe specific logic here on what happens depending on search space
+    and generation strategy attributes.
+    """
+    output_plot_list = []
+    output_plot_list.append(
+        _get_objective_trace_plot(
+            trials=experiment.trials,
+            metric_name=not_none(experiment.optimization_config).objective.metric.name,
+            model_transitions=generation_strategy.model_transitions,
+            optimization_direction=(
+                "minimize"
+                if not_none(experiment.optimization_config).objective.minimize
+                else "maximize"
+            ),
+        )
+    )
+
+    # TODO: Replace this check with a check of whether the model
+    # implements `predict`.
+    if isinstance(generation_strategy.model, RandomModelBridge):
+        return output_plot_list
+
+    output_plot_list.append(
+        _get_objective_v_param_plot(
+            search_space=experiment.search_space,
+            model=not_none(generation_strategy.model),
+            metric_name=not_none(experiment.optimization_config).objective.metric.name,
+            trials=experiment.trials,
+        )
+    )
+
+    return [plot for plot in output_plot_list if plot is not None]
