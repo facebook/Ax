e60234d7c634624fad0bc7557a34a8a617bed760
Lena Kashtelyan <drfreund@fb.com>
2021-03-17 15:28:01 -0700

    Enable mocking of functions in `ax/storage/sqa_store/save.py` and `load.py` (there was aliasing that got in the way)
    Summary:
    The way our init files are currently specified in `ax/storage/sqa_store` creates some shadowing: we have `ax.storage.sqa_store.save` the module (from `save.py` file) and the function ––`save_experiment`, aliased as `save` (or maybe even two of them, as the JSON save is aliased in `sqa_store/__init__.py` and then SQL `save_experiment` is aliased in `storage/__init__.py` –– not sure if aliasing gets chained or not, but this is beside the point). This makes it so that it's impossible do to something like `patch("ax.storage.sqa_store.save.<my_function>"`), because `ax.storage.sqa_store.save` is translated to a function (`save_experiment`) instead of a module and results in an error like:
    ```
    AttributeError: <function save_experiment at 0x7f8a787eddc0> does not have the attribute 'my_function'
    ```
    
    This was done for a good reason in D14997052 (https://github.com/facebook/Ax/commit/0bdbcbc53252e423898e9f088b740d52358eafc1), so putting up an RfC to collectively brainstorm how to fix. I can just not mock things in that file (although it is very helpful for testing and will also happen the same way for `load.py`), but I think we should at least all be aware of this behavior if not fix it.
    
    Reviewed By: ldworkin
    
    Differential Revision: D27128028
    
    fbshipit-source-id: 3254e06a6df1645933fa3bc0628893a6581a09ba

--- ax/storage/json_store/__init__.py
+++ ax/storage/json_store/__init__.py
@@ -4,8 +4,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from ax.storage.json_store.load import load_experiment as load
-from ax.storage.json_store.save import save_experiment as save
+from ax.storage.json_store.load import load_experiment as json_load
+from ax.storage.json_store.save import save_experiment as json_save
 
 
-__all__ = ["load", "save"]
+__all__ = ["json_load", "json_save"]
