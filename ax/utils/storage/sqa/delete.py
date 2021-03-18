7c8811fd323d4cea43571ebc9ecd42a90489ff7c
Shaun Singh <shaundsingh@fb.com>
2021-03-18 11:19:34 -0700

    Add a util for deleting Ax SQA experiments
    Summary:
    Sometimes you need to delete an experiment (usually due to DB perf issues), and it's not that clear how to do.
    
    T86578463; T86602873
    
    Reviewed By: ldworkin
    
    Differential Revision: D27048627
    
    fbshipit-source-id: 4a778d577087e234967519959c2e4797e1678bd6

---
+++ ax/utils/storage/sqa/delete.py
@@ -0,0 +1,14 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ax.storage.sqa_store.db import session_scope
+from ax.storage.sqa_store.sqa_classes import SQAExperiment
+
+
+def delete_experiment(exp_name: str) -> None:
+    with session_scope() as session:
+        exp = session.query(SQAExperiment).filter_by(name=exp_name).one_or_none()
+        session.delete(exp)
+        session.flush()
