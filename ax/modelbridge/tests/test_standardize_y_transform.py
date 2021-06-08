00a5436a29160138c8f4bdeda9c368d75d84e1b6
Max Balandat <balandat@fb.com>
2021-06-08 10:05:36 -0700

    Use population bias correction in StandardizeYTransform std computation (#589)
    Summary:
    `torch.std` by default uses the Bessel correction (normalize by `N-1`), but `numpy.std` does not (normalize by N). This causes the botorch models to warn about unnormalized data even though data is being normalized in the modelbridge.
    
    This won't matter much from a statistical perspective (this is just data normalization and not inference after all), but it feels like addressing this in Ax may be more reasonable, since in BoTorch when people are asked to standardize their data they will most likely use `torch.std` with default args and it would be very confusing if that would not get rid of the warning.
    
    Pull Request resolved: https://github.com/facebook/Ax/pull/589
    
    Reviewed By: bletham
    
    Differential Revision: D28963474
    
    Pulled By: Balandat
    
    fbshipit-source-id: ff75b5edb0d0ca1936a36dc2d4960fcea7566743

--- ax/modelbridge/tests/test_standardize_y_transform.py
+++ ax/modelbridge/tests/test_standardize_y_transform.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 from copy import deepcopy
+from math import sqrt
 
 import numpy as np
 from ax.core.metric import Metric
@@ -44,7 +45,7 @@ class StandardizeYTransformTest(TestCase):
 
     def testInit(self):
         self.assertEqual(self.t.Ymean, {"m1": 1.0, "m2": 1.5})
-        self.assertEqual(self.t.Ystd, {"m1": 1.0, "m2": 0.5})
+        self.assertEqual(self.t.Ystd, {"m1": 1.0, "m2": sqrt(1 / 3)})
         with self.assertRaises(ValueError):
             StandardizeY(
                 search_space=None, observation_features=None, observation_data=[]
@@ -53,14 +54,20 @@ class StandardizeYTransformTest(TestCase):
     def testTransformObservations(self):
         obsd1_t = ObservationData(
             metric_names=["m1", "m2", "m2"],
-            means=np.array([0.0, 1.0, -1.0]),
-            covariance=np.array([[1.0, 0.4, 0.8], [0.4, 8.0, 3.2], [0.8, 3.2, 12.0]]),
+            means=np.array([0.0, sqrt(3 / 4), -sqrt(3 / 4)]),
+            covariance=np.array(
+                [
+                    [1.0, 0.2 * sqrt(3), 0.4 * sqrt(3)],
+                    [0.2 * sqrt(3), 6.0, 2.4],
+                    [0.4 * sqrt(3), 2.4, 9.0],
+                ],
+            ),
         )
         obsd2 = [deepcopy(self.obsd1)]
         obsd2 = self.t.transform_observation_data(obsd2, [])
-        self.assertTrue(obsd2[0] == obsd1_t)
+        self.assertTrue(osd_allclose(obsd2[0], obsd1_t))
         obsd2 = self.t.untransform_observation_data(obsd2, [])
-        self.assertTrue(obsd2[0] == self.obsd1)
+        self.assertTrue(osd_allclose(obsd2[0], self.obsd1))
 
     def testTransformOptimizationConfig(self):
         m1 = Metric(name="m1")
@@ -89,13 +96,16 @@ class StandardizeYTransformTest(TestCase):
                 metric=m1, op=ComparisonOp.GEQ, bound=1.0, relative=False
             ),
             OutcomeConstraint(
-                metric=m2, op=ComparisonOp.LEQ, bound=4.0, relative=False
+                metric=m2,
+                op=ComparisonOp.LEQ,
+                bound=2.0 * sqrt(3),  # (3.5 - 1.5) / sqrt(1/3)
+                relative=False,
             ),
             ScalarizedOutcomeConstraint(
                 metrics=[m1, m2],
-                weights=[0.5, 0.25],  # [0.5*1.0, 0.5*0.5]
+                weights=[0.5 * 1.0, 0.5 * sqrt(1 / 3)],
                 op=ComparisonOp.LEQ,
-                bound=2.25,  # 3.5 - (0.5* 1.0 + 0.5*1.5)
+                bound=2.25,  # 3.5 - (0.5 * 1.0 + 0.5 * 1.5)
                 relative=False,
             ),
         ]
@@ -109,3 +119,13 @@ class StandardizeYTransformTest(TestCase):
         oc = OptimizationConfig(objective=objective, outcome_constraints=[con])
         with self.assertRaises(ValueError):
             oc = self.t.transform_optimization_config(oc, None, None)
+
+
+def osd_allclose(osd1: ObservationData, osd2: ObservationData) -> bool:
+    if osd1.metric_names != osd2.metric_names:
+        return False
+    if not np.allclose(osd1.means, osd2.means):
+        return False
+    if not np.allclose(osd1.covariance, osd2.covariance):
+        return False
+    return True
