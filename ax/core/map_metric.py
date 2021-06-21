9f8f9f56ca913b21727a11879615eea35ae09aa5
Max Balandat <balandat@fb.com>
2021-06-21 07:05:05 -0700

    Simplify MapMetric
    Summary: Seems like a lot of boilerplate for no apparent reason with the added downside of having to fight pyre?
    
    Reviewed By: danielrjiang
    
    Differential Revision: D29237262
    
    fbshipit-source-id: bebe489ede43881bd251f26553e328d35e0aa956

--- ax/core/map_metric.py
+++ ax/core/map_metric.py
@@ -6,9 +6,8 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict, Iterable, Optional, Type
+from typing import TYPE_CHECKING, Type
 
-from ax.core.abstract_data import AbstractDataFrameData
 from ax.core.map_data import MapData
 from ax.core.metric import Metric
 
@@ -34,104 +33,3 @@ class MapMetric(Metric):
 
     # pyre-fixme[15]: Inconsistent override of `Type[Data]` with `Type[MapData]`
     data_constructor: Type[MapData] = MapData
-
-    def __init__(
-        self,
-        name: str,
-        lower_is_better: Optional[bool] = None,
-        properties: Optional[Dict[str, Any]] = None,
-    ) -> None:
-        """Inits Metric.
-
-        Args:
-            name: Name of metric.
-            lower_is_better: Flag for metrics which should be minimized.
-            properties: Dictionary of this metric's properties
-        """
-        self._name = name
-        self.lower_is_better = lower_is_better
-        self.properties = properties or {}
-
-    def fetch_trial_data(
-        self, trial: core.base_trial.BaseTrial, **kwargs: Any
-    ) -> AbstractDataFrameData:
-        """Fetch data for one trial."""
-        return super().fetch_trial_data(trial=trial, **kwargs)
-
-    def fetch_experiment_data(
-        self, experiment: core.experiment.Experiment, **kwargs: Any
-    ) -> AbstractDataFrameData:
-        """Fetch this metric's data for an experiment.
-
-        Default behavior is to fetch data from all trials expecting data
-        and concatenate the results.
-        """
-        return super().fetch_experiment_data(experiment=experiment, **kwargs)
-
-    @classmethod
-    # pyre-fixme [14]: overrides superclass method inconsistently.
-    #   Parameter of type `Iterable[MapMetric]` is not a supertype of
-    #   overridden `Iterable[Metric]
-    def fetch_trial_data_multi(
-        cls,
-        trial: core.base_trial.BaseTrial,
-        metrics: Iterable[MapMetric],
-        **kwargs: Any,
-    ) -> AbstractDataFrameData:
-        """Fetch multiple metrics data for one trial.
-
-        Default behavior calls `fetch_trial_data` for each metric.
-        Subclasses should override this to trial data computation for multiple metrics.
-        """
-        return super().fetch_trial_data_multi(trial=trial, metrics=metrics, **kwargs)
-
-    @classmethod
-    # pyre-fixme [14]: overrides superclass method inconsistently.
-    #   Parameter of type `Iterable[MapMetric]` is not a supertype of
-    #   overridden `Iterable[Metric]`
-    def fetch_experiment_data_multi(
-        cls,
-        experiment: core.experiment.Experiment,
-        metrics: Iterable[MapMetric],
-        trials: Optional[Iterable[core.base_trial.BaseTrial]] = None,
-        **kwargs: Any,
-    ) -> AbstractDataFrameData:
-        """Fetch multiple metrics data for an experiment.
-
-        Default behavior calls `fetch_trial_data_multi` for each trial.
-        Subclasses should override to batch data computation across trials + metrics.
-        """
-        return super().fetch_experiment_data_multi(
-            experiment=experiment, metrics=metrics, trials=trials, **kwargs
-        )
-
-    @classmethod
-    # pyre-fixme [14]: overrides superclass method inconsistently.
-    #   Parameter of type `Iterable[MapMetric]` is not a supertype of
-    #   overridden `Iterable[Metric]`
-    def lookup_or_fetch_experiment_data_multi(
-        cls,
-        experiment: core.experiment.Experiment,
-        metrics: Iterable[MapMetric],
-        trials: Optional[Iterable[core.base_trial.BaseTrial]] = None,
-        **kwargs: Any,
-    ) -> AbstractDataFrameData:
-        """Fetch or lookup (with fallback to fetching) data for given metrics,
-        depending on whether they are available while running.
-
-        If metric is available while running, its data can change (and therefore
-        we should always re-fetch it). If metric is available only upon trial
-        completion, its data does not change, so we can look up that data on
-        the experiment and only fetch the data that is not already attached to
-        the experiment.
-
-        NOTE: If fetching data for a metrics class that is only available upon
-        trial completion, data fetched in this function (data that was not yet
-        available on experiment) will be attached to experiment.
-        """
-        return super().lookup_or_fetch_experiment_data_multi(
-            experiment=experiment,
-            metrics=metrics,
-            trials=trials,
-            **kwargs,
-        )
