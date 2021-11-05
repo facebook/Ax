ebde9d6ee1c4a2317e7483a4baa93dc2155b308c
Lena Kashtelyan <drfreund@fb.com>
2021-11-05 09:46:06 -0700

    Add `ModelSpec` subclass for models specified as factory functions
    Summary:
    In generation step we support models defined as either members of en enum that extends `ModelsRegistryBase` [1] or a factory function. I'd like to have the following structure for now:
    
    ```
    dataclass
    class GenerationNode:
        model_specs: List[ModelSpec]
        num_trials: int
        ...  # A bunch of other stuff
    
    def fit(...):
        # fits all the model specs
    
    def gen(...):
        # gens from all the model specs
    ```
    ```
    dataclass
    class GenerationStep(GenerationNode):
        model: Union[ModelsRegistryBase, TModelFactoryFunction]
        num_trials: int
        model_kwargs: Dict[str, Any]
        model_gen_kwargs: Dict[str, Any]
        ...  # A bunch of other stuff
    
        def __post_init__(self):
            # Because we set model specs here, we can directly leverage
            # `super().fit` and `gen` instead of overriding them here
            self.model_specs = [
                  ModelSpec(
                      model=self.model,
                      model_kwargs=self.model_kwargs,
                      model_gen_kwargs=self.model_gen_kwargs,
                  )
            ]
    ```
    
    I think the benefits of this structure are:
    1) We add a shared and reusable `GenerationNode` abstraction that will contain the bulk of the generation step logic for `fit`, `update`, and `gen` and interface with multiple `ModelSpec`-s to fit/gen from them.
    2) We keep a notion of `GenerationStep` to backwards compatibilityand to be able to limit che changes needed to generation strategy at this point; it inherits `fit`, `update`, and `gen` from `GenerationNode`, and its only real difference is that it only has one `ModelSpec` and that model spec is expressed in backwards-compatible way.
        1) All actual logic involved in gen/fit from `GenerationStep` actually delegates to `GenerationNode`.
    
    The only blocker to this structure right now is that `ModelSpec` only accepts model type specified as a member of `Models` enum and not as a factory function (which is allowed by `GenerationStep` and is important functionality, since it's the only way to use the generation strategy with something other than a `Models` member). I think disallowing factory functions in
    `GenerationStep` is out of scope for now (although we could add a deprecation warning), so instead my proposed solution is in this diff.
    
     ---
    
    Reviewed By: ldworkin
    
    Differential Revision: D31996883
    
    fbshipit-source-id: 73e97a0835af3a133d567c2a728ae92931d8e112

--- ax/modelbridge/model_spec.py
+++ ax/modelbridge/model_spec.py
@@ -4,8 +4,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import warnings
 from dataclasses import dataclass
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Callable
 
 from ax.core.data import Data
 from ax.core.experiment import Experiment
@@ -17,22 +18,30 @@ from ax.exceptions.core import UserInputError
 from ax.modelbridge.base import ModelBridge
 from ax.modelbridge.registry import ModelRegistryBase
 from ax.utils.common.base import Base
-from ax.utils.common.kwargs import consolidate_kwargs, get_function_argument_names
+from ax.utils.common.kwargs import (
+    consolidate_kwargs,
+    get_function_argument_names,
+    filter_kwargs,
+)
 from ax.utils.common.typeutils import not_none
+
+
+TModelFactory = Callable[..., ModelBridge]
 
 
 @dataclass
 class ModelSpec(Base):
     model_enum: ModelRegistryBase
-    # Kwargs to pass into the `Model` + `ModelBridge` constructor,
-    # (`ModelRegistryBase.__call__`).
+    # Kwargs to pass into the `Model` + `ModelBridge` constructors in
+    # `ModelRegistryBase.__call__`.
     model_kwargs: Optional[Dict[str, Any]] = None
-    # Kwargs to pass into the `ModelBridge.gen`.
+    # Kwargs to pass to `ModelBridge.gen`.
     model_gen_kwargs: Optional[Dict[str, Any]] = None
     # Fixed generation features to pass into the Model's `.gen` function.
     fixed_features: Optional[ObservationFeatures] = None
 
-    # Fitted model, constructed using specified model_kwargs, Data on fit()
+    # Fitted model, constructed using specified `model_kwargs` and `Data`
+    # on `ModelSpec.fit`
     _fitted_model: Optional[ModelBridge] = None
 
     @property
@@ -40,6 +49,16 @@ class ModelSpec(Base):
         """Returns the fitted Ax model, asserting fit() was called"""
         self._assert_fitted()
         return not_none(self._fitted_model)
+
+    @property
+    def model_key(self) -> str:
+        """Key string to identify the model used by this ``ModelSpec``."""
+        # NOTE: In the future, might need to add more to model key to make
+        # model specs with the same model (but different kwargs) easier to
+        # distinguish from their key. Could also add separate property, just
+        # `key` (for `ModelSpec.key`, which will be unique even between model
+        # specs with same model type).
+        return self.model_enum.value
 
     def fit(
         self,
@@ -53,18 +72,15 @@ class ModelSpec(Base):
         model kwargs set on the model spec, alongside any passed down as
         kwargs to this function (local kwargs take precedent)
         """
-        search_space = search_space or experiment.search_space
-        optimization_config = optimization_config or experiment.optimization_config
-        model_kwargs = {
-            **(self.model_kwargs or {}),
-            **model_kwargs,
-        }
         self._fitted_model = self.model_enum(
             experiment=experiment,
             data=data,
-            search_space=search_space,
-            optimization_config=optimization_config,
-            **model_kwargs,
+            **self._prepare_model_fit_kwargs(
+                experiment=experiment,
+                search_space=search_space,
+                optimization_config=optimization_config,
+                **model_kwargs,
+            ),
         )
 
     def update(self, experiment: Experiment, new_data: Data) -> None:
@@ -96,3 +112,83 @@ class ModelSpec(Base):
         """Helper that verifies a model was fitted, raising an error if not"""
         if self._fitted_model is None:
             raise UserInputError("No fitted model found. Call fit() to generate one")
+
+    def _prepare_model_fit_kwargs(
+        self,
+        experiment: Experiment,
+        search_space: Optional[SearchSpace] = None,
+        optimization_config: Optional[OptimizationConfig] = None,
+        **model_kwargs: Any,
+    ) -> Dict[str, Any]:
+        """TODO"""
+        return {
+            "search_space": search_space or experiment.search_space,
+            "optimization_config": optimization_config
+            or experiment.optimization_config,
+            **(self.model_kwargs or {}),
+            **model_kwargs,
+        }
+
+
+@dataclass
+class FactoryFunctionModelSpec(ModelSpec):
+    factory_function: Optional[TModelFactory] = None
+    # pyre-ignore[15]: `ModelSpec` has this as non-optional
+    model_enum: Optional[ModelRegistryBase] = None
+
+    def __post_init__(self) -> None:
+        if self.model_enum is not None:
+            raise UserInputError(
+                "Use regular `ModelSpec` when it's possible to describe the "
+                "model as `ModelRegistryBase` subclass enum member."
+            )
+        if self.factory_function is None:
+            raise UserInputError(
+                "Please specify a valid function returning a `ModelBridge` instance "
+                "as the required `factory_function` argument to "
+                "`FactoryFunctionModelSpec`."
+            )
+        warnings.warn(
+            "Using a factory function to describe the model, so optimization state "
+            "cannot be stored and optimization is not resumable if interrupted."
+        )
+
+    @property
+    def model_key(self) -> str:
+        """Key string to identify the model used by this ``ModelSpec``."""
+        try:
+            # `model` is defined via a factory function.
+            return not_none(self.factory_function).__name__  # pyre-ignore[16]
+        except Exception:
+            raise TypeError(  # pragma: no cover
+                f"{self.factory_function} is not a valid function, cannot extract name."
+            )
+
+    def fit(
+        self,
+        experiment: Experiment,
+        data: Data,
+        search_space: Optional[SearchSpace] = None,
+        optimization_config: Optional[OptimizationConfig] = None,
+        **model_kwargs: Any,
+    ) -> None:
+        """Fits the specified model on the given experiment + data using the
+        model kwargs set on the model spec, alongside any passed down as
+        kwargs to this function (local kwargs take precedent)
+        """
+        factory_function = not_none(self.factory_function)
+        self._fitted_model = factory_function(
+            **filter_kwargs(
+                factory_function,
+                experiment=experiment,
+                data=data,
+                # Some factory functions (like `get_sobol`) require search space
+                # instead of experiment.
+                **self._prepare_model_fit_kwargs(
+                    experiment=experiment,
+                    search_space=search_space,
+                    optimization_config=optimization_config,
+                    **model_kwargs,
+                ),
+            )
+        )
--- ax/modelbridge/tests/test_model_spec.py
+++ ax/modelbridge/tests/test_model_spec.py
@@ -5,7 +5,8 @@
 # LICENSE file in the root directory of this source tree.
 
 from ax.exceptions.core import UserInputError
-from ax.modelbridge.model_spec import ModelSpec
+from ax.modelbridge.factory import get_sobol
+from ax.modelbridge.model_spec import ModelSpec, FactoryFunctionModelSpec
 from ax.modelbridge.registry import Models
 from ax.utils.common.testutils import TestCase
 from ax.utils.testing.core_stubs import get_branin_experiment
@@ -29,3 +30,22 @@ class ModelSpecTest(TestCase):
         ms.gen(n=1)
         with self.assertRaises(NotImplementedError):
             ms.update(experiment=self.experiment, new_data=self.data)
+
+    def test_model_key(self):
+        ms = ModelSpec(model_enum=Models.GPEI)
+        self.assertEqual(ms.model_key, "GPEI")
+
+
+class FactoryFunctionModelSpecTest(ModelSpecTest):
+    def test_construct(self):
+        ms = FactoryFunctionModelSpec(factory_function=get_sobol)
+        with self.assertRaises(UserInputError):
+            ms.gen(n=1)
+        ms.fit(experiment=self.experiment, data=self.data)
+        ms.gen(n=1)
+        with self.assertRaises(NotImplementedError):
+            ms.update(experiment=self.experiment, new_data=self.data)
+
+    def test_model_key(self):
+        ms = FactoryFunctionModelSpec(factory_function=get_sobol)
+        self.assertEqual(ms.model_key, "get_sobol")
