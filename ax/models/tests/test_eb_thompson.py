d556c9ac4cf4fba500c1836d5561232ca8603711
Miles Olson <mpolson64@fb.com>
2021-11-23 06:32:30 -0800

    Remove unused import
    Summary: Oops! Totally my mistake, but maybe worthwhile to think about if we should make lint errors block land?
    
    Reviewed By: EugenHotaj
    
    Differential Revision: D32601810
    
    fbshipit-source-id: 504e36eac438ba1ce3d1234f6d8d4d7721618ed7

--- ax/models/tests/test_eb_thompson.py
+++ ax/models/tests/test_eb_thompson.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 import numpy as np
 from ax.models.discrete.eb_thompson import EmpiricalBayesThompsonSampler
