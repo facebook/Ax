b58ad5a031b88f0e34737d0230291d5e6d3c6b5c
Daniel Jiang <drjiang@fb.com>
2021-12-02 14:45:18 -0800

    improve handling of duplicate training curves for TensorboardCurveMetric (#739)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/739
    
    see title
    
    Reviewed By: Balandat
    
    Differential Revision: D32732543
    
    fbshipit-source-id: 37bff5e5dca726024e996e3ae356d72dd4357d77

--- ax/metrics/tests/test_tensorboard.py
+++ ax/metrics/tests/test_tensorboard.py
@@ -4,12 +4,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import tempfile
 from unittest import mock
 
 import numpy as np
 import pandas as pd
-from ax.metrics.tensorboard import TensorboardCurveMetric
+from ax.metrics.tensorboard import TensorboardCurveMetric, get_tb_from_posix
 from ax.utils.common.testutils import TestCase
+from torch.utils.tensorboard import SummaryWriter
 
 
 class TensorboardCurveMetricTest(TestCase):
@@ -28,7 +30,35 @@ class TensorboardCurveMetricTest(TestCase):
         self.assertTrue(np.array_equal(out["2"].values, np.array([2, 2])))
 
     def test_GetTbFromPosix(self):
-        # TODO: Read some actual tensorboard logs, e.g. from a temporary file
-        # we write to, or from some dir with test data. The latter is preferred
-        # so that we can ensure backwards compatibility.
-        pass
+        time = 0
+        with tempfile.TemporaryDirectory() as temp_dir:
+            writer = SummaryWriter(log_dir=temp_dir)
+            # first training curve with 3 points
+            for i in range(3):
+                writer.add_scalar(
+                    tag="train/loss", scalar_value=i * 10, global_step=i, walltime=time
+                )
+                time += 1
+            result = get_tb_from_posix(path=temp_dir)
+            self.assertTrue(
+                np.allclose(result["train/loss"].index.values, np.array([0, 1, 2]))
+            )
+            self.assertTrue(
+                np.allclose(result["train/loss"].values, np.array([0, 10, 20]))
+            )
+            # second training curve (like a restarted run) with later walltimes
+            for i in range(5):
+                writer.add_scalar(
+                    tag="train/loss", scalar_value=i * 5, global_step=i, walltime=time
+                )
+                time += 1
+            result = get_tb_from_posix(path=temp_dir)
+            # expected behavior is to return the second training curve and ignore first
+            self.assertTrue(
+                np.allclose(
+                    result["train/loss"].index.values, np.array([0, 1, 2, 3, 4])
+                )
+            )
+            self.assertTrue(
+                np.allclose(result["train/loss"].values, np.array([0, 5, 10, 15, 20]))
+            )
