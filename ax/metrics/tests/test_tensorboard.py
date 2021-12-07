791212692da0ee01dfc02ff019fb004fea14e2a0
Daniel Jiang <drjiang@fb.com>
2021-12-06 17:01:35 -0800

    remove get_tb_from_posix test
    Summary:
    The use of TemporaryDirectory looks to be causing various unpredictable issues in OSS:
    - https://github.com/facebook/Ax/runs/4436501181?check_suite_focus=true
    - https://github.com/facebook/Ax/actions/runs/1531751150
    Removing it for now until further investigation.
    
    Reviewed By: Balandat
    
    Differential Revision: D32897524
    
    fbshipit-source-id: 770b177ab57fa5a109949cbbe8e5b91b18dced23

--- ax/metrics/tests/test_tensorboard.py
+++ ax/metrics/tests/test_tensorboard.py
@@ -4,14 +4,12 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import tempfile
 from unittest import mock
 
 import numpy as np
 import pandas as pd
-from ax.metrics.tensorboard import TensorboardCurveMetric, get_tb_from_posix
+from ax.metrics.tensorboard import TensorboardCurveMetric
 from ax.utils.common.testutils import TestCase
-from torch.utils.tensorboard import SummaryWriter
 
 
 class TensorboardCurveMetricTest(TestCase):
@@ -28,37 +26,3 @@ class TensorboardCurveMetricTest(TestCase):
         self.assertEqual(len(out), 2)
         self.assertTrue(np.array_equal(out["1"].values, np.array([1, 1])))
         self.assertTrue(np.array_equal(out["2"].values, np.array([2, 2])))
-
-    def test_GetTbFromPosix(self):
-        time = 0
-        with tempfile.TemporaryDirectory() as temp_dir:
-            writer = SummaryWriter(log_dir=temp_dir)
-            # first training curve with 3 points
-            for i in range(3):
-                writer.add_scalar(
-                    tag="train/loss", scalar_value=i * 10, global_step=i, walltime=time
-                )
-                time += 1
-            result = get_tb_from_posix(path=temp_dir)
-            self.assertTrue(
-                np.allclose(result["train/loss"].index.values, np.array([0, 1, 2]))
-            )
-            self.assertTrue(
-                np.allclose(result["train/loss"].values, np.array([0, 10, 20]))
-            )
-            # second training curve (like a restarted run) with later walltimes
-            for i in range(5):
-                writer.add_scalar(
-                    tag="train/loss", scalar_value=i * 5, global_step=i, walltime=time
-                )
-                time += 1
-            result = get_tb_from_posix(path=temp_dir)
-            # expected behavior is to return the second training curve and ignore first
-            self.assertTrue(
-                np.allclose(
-                    result["train/loss"].index.values, np.array([0, 1, 2, 3, 4])
-                )
-            )
-            self.assertTrue(
-                np.allclose(result["train/loss"].values, np.array([0, 5, 10, 15, 20]))
-            )
