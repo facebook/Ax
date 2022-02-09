facebook/Ax

commit 3b0cafa1d49f10113eb5dbbca53effbd9e6ccf0a

Support new BoTorch PosteriorTransform (#803)
Summary:
Pull Request resolved: https://github.com/facebook/Ax/pull/803

Changes required by the introduction of PosteriorTransform in BoTorch

Reviewed By: sdaulton

Differential Revision: D30317863

fbshipit-source-id: 8f30b191ba19e11e2684b3da9fd3c0f99d340703

ax/models/torch/utils.py
@@ -21,11 +21,13 @@ from botorch.acquisition.fixed_feature import FixedFeatureAcquisitionFunction
 from botorch.acquisition.monte_carlo import qSimpleRegret
 from botorch.acquisition.multi_objective.objective import WeightedMCMultiOutputObjective
 from botorch.acquisition.objective import (
-    AcquisitionObjective,
     ConstrainedMCObjective,
-    LinearMCObjective,
     MCAcquisitionObjective,
     ScalarizedObjective,
+)
+from botorch.acquisition.objective import (
+    PosteriorTransform,
+    ScalarizedPosteriorTransform,
 )
 from botorch.acquisition.utils import get_infeasible_cost
 from botorch.exceptions.errors import UnsupportedError
@@ -36,7 +38,6 @@ from botorch.utils.constraints import get_outcome_constraint_transforms
 from botorch.utils.objective import get_objective_weights_transform
 from botorch.utils.sampling import sample_hypersphere, sample_simplex
 from torch import Tensor
-
 
 logger = get_logger(__name__)
 
@@ -336,24 +337,19 @@ def tensor_callable_to_array_callable(
     return array_func
 
 
-def get_botorch_objective(
+def get_botorch_objective_and_transform(
     model: Model,
     objective_weights: Tensor,
-    use_scalarized_objective: bool = True,
     outcome_constraints: Optional[Tuple[Tensor, Tensor]] = None,
     objective_thresholds: Optional[Tensor] = None,
     X_observed: Optional[Tensor] = None,
-) -> AcquisitionObjective:
+) -> Tuple[Optional[MCAcquisitionObjective], Optional[PosteriorTransform]]:
     """Constructs a BoTorch `AcquisitionObjective` object.
 
     Args:
         model: A BoTorch Model
         objective_weights: The objective is to maximize a weighted sum of
             the columns of f(x). These are the weights.
-        use_scalarized_objective: A boolean parameter that defaults to True,
-            specifying whether ScalarizedObjective should be used.
-            NOTE: when using outcome_constraints, use_scalarized_objective
-            will be ignored.
         outcome_constraints: A tuple of (A, b). For k outcome constraints
             and m outputs at f(x), A is (k x m) and b is (k x 1) such that
             A f(x) <= b. (Not used by single task models)
@@ -364,26 +360,23 @@ def get_botorch_objective(
             objective or the constraints. None if there are no such points.
 
     Returns:
-        A BoTorch `AcquisitionObjective` object. It will be one of:
-        `ScalarizedObjective`, `LinearMCOObjective`, `ConstrainedMCObjective`.
+        A two-tuple containing (optioally) an `MCAcquisitionObjective` and
+        (optionally) a `PosteriorTransform`.
     """
     if objective_thresholds is not None:
+        # we are doing multi-objective optimization
         nonzero_idcs = torch.nonzero(objective_weights).view(-1)
         objective_weights = objective_weights[nonzero_idcs]
-        return WeightedMCMultiOutputObjective(
+        objective = WeightedMCMultiOutputObjective(
             weights=objective_weights, outcomes=nonzero_idcs.tolist()
         )
+        return objective, None
     if X_observed is None:
         raise UnsupportedError(
-            "X_observed is required to construct a BoTorch Objective."
+            "X_observed is required to construct a BoTorch objective."
         )
     if outcome_constraints:
-        if use_scalarized_objective:
-            logger.warning(
-                "Currently cannot use ScalarizedObjective when there are outcome "
-                "constraints. Ignoring (default) kwarg `use_scalarized_objective`"
-                "= True. Creating ConstrainedMCObjective."
-            )
+        # If there are outcome constraints, we use MC Acquistion functions
         obj_tf = get_objective_weights_transform(objective_weights)
 
         def objective(samples: Tensor, X: Optional[Tensor] = None) -> Tensor:
@@ -391,12 +384,13 @@ def get_botorch_objective(
 
         con_tfs = get_outcome_constraint_transforms(outcome_constraints)
         inf_cost = get_infeasible_cost(X=X_observed, model=model, objective=obj_tf)
-        return ConstrainedMCObjective(
+        objective = ConstrainedMCObjective(
             objective=objective, constraints=con_tfs or [], infeasible_cost=inf_cost
         )
-    elif use_scalarized_objective:
-        return ScalarizedObjective(weights=objective_weights)
-    return LinearMCObjective(weights=objective_weights)
+        return objective, None
+    # Case of linear weights - use ScalarizedPosteriorTransform
+    transform = ScalarizedPosteriorTransform(weights=objective_weights)
+    return None, transform
 
 
 def get_out_of_sample_best_point_acqf(
@@ -457,19 +451,23 @@ def get_out_of_sample_best_point_acqf(
         qmc=qmc,
         seed_inner=seed_inner,
     )
-    objective = get_botorch_objective(
+    objective, posterior_transform = get_botorch_objective_and_transform(
         model=model,
         objective_weights=objective_weights,
         outcome_constraints=outcome_constraints,
         X_observed=X_observed,
     )
-    if not isinstance(objective, (ScalarizedObjective, MCAcquisitionObjective)):
-        raise UnsupportedError(
-            f"Unknown objective type: {objective.__class__}"  # pragma: nocover
-        )
-    else:
-        # pyre-ignore[28]: All acq. functions here expect `objective` kwarg.
-        acqf = acqf_class(model=model, objective=objective, **acqf_options)
+
+    if objective is not None:
+        if not isinstance(objective, MCAcquisitionObjective):
+            raise UnsupportedError(
+                f"Unknown objective type: {objective.__class__}"  # pragma: nocover
+            )
+        acqf_options = {"objective": objective, **acqf_options}
+    if posterior_transform is not None:
+        acqf_options = {"posterior_transform": posterior_transform, **acqf_options}
+
+    acqf = acqf_class(model=model, **acqf_options)  # pyre-ignore [45]
 
     if fixed_features:
         acqf = FixedFeatureAcquisitionFunction(

ax/models/tests/test_torch_utils.py
@@ -7,13 +7,15 @@ from typing import Set, Tuple
 from unittest.mock import MagicMock, patch
 
 import torch
-from ax.models.torch.utils import _get_X_pending_and_observed, get_botorch_objective
+from ax.models.torch.utils import (
+    _get_X_pending_and_observed,
+    get_botorch_objective_and_transform,
+)
 from ax.utils.common.testutils import TestCase
 from botorch.acquisition.multi_objective.objective import WeightedMCMultiOutputObjective
 from botorch.acquisition.objective import (
     ConstrainedMCObjective,
-    LinearMCObjective,
-    ScalarizedObjective,
+    ScalarizedPosteriorTransform,
 )
 from botorch.exceptions.errors import BotorchTensorDimensionError
 from botorch.models.model import Model
@@ -63,68 +65,45 @@ class TorchUtilsTest(TestCase):
         expected = Xs[0]
         self.assertEqual(_to_obs_set(expected), _to_obs_set(X_observed))
 
-    @patch(f"{get_botorch_objective.__module__}.get_infeasible_cost", return_value=1.0)
+    @patch(
+        f"{get_botorch_objective_and_transform.__module__}.get_infeasible_cost",
+        return_value=1.0,
+    )
     def test_get_botorch_objective(self, _):
-        # Whether `use_scalarized_objective` is specified or not, when there are
-        # outcome constraints, `ConstrainedMCObjective` should be picked.
-        self.assertIsInstance(
-            get_botorch_objective(
-                model=self.mock_botorch_model,
-                outcome_constraints=self.outcome_constraints,
-                objective_weights=self.objective_weights,
-                X_observed=self.X_dummy,
-                use_scalarized_objective=False,
-            ),
-            ConstrainedMCObjective,
+        # If there are outcome constraints, use a ConstrainedMCObjective
+        obj, tf = get_botorch_objective_and_transform(
+            model=self.mock_botorch_model,
+            outcome_constraints=self.outcome_constraints,
+            objective_weights=self.objective_weights,
+            X_observed=self.X_dummy,
         )
-        self.assertIsInstance(
-            get_botorch_objective(
-                model=self.mock_botorch_model,
-                outcome_constraints=self.outcome_constraints,
-                objective_weights=self.objective_weights,
-                X_observed=self.X_dummy,
-            ),
-            ConstrainedMCObjective,
-        )
+        self.assertIsInstance(obj, ConstrainedMCObjective)
+        self.assertIsNone(tf)
 
-        # In absence of outcome constraints and with not using scalarized objective,
-        # `LinearMCObjective` should be picked.
-        self.assertIsInstance(
-            get_botorch_objective(
-                model=self.mock_botorch_model,
-                objective_weights=self.objective_weights,
-                X_observed=self.X_dummy,
-                use_scalarized_objective=False,
-            ),
-            LinearMCObjective,
+        # By default, `ScalarizedPosteriorTransform` should be picked in absence of
+        # outcome constraints.
+        obj, tf = get_botorch_objective_and_transform(
+            model=self.mock_botorch_model,
+            objective_weights=self.objective_weights,
+            X_observed=self.X_dummy,
         )
-
-        # By default, `ScalarizedObjective` should be picked in absence of outcome
-        # constraints.
-        self.assertIsInstance(
-            get_botorch_objective(
-                model=self.mock_botorch_model,
-                objective_weights=self.objective_weights,
-                X_observed=self.X_dummy,
-            ),
-            ScalarizedObjective,
-        )
+        self.assertIsNone(obj)
+        self.assertIsInstance(tf, ScalarizedPosteriorTransform)
 
         # Test MOO case.
         with self.assertRaises(BotorchTensorDimensionError):
-            get_botorch_objective(
+            get_botorch_objective_and_transform(
                 model=self.mock_botorch_model,
                 objective_weights=self.objective_weights,  # Only has 1 objective.
                 X_observed=self.X_dummy,
                 objective_thresholds=self.objective_thresholds,
             )
 
-        self.assertIsInstance(
-            get_botorch_objective(
-                model=self.mock_botorch_model,
-                objective_weights=self.moo_objective_weights,  # Has 2 objectives.
-                X_observed=self.X_dummy,
-                objective_thresholds=self.objective_thresholds,
-            ),
-            WeightedMCMultiOutputObjective,
+        obj, tf = get_botorch_objective_and_transform(
+            model=self.mock_botorch_model,
+            objective_weights=self.moo_objective_weights,  # Has 2 objectives.
+            X_observed=self.X_dummy,
+            objective_thresholds=self.objective_thresholds,
         )
+        self.assertIsInstance(obj, WeightedMCMultiOutputObjective)
+        self.assertIsNone(tf)

ax/models/torch/alebo.py
@@ -21,6 +21,7 @@ from ax.utils.common.docutils import copy_doc
 from ax.utils.common.logger import get_logger
 from botorch.acquisition.acquisition import AcquisitionFunction
 from botorch.acquisition.analytic import ExpectedImprovement
+from botorch.acquisition.objective import PosteriorTransform
 from botorch.models.gp_regression import FixedNoiseGP
 from botorch.models.gpytorch import GPyTorchModel
 from botorch.models.model_list_gp_regression import ModelListGP
@@ -177,12 +178,16 @@ class ALEBOGP(FixedNoiseGP):
         X: Tensor,
         output_indices: Optional[List[int]] = None,
         observation_noise: Union[bool, Tensor] = False,
+        posterior_transform: Optional[PosteriorTransform] = None,
         **kwargs: Any,
     ) -> GPyTorchPosterior:
         assert output_indices is None
         assert not observation_noise
         mvn = self(X)
-        return GPyTorchPosterior(mvn=mvn)
+        posterior = GPyTorchPosterior(mvn=mvn)
+        if posterior_transform is not None:
+            return posterior_transform(posterior)
+        return posterior
 
 
 def get_fitted_model(

ax/models/torch/botorch_kg.py
@@ -13,7 +13,7 @@ from ax.models.torch.botorch_defaults import recommend_best_out_of_sample_point
 from ax.models.torch.utils import (
     _get_X_pending_and_observed,
     _to_inequality_constraints,
-    get_botorch_objective,
+    get_botorch_objective_and_transform,
     get_out_of_sample_best_point_acqf,
     subset_model,
 )
@@ -24,7 +24,7 @@ from botorch.acquisition.knowledge_gradient import (
     qKnowledgeGradient,
     qMultiFidelityKnowledgeGradient,
 )
-from botorch.acquisition.objective import AcquisitionObjective, MCAcquisitionObjective
+from botorch.acquisition.objective import MCAcquisitionObjective, PosteriorTransform
 from botorch.acquisition.utils import (
     expand_trace_observations,
     project_to_target_fidelity,
@@ -139,7 +139,7 @@ class KnowledgeGradient(BotorchModel):
             objective_weights = subset_model_results.objective_weights
             outcome_constraints = subset_model_results.outcome_constraints
 
-        objective = get_botorch_objective(
+        objective, posterior_transform = get_botorch_objective_and_transform(
             model=model,
             objective_weights=objective_weights,
             outcome_constraints=outcome_constraints,
@@ -182,6 +182,7 @@ class KnowledgeGradient(BotorchModel):
         acq_function = _instantiate_KG(
             model=model,
             objective=objective,
+            posterior_transform=posterior_transform,
             qmc=qmc,
             n_fantasies=n_fantasies,
             num_trace_observations=options.get("num_trace_observations", 0),
@@ -292,7 +293,8 @@ class KnowledgeGradient(BotorchModel):
 
 def _instantiate_KG(
     model: Model,
-    objective: AcquisitionObjective,
+    objective: Optional[MCAcquisitionObjective] = None,
+    posterior_transform: Optional[PosteriorTransform] = None,
     qmc: bool = True,
     n_fantasies: int = 64,
     mc_samples: int = 256,
@@ -343,6 +345,7 @@ def _instantiate_KG(
             num_fantasies=n_fantasies,
             sampler=fantasy_sampler,
             objective=objective,
+            posterior_transform=posterior_transform,
             inner_sampler=inner_sampler,
             X_pending=X_pending,
             current_value=current_value,
@@ -356,6 +359,7 @@ def _instantiate_KG(
         num_fantasies=n_fantasies,
         sampler=fantasy_sampler,
         objective=objective,
+        posterior_transform=posterior_transform,
         inner_sampler=inner_sampler,
         X_pending=X_pending,
         current_value=current_value,

ax/models/torch/botorch_modular/acquisition.py
@@ -23,7 +23,7 @@ from ax.models.torch.botorch_modular.surrogate import Surrogate
 from ax.models.torch.botorch_moo_defaults import infer_objective_thresholds
 from ax.models.torch.utils import (
     _get_X_pending_and_observed,
-    get_botorch_objective,
+    get_botorch_objective_and_transform,
     subset_model,
 )
 from ax.utils.common.base import Base
@@ -31,9 +31,8 @@ from ax.utils.common.constants import Keys
 from ax.utils.common.docutils import copy_doc
 from ax.utils.common.typeutils import not_none
 from botorch.acquisition.acquisition import AcquisitionFunction
-from botorch.acquisition.analytic import AnalyticAcquisitionFunction
 from botorch.acquisition.input_constructors import get_acqf_input_constructor
-from botorch.acquisition.objective import AcquisitionObjective
+from botorch.acquisition.objective import MCAcquisitionObjective, PosteriorTransform
 from botorch.models.model import Model
 from botorch.optim.optimize import optimize_acqf
 from botorch.optim.optimize import optimize_acqf_mixed, optimize_acqf_discrete
@@ -144,7 +143,7 @@ class Acquisition(Base):
                 if subset_idcs is not None
                 else self._objective_thresholds
             )
-        objective = self.get_botorch_objective(
+        objective, posterior_transform = self.get_botorch_objective_and_transform(
             botorch_acqf_class=botorch_acqf_class,
             model=model,
             objective_weights=objective_weights,
@@ -175,6 +174,7 @@ class Acquisition(Base):
             model=model,
             training_data=self.surrogate.training_data,
             objective=objective,
+            posterior_transform=posterior_transform,
             **input_constructor_kwargs,
         )
         self.acqf = botorch_acqf_class(**acqf_inputs)  # pyre-ignore [45]
@@ -389,7 +389,7 @@ class Acquisition(Base):
         """
         return {}
 
-    def get_botorch_objective(
+    def get_botorch_objective_and_transform(
         self,
         botorch_acqf_class: Type[AcquisitionFunction],
         model: Model,
@@ -397,13 +397,10 @@ class Acquisition(Base):
         objective_thresholds: Optional[Tensor] = None,
         outcome_constraints: Optional[Tuple[Tensor, Tensor]] = None,
         X_observed: Optional[Tensor] = None,
-    ) -> AcquisitionObjective:
-        return get_botorch_objective(
+    ) -> Tuple[Optional[MCAcquisitionObjective], Optional[PosteriorTransform]]:
+        return get_botorch_objective_and_transform(
             model=model,
             objective_weights=objective_weights,
-            use_scalarized_objective=issubclass(
-                botorch_acqf_class, AnalyticAcquisitionFunction
-            ),
             outcome_constraints=outcome_constraints,
             objective_thresholds=objective_thresholds,
             X_observed=X_observed,

ax/models/torch/botorch_moo_defaults.py
@@ -153,7 +153,7 @@ def get_NEHVI(
     return get_acquisition_function(
         acquisition_function_name="qNEHVI",
         model=model,
-        objective=objective,  # pyre-ignore [6]
+        objective=objective,
         X_observed=X_observed,
         X_pending=X_pending,
         constraints=cons_tfs,
@@ -228,10 +228,6 @@ def get_EHVI(
     return get_acquisition_function(
         acquisition_function_name="qEHVI",
         model=model,
-        # TODO (jej): Fix pyre error below by restructuring class hierarchy.
-        # pyre-fixme[6]: Expected `botorch.acquisition.objective.
-        #  MCAcquisitionObjective` for 3rd parameter `objective` to call
-        #  `get_acquisition_function` but got `IdentityMCMultiOutputObjective`.
         objective=objective,
         X_observed=X_observed,
         X_pending=X_pending,

ax/models/torch/tests/test_acquisition.py
@@ -110,7 +110,7 @@ class AcquisitionTest(TestCase):
         f"{ACQUISITION_PATH}.subset_model",
         return_value=SubsetModelData(None, torch.ones(1), None, None, None),
     )
-    @mock.patch(f"{ACQUISITION_PATH}.get_botorch_objective")
+    @mock.patch(f"{ACQUISITION_PATH}.get_botorch_objective_and_transform")
     @mock.patch(
         f"{CURRENT_PATH}.Acquisition.compute_model_dependencies",
         return_value={"current_value": 1.2},
@@ -122,7 +122,7 @@ class AcquisitionTest(TestCase):
         self,
         mock_botorch_acqf_class,
         mock_compute_model_deps,
-        mock_get_objective,
+        mock_get_objective_and_transform,
         mock_subset_model,
         mock_get_X,
     ):
@@ -134,7 +134,7 @@ class AcquisitionTest(TestCase):
             )
 
         botorch_objective = LinearMCObjective(weights=torch.tensor([1.0]))
-        mock_get_objective.return_value = botorch_objective
+        mock_get_objective_and_transform.return_value = (botorch_objective, None)
         mock_get_X.return_value = (self.pending_observations[0], self.X[:1])
         acquisition = Acquisition(
             surrogate=self.surrogate,
@@ -167,7 +167,7 @@ class AcquisitionTest(TestCase):
             objective_thresholds=self.objective_thresholds,
         )
         mock_subset_model.reset_mock()
-        mock_get_objective.reset_mock()
+        mock_get_objective_and_transform.reset_mock()
         self.mock_input_constructor.reset_mock()
         mock_botorch_acqf_class.reset_mock()
         self.options[Keys.SUBSET_MODEL] = False
@@ -183,14 +183,13 @@ class AcquisitionTest(TestCase):
             options=self.options,
         )
         mock_subset_model.assert_not_called()
-        # Check `get_botorch_objective` kwargs
-        mock_get_objective.assert_called_once()
-        _, ckwargs = mock_get_objective.call_args
+        # Check `get_botorch_objective_and_transform` kwargs
+        mock_get_objective_and_transform.assert_called_once()
+        _, ckwargs = mock_get_objective_and_transform.call_args
         self.assertIs(ckwargs["model"], self.acquisition.surrogate.model)
         self.assertIs(ckwargs["objective_weights"], self.objective_weights)
         self.assertIs(ckwargs["outcome_constraints"], self.outcome_constraints)
         self.assertTrue(torch.equal(ckwargs["X_observed"], self.X[:1]))
-        self.assertFalse(ckwargs["use_scalarized_objective"])
         # Check final `acqf` creation
         model_deps = {Keys.CURRENT_VALUE: 1.2}
         self.mock_input_constructor.assert_called_once()

commit de8e8051742d42f65ec525af4b3cab3435f9347f

Fix test failures introduced by #803

ax/models/torch/utils.py
@@ -20,11 +20,7 @@ from botorch.acquisition.analytic import PosteriorMean
 from botorch.acquisition.fixed_feature import FixedFeatureAcquisitionFunction
 from botorch.acquisition.monte_carlo import qSimpleRegret
 from botorch.acquisition.multi_objective.objective import WeightedMCMultiOutputObjective
-from botorch.acquisition.objective import (
-    ConstrainedMCObjective,
-    MCAcquisitionObjective,
-    ScalarizedObjective,
-)
+from botorch.acquisition.objective import ConstrainedMCObjective, MCAcquisitionObjective
 from botorch.acquisition.objective import (
     PosteriorTransform,
     ScalarizedPosteriorTransform,

ax/models/tests/test_botorch_kg.py
@@ -17,7 +17,7 @@ from botorch.acquisition.knowledge_gradient import qMultiFidelityKnowledgeGradie
 from botorch.acquisition.monte_carlo import qSimpleRegret
 from botorch.acquisition.objective import (
     LinearMCObjective,
-    ScalarizedObjective,
+    ScalarizedPosteriorTransform,
 )
 from botorch.exceptions.errors import UnsupportedError
 from botorch.models.transforms.input import Warp
@@ -125,8 +125,8 @@ class KnowledgeGradientTest(TestCase):
             )
             mock_warmstart_initialization.assert_called_once()
 
-        obj = ScalarizedObjective(weights=self.objective_weights)
-        dummy_acq = PosteriorMean(model=model.model, objective=obj)
+        posterior_tf = ScalarizedPosteriorTransform(weights=self.objective_weights)
+        dummy_acq = PosteriorMean(model=model.model, posterior_transform=posterior_tf)
         with mock.patch(
             "ax.models.torch.utils.PosteriorMean", return_value=dummy_acq
         ) as mock_posterior_mean:
@@ -314,28 +314,39 @@ class KnowledgeGradientTest(TestCase):
         )
 
         # test _instantiate_KG
-        objective = ScalarizedObjective(weights=self.objective_weights)
+        posterior_tf = ScalarizedPosteriorTransform(weights=self.objective_weights)
 
         # test acquisition setting
         acq_function = _instantiate_KG(
-            model=model.model, objective=objective, n_fantasies=10, qmc=True
+            model=model.model,
+            posterior_transform=posterior_tf,
+            n_fantasies=10,
+            qmc=True,
         )
         self.assertIsInstance(acq_function.sampler, SobolQMCNormalSampler)
-        self.assertIsInstance(acq_function.objective, ScalarizedObjective)
+        self.assertIsInstance(
+            acq_function.posterior_transform, ScalarizedPosteriorTransform
+        )
         self.assertEqual(acq_function.num_fantasies, 10)
 
         acq_function = _instantiate_KG(
-            model=model.model, objective=objective, n_fantasies=10, qmc=False
+            model=model.model,
+            posterior_transform=posterior_tf,
+            n_fantasies=10,
+            qmc=False,
         )
         self.assertIsInstance(acq_function.sampler, IIDNormalSampler)
 
         acq_function = _instantiate_KG(
-            model=model.model, objective=objective, qmc=False
+            model=model.model, posterior_transform=posterior_tf, qmc=False
         )
         self.assertIsNone(acq_function.inner_sampler)
 
         acq_function = _instantiate_KG(
-            model=model.model, objective=objective, qmc=True, X_pending=self.X_dummy
+            model=model.model,
+            posterior_transform=posterior_tf,
+            qmc=True,
+            X_pending=self.X_dummy,
         )
         self.assertIsNone(acq_function.inner_sampler)
         self.assertTrue(torch.equal(acq_function.X_pending, self.X_dummy))
@@ -384,7 +395,7 @@ class KnowledgeGradientTest(TestCase):
 
         acq_function = _instantiate_KG(
             model=model.model,
-            objective=objective,
+            posterior_transform=posterior_tf,
             target_fidelities={2: 1.0},
             current_value=0,
         )
@@ -400,7 +411,7 @@ class KnowledgeGradientTest(TestCase):
         with self.assertRaises(RuntimeError):
             _instantiate_KG(
                 model=model.model,
-                objective=objective,
+                posterior_transform=posterior_tf,
                 target_fidelities={1: 1.0},
                 fidelity_weights={2: 1.0},
                 current_value=0,