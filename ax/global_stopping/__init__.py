3c7f456d82d35d1af160df5af73cc3dff06441c9
Abbas Kazerouni <kazerouni@fb.com>
2022-03-11 13:58:59 -0800

    AxGlobalStopping
    Summary: This implements the interface as well as a simple global stopping strategy to be used by Ax service and loop API's.
    
    Reviewed By: Balandat
    
    Differential Revision: D34691150
    
    fbshipit-source-id: 148272d7dccc659309f0047cdd21fb6ce2c0f458

---
+++ ax/global_stopping/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from ax.global_stopping import strategies
+
+__all__ = ["strategies"]
---
+++ ax/global_stopping/strategies/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ax.global_stopping.strategies.base import BaseGlobalStoppingStrategy
+
+
+__all__ = [
+    "BaseGlobalStoppingStrategy",
+]
