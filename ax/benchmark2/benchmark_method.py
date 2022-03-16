a116a537de5e597f179b9543a4f2695a02f00067
Miles Olson <mpolson64@fb.com>
2022-03-16 07:39:33 -0700

    BenchmarkMethod dataclass of (GenerationStrategy, SchedulerOptions)
    Summary: Represent a method in benchmarking as (GenerationStrategy, SchedulerOptions) instead of just a generation strategy as we previously did. This will allow us to test things like early stopping, global stopping, and changes to parallelism more effectively in the new suite.
    
    Differential Revision: D34828763
    
    fbshipit-source-id: c1f579b81f147daa8880e2e8bc852d8f19ae3584

---
+++ ax/benchmark2/benchmark_method.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dataclasses import dataclass
+
+from ax.exceptions.core import UserInputError
+from ax.modelbridge.generation_strategy import GenerationStrategy
+from ax.service.scheduler import SchedulerOptions
+
+
+@dataclass(frozen=True)
+class BenchmarkMethod:
+    """Benchmark method, represented in terms of Ax generation strategy (which tells us
+    which models to use when) and scheduler options (which tell us extra execution
+    information like maximum parallelism, early stopping configuration, etc.)
+    """
+
+    generation_strategy: GenerationStrategy
+    scheduler_options: SchedulerOptions
+
+    def __post_init__(self) -> None:
+        if self.scheduler_options.total_trials is None:
+            raise UserInputError(
+                "SchedulerOptions.total_trials may not be None in BenchmarkMethod."
+            )
---
+++ ax/benchmark2/tests/test_benchmark_method.py
@@ -0,0 +1,46 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ax.benchmark2.benchmark_method import BenchmarkMethod
+from ax.exceptions.core import UserInputError
+from ax.modelbridge.generation_strategy import GenerationStrategy, GenerationStep
+from ax.modelbridge.registry import Models
+from ax.service.scheduler import SchedulerOptions
+from ax.utils.common.testutils import TestCase
+
+
+class TestBenchmarkMethod(TestCase):
+    def test_benchmark_method(self):
+        gs = GenerationStrategy(
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=10,
+                )
+            ],
+            name="SOBOL",
+        )
+        options = SchedulerOptions(total_trials=10)
+        method = BenchmarkMethod(generation_strategy=gs, scheduler_options=options)
+
+        self.assertEqual(method.generation_strategy, gs)
+        self.assertEqual(method.scheduler_options, options)
+
+    def test_total_trials_none(self):
+        gs = GenerationStrategy(
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=10,
+                )
+            ],
+            name="SOBOL",
+        )
+        options = SchedulerOptions()
+
+        with self.assertRaisesRegex(
+            UserInputError, "SchedulerOptions.total_trials may not be None"
+        ):
+            BenchmarkMethod(generation_strategy=gs, scheduler_options=options)
