5bd94e3343d0a2d07b77fbab56e66f21d08d4378
Daniel Cohen <danielcohennyc@fb.com>
2022-03-22 07:41:07 -0700

    Return AxPlotConfigs instead of rendering in PTSClient and PTSAnalyzer (#859)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/859
    
    I didn't alter the contract in qe_lib's QuickBO analyzer because it currently has a tutorial and we'll be rewriting it soon
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D34932892
    
    fbshipit-source-id: 3714f2f85e3c42ec1514151f6134f4ab5904922a

--- ax/service/tests/test_ax_client.py
+++ ax/service/tests/test_ax_client.py
@@ -8,8 +8,8 @@ import math
 import sys
 import time
 from math import ceil
-from typing import Any, List, Tuple, Optional
-from unittest.mock import Mock, patch
+from typing import List, Tuple, Optional
+from unittest.mock import patch
 
 import numpy as np
 import torch
@@ -1691,34 +1691,6 @@ class TestAxClient(TestCase):
         )
 
         self.assertGreaterEqual(ax_client.get_hypervolume(), 0)
-
-    @patch(
-        "ax.service.ax_client.render"
-    )  # it will error if it tries to render the mock plot
-    @patch(
-        "ax.service.ax_client.plot_pareto_frontier"
-    )  # it will error if it tries to plot the mock
-    @patch("ax.service.ax_client.compute_posterior_pareto_frontier")
-    def test_view_pareto_frontier_plot(self, mock_plot: Mock, *args: Any) -> None:
-        primary_metric_name, secondary_metric_name = "branin", "currin"
-        ax_client, branin_currin = get_branin_currin_optimization_with_N_sobol_trials(
-            num_trials=20
-        )
-        ax_client.view_pareto_frontier_plot(
-            primary_metric_name=primary_metric_name,
-            secondary_metric_name=secondary_metric_name,
-        )
-        kwargs = mock_plot.mock_calls[0][2]
-        self.assertEqual(
-            kwargs["experiment"],
-            ax_client.experiment,
-        )
-        self.assertEqual(
-            kwargs["data"],
-            ax_client.experiment.fetch_data(),
-        )
-        self.assertEqual(kwargs["primary_objective"].name, primary_metric_name)
-        self.assertEqual(kwargs["secondary_objective"].name, secondary_metric_name)
 
     def test_with_hss(self):
         ax_client = AxClient()
