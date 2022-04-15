3ee648c426e7d0724d3dca958c5225b4a6df37ec
Daniel Cohen <danielcohennyc@fb.com>
2022-04-15 13:43:30 -0700

    Fix logic for `testInitFiles` to only assert on python modules with python files (#915)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/915
    
    as per title
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D35683998
    
    fbshipit-source-id: e7530492f2f4e8464c8172ed9eda664661a92de8

--- ax/utils/testing/test_init_files.py
+++ ax/utils/testing/test_init_files.py
@@ -5,6 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
+from glob import glob
 
 from ax.utils.common.testutils import TestCase
 
@@ -13,7 +14,8 @@ class InitTest(TestCase):
     def testInitFiles(self) -> None:
         """__init__.py files are necessary when not using buck targets"""
         for root, _dirs, files in os.walk("./ax/ax", topdown=False):
-            self.assertTrue(
-                "__init__.py" in files,
-                "directory " + root + " does not contain a .__init__.py file",
-            )
+            if len(glob(f"{root}/**/*.py", recursive=True)) > 0:
+                self.assertTrue(
+                    "__init__.py" in files,
+                    "directory " + root + " does not contain a .__init__.py file",
+                )
