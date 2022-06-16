69935c55be49e75cbbbe539b11df25d5ece2ac69
Sait Cakmak <saitcakmak@fb.com>
2022-06-16 13:28:52 -0700

    Additional acquisition functions for nightly benchmark
    Summary: Adds a few single-objective acquisition functions (that work out-of-box) to the nightly benchmark run.
    
    Reviewed By: Balandat
    
    Differential Revision: D37087388
    
    fbshipit-source-id: c147487c4c4c0663a4f0324bd635b67fc88454f9

--- ax/benchmark/methods/modular_botorch.py
+++ ax/benchmark/methods/modular_botorch.py
@@ -3,12 +3,15 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from typing import Any, Dict, Optional, Type
+
 from ax.benchmark.benchmark_method import BenchmarkMethod
 from ax.modelbridge.generation_strategy import GenerationStep, GenerationStrategy
 from ax.modelbridge.registry import Models
 from ax.models.torch.botorch_modular.surrogate import Surrogate
 from ax.service.scheduler import SchedulerOptions
 from ax.utils.common.constants import Keys
+from botorch.acquisition.acquisition import AcquisitionFunction
 from botorch.acquisition.monte_carlo import qNoisyExpectedImprovement
 from botorch.acquisition.multi_objective.monte_carlo import (
     qNoisyExpectedHypervolumeImprovement,
@@ -16,7 +19,9 @@ from botorch.acquisition.multi_objective.monte_carlo import (
 from botorch.models.gp_regression import FixedNoiseGP
 
 
-def get_sobol_botorch_modular_fixed_noise_gp_qnei() -> BenchmarkMethod:
+def get_sobol_botorch_modular_fixed_noise_gp_qnei(
+    total_trials: int = 30,
+) -> BenchmarkMethod:
     model_gen_kwargs = {
         "model_gen_options": {
             Keys.OPTIMIZER_KWARGS: {
@@ -48,7 +53,7 @@ def get_sobol_botorch_modular_fixed_noise_gp_qnei() -> BenchmarkMethod:
         ],
     )
 
-    scheduler_options = SchedulerOptions(total_trials=30)
+    scheduler_options = SchedulerOptions(total_trials=total_trials)
 
     return BenchmarkMethod(
         name=generation_strategy.name,
@@ -57,7 +62,9 @@ def get_sobol_botorch_modular_fixed_noise_gp_qnei() -> BenchmarkMethod:
     )
 
 
-def get_sobol_botorch_modular_fixed_noise_gp_qnehvi() -> BenchmarkMethod:
+def get_sobol_botorch_modular_fixed_noise_gp_qnehvi(
+    total_trials: int = 30,
+) -> BenchmarkMethod:
     model_gen_kwargs = {
         "model_gen_options": {
             Keys.OPTIMIZER_KWARGS: {
@@ -89,7 +96,7 @@ def get_sobol_botorch_modular_fixed_noise_gp_qnehvi() -> BenchmarkMethod:
         ],
     )
 
-    scheduler_options = SchedulerOptions(total_trials=30)
+    scheduler_options = SchedulerOptions(total_trials=total_trials)
 
     return BenchmarkMethod(
         name=generation_strategy.name,
@@ -98,7 +105,9 @@ def get_sobol_botorch_modular_fixed_noise_gp_qnehvi() -> BenchmarkMethod:
     )
 
 
-def get_sobol_botorch_modular_default() -> BenchmarkMethod:
+def get_sobol_botorch_modular_default(
+    total_trials: int = 30,
+) -> BenchmarkMethod:
     generation_strategy = GenerationStrategy(
         name="SOBOL+BOTORCH_MODULAR::default",
         steps=[
@@ -111,7 +120,37 @@ def get_sobol_botorch_modular_default() -> BenchmarkMethod:
         ],
     )
 
-    scheduler_options = SchedulerOptions(total_trials=30)
+    scheduler_options = SchedulerOptions(total_trials=total_trials)
+
+    return BenchmarkMethod(
+        name=generation_strategy.name,
+        generation_strategy=generation_strategy,
+        scheduler_options=scheduler_options,
+    )
+
+
+def get_sobol_botorch_modular_acquisition(
+    acquisition_cls: Type[AcquisitionFunction],
+    acquisition_options: Optional[Dict[str, Any]] = None,
+    total_trials: int = 30,
+) -> BenchmarkMethod:
+    generation_strategy = GenerationStrategy(
+        name=f"SOBOL+BOTORCH_MODULAR::{acquisition_cls.__name__}",
+        steps=[
+            GenerationStep(model=Models.SOBOL, num_trials=5, min_trials_observed=3),
+            GenerationStep(
+                model=Models.BOTORCH_MODULAR,
+                num_trials=-1,
+                max_parallelism=1,
+                model_kwargs={
+                    "botorch_acqf_class": acquisition_cls,
+                    "acquisition_options": acquisition_options,
+                },
+            ),
+        ],
+    )
+
+    scheduler_options = SchedulerOptions(total_trials=total_trials)
 
     return BenchmarkMethod(
         name=generation_strategy.name,
---
+++ ax/benchmark/tests/test_methods.py
@@ -0,0 +1,27 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from ax.benchmark.methods.modular_botorch import get_sobol_botorch_modular_acquisition
+from ax.modelbridge.registry import Models
+from ax.utils.common.testutils import TestCase
+from botorch.acquisition.knowledge_gradient import qKnowledgeGradient
+
+
+class TestMethods(TestCase):
+    def test_mbm_acquisition(self):
+        method = get_sobol_botorch_modular_acquisition(
+            acquisition_cls=qKnowledgeGradient,
+            acquisition_options={"num_fantasies": 16},
+            total_trials=50,
+        )
+        self.assertEqual(method.scheduler_options.total_trials, 50)
+        self.assertEqual(method.name, "SOBOL+BOTORCH_MODULAR::qKnowledgeGradient")
+        gs = method.generation_strategy
+        sobol, kg = gs._steps
+        self.assertEqual(sobol.num_trials, 5)
+        self.assertEqual(kg.model, Models.BOTORCH_MODULAR)
+        model_kwargs = kg.model_kwargs
+        self.assertEqual(model_kwargs["botorch_acqf_class"], qKnowledgeGradient)
+        self.assertEqual(model_kwargs["acquisition_options"], {"num_fantasies": 16})
