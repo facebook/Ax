83cbfcba61dc33fd4e4a1543fa192b66a7150d6d
Lena Kashtelyan <drfreund@fb.com>
2022-07-19 08:19:43 -0700

    Clean up MNIST after test
    Reviewed By: mpolson64
    
    Differential Revision: D37941736
    
    fbshipit-source-id: 4f616e71096cd8038116af4aa197aa04c3e6565a

--- ax/benchmark/tests/test_problem_storage.py
+++ ax/benchmark/tests/test_problem_storage.py
@@ -3,6 +3,9 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import glob
+import os
+
 from ax.benchmark.problems.hpo.torchvision import PyTorchCNNTorchvisionBenchmarkProblem
 from ax.storage.json_store.decoder import object_from_json
 from ax.storage.json_store.encoder import object_to_json
@@ -10,6 +13,14 @@ from ax.utils.common.testutils import TestCase
 
 
 class TestProblems(TestCase):
+    def tearDown(self):
+        mnist_dir_path = "data/MNIST/raw"
+        files = glob.glob(mnist_dir_path + "/*")
+        for f in files:
+            os.remove(f)
+        os.rmdir(mnist_dir_path)
+        super().tearDown()
+
     def test_torchvision_encode_decode(self):
         original_object = PyTorchCNNTorchvisionBenchmarkProblem.from_dataset_name(
             name="MNIST", num_trials=50
