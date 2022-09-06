dd849a11e3faef6e558e8b03c1b64a9cf13cc2f6
Max Balandat <balandat@fb.com>
2022-09-05 23:51:19 -0700

    Require py3.8, use gpytorch latest in github CI (#1109)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/1109
    
    Changes to account for the LazyTensor -> LinearOperator move in https://github.com/cornellius-gp/gpytorch/pull/2027
    
    Reviewed By: saitcakmak
    
    Differential Revision: D38998031
    
    fbshipit-source-id: c9eeaca0bf56e151521aac8a22f975dffca19b99

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ def setup_package() -> None:
         ],
         long_description=long_description,
         long_description_content_type="text/markdown",
-        python_requires=">=3.7",
+        python_requires=">=3.8",
         install_requires=REQUIRES,
         packages=find_packages(),
         package_data={
