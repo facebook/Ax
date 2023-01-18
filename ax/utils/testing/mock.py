071089cd36e0ed879c88f2a4676a1c7cd944975e
Sait Cakmak <saitcakmak@meta.com>
2023-01-17 16:43:01 -0800

    Introduce a `skip_fit_gpytorch_mll` for faster testing (#1379)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/1379
    
    Similar to `fast_botorch_optimize`, this can be used to speed up tests by skipping model fitting.
    
    Reviewed By: mpolson64
    
    Differential Revision: D42557155
    
    fbshipit-source-id: 6dd9a4906e2c1652c6d807148b338bb5cfa49470

--- ax/utils/testing/mock.py
+++ ax/utils/testing/mock.py
@@ -101,3 +101,34 @@ def fast_botorch_optimize(f: Callable) -> Callable:
             return f(*args, **kwargs)
 
     return inner
+
+
+@contextmanager
+def skip_fit_gpytorch_mll_context_manager() -> Generator[None, None, None]:
+    """A context manager that makes `fit_gpytorch_mll` a no-op.
+
+    This should only be used to speed up slow tests.
+    """
+    with mock.patch(
+        "botorch.fit.FitGPyTorchMLL", side_effect=lambda *args, **kwargs: args[0]
+    ) as mock_fit:
+        yield
+    if mock_fit.call_count < 1:
+        raise AssertionError(
+            "No mocks were called in the context manager. Please remove unused "
+            "skip_fit_gpytorch_mll_context_manager()."
+        )
+
+
+# pyre-fixme[24]: Generic type `Callable` expects 2 type parameters.
+def skip_fit_gpytorch_mll(f: Callable) -> Callable:
+    """Wraps f in the skip_fit_gpytorch_mll_context_manager for use as a decorator."""
+
+    @wraps(f)
+    # pyre-fixme[3]: Return type must be annotated.
+    # pyre-fixme[2]: Parameter must be annotated.
+    def inner(*args, **kwargs):
+        with skip_fit_gpytorch_mll_context_manager():
+            return f(*args, **kwargs)
+
+    return inner
