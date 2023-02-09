7960fbd344acef90f5f6426108d9a4574fd1b9a5
Sait Cakmak <saitcakmak@meta.com>
2023-02-08 18:02:20 -0800

    Skip MCMC model fitting in fast_botorch_optimize (#1410)
    Summary: Pull Request resolved: https://github.com/facebook/Ax/pull/1410
    
    Reviewed By: Balandat
    
    Differential Revision: D43138348
    
    fbshipit-source-id: 6019446b6c3f061806be28eab3efd760cdba92d3

--- ax/utils/testing/mock.py
+++ ax/utils/testing/mock.py
@@ -5,15 +5,18 @@
 
 from contextlib import contextmanager, ExitStack
 from functools import wraps
-from typing import Callable, Generator
+from typing import Any, Callable, Dict, Generator, Optional
 from unittest import mock
 
+from ax.models.torch.fully_bayesian import run_inference
+from botorch.fit import fit_fully_bayesian_model_nuts
 from botorch.generation.gen import minimize_with_timeout
-
 from botorch.optim.initializers import (
     gen_batch_initial_conditions,
     gen_one_shot_kg_initial_conditions,
 )
+from scipy.optimize.optimize import OptimizeResult
+from torch import Tensor
 
 
 @contextmanager
@@ -28,30 +31,30 @@ def fast_botorch_optimize_context_manager(
             USE RESPONSIBLY.
     """
 
-    # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    def one_iteration_minimize(*args, **kwargs):
+    def one_iteration_minimize(*args: Any, **kwargs: Any) -> OptimizeResult:
         if kwargs["options"] is None:
             kwargs["options"] = {}
 
         kwargs["options"]["maxiter"] = 1
         return minimize_with_timeout(*args, **kwargs)
 
-    # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    def minimal_gen_ics(*args, **kwargs):
+    def minimal_gen_ics(*args: Any, **kwargs: Any) -> Tensor:
         kwargs["num_restarts"] = 2
         kwargs["raw_samples"] = 4
 
         return gen_batch_initial_conditions(*args, **kwargs)
 
-    # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    def minimal_gen_os_ics(*args, **kwargs):
+    def minimal_gen_os_ics(*args: Any, **kwargs: Any) -> Optional[Tensor]:
         kwargs["num_restarts"] = 2
         kwargs["raw_samples"] = 4
 
         return gen_one_shot_kg_initial_conditions(*args, **kwargs)
+
+    def minimal_run_inference(*args: Any, **kwargs: Any) -> Dict[str, Tensor]:
+        return run_inference(*args, **_get_minimal_mcmc_kwargs(**kwargs))
+
+    def minimal_fit_fully_bayesian(*args: Any, **kwargs: Any) -> None:
+        fit_fully_bayesian_model_nuts(*args, **_get_minimal_mcmc_kwargs(**kwargs))
 
     with ExitStack() as es:
         mock_generation = es.enter_context(
@@ -82,11 +85,32 @@ def fast_botorch_optimize_context_manager(
             )
         )
 
+        mock_mcmc_legacy = es.enter_context(
+            mock.patch(
+                "ax.models.torch.fully_bayesian.run_inference",
+                wraps=minimal_run_inference,
+            )
+        )
+
+        mock_mcmc_mbm = es.enter_context(
+            mock.patch(
+                "ax.models.torch.botorch_modular.utils.fit_fully_bayesian_model_nuts",
+                wraps=minimal_fit_fully_bayesian,
+            )
+        )
+
         yield
 
     if (not force) and all(
         mock_.call_count < 1
-        for mock_ in [mock_generation, mock_fit, mock_gen_ics, mock_gen_os_ics]
+        for mock_ in [
+            mock_generation,
+            mock_fit,
+            mock_gen_ics,
+            mock_gen_os_ics,
+            mock_mcmc_legacy,
+            mock_mcmc_mbm,
+        ]
     ):
         raise AssertionError(
             "No mocks were called in the context manager. Please remove unused "
@@ -100,8 +124,7 @@ def fast_botorch_optimize(f: Callable) -> Callable:
 
     @wraps(f)
     # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    def inner(*args, **kwargs):
+    def inner(*args: Any, **kwargs: Any):
         with fast_botorch_optimize_context_manager():
             return f(*args, **kwargs)
 
@@ -131,9 +154,16 @@ def skip_fit_gpytorch_mll(f: Callable) -> Callable:
 
     @wraps(f)
     # pyre-fixme[3]: Return type must be annotated.
-    # pyre-fixme[2]: Parameter must be annotated.
-    def inner(*args, **kwargs):
+    def inner(*args: Any, **kwargs: Any):
         with skip_fit_gpytorch_mll_context_manager():
             return f(*args, **kwargs)
 
     return inner
+
+
+def _get_minimal_mcmc_kwargs(**kwargs: Any) -> Dict[str, Any]:
+    kwargs["warmup_steps"] = 0
+    # Just get as many samples as otherwise expected.
+    kwargs["num_samples"] = kwargs.get("num_samples", 256) // kwargs.get("thinning", 16)
+    kwargs["thinning"] = 1
+    return kwargs
