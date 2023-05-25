9153723bfdb00781ea722dcfce9631b1ff558c29
Miles Olson <mpolson64@fb.com>
2023-05-25 16:08:49 -0700

    AxClient Telemetry
    Summary: Minor refactor to telemetry setup to allow for AxClient creation and completion records, as well as optimization created and completed records FROM AxClients.
    
    Differential Revision: https://internalfb.com/D45961113
    
    fbshipit-source-id: 3f18e6cfbeb76bf72c6deabf9594be6be7602feb

--- ax/telemetry/tests/test_optimization.py
+++ ax/telemetry/tests/test_optimization.py
@@ -4,7 +4,11 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+from dataclasses import asdict
+
+from ax.service.ax_client import AxClient, ObjectiveProperties
 from ax.service.scheduler import Scheduler, SchedulerOptions
+from ax.telemetry.ax_client import AxClientCompletedRecord, AxClientCreatedRecord
 from ax.telemetry.optimization import (
     OptimizationCompletedRecord,
     OptimizationCreatedRecord,
@@ -16,7 +20,7 @@ from ax.utils.testing.modeling_stubs import get_generation_strategy
 
 
 class TestOptimization(TestCase):
-    def test_optimization_created_record_flatten(self) -> None:
+    def test_optimization_created_record_from_scheduler(self) -> None:
         scheduler = Scheduler(
             experiment=get_branin_experiment(),
             generation_strategy=get_generation_strategy(),
@@ -27,10 +31,8 @@ class TestOptimization(TestCase):
             ),
         )
 
-        record = OptimizationCreatedRecord(
-            scheduler_created_record=SchedulerCreatedRecord.from_scheduler(
-                scheduler=scheduler
-            ),
+        record = OptimizationCreatedRecord.from_scheduler(
+            scheduler=scheduler,
             product_surface="Axolotl",
             launch_surface="web",
             deployed_job_id=1118,
@@ -40,7 +42,6 @@ class TestOptimization(TestCase):
             num_custom_trials=0,
         )
 
-        flat = record.flatten()
         expected_dict = {
             **SchedulerCreatedRecord.from_scheduler(scheduler=scheduler).flatten(),
             "product_surface": "Axolotl",
@@ -52,9 +53,48 @@ class TestOptimization(TestCase):
             "num_custom_trials": 0,
         }
 
-        self.assertEqual(flat, expected_dict)
+        self.assertEqual(asdict(record), expected_dict)
 
-    def test_optimization_completed_record_flatten(self) -> None:
+    def test_optimization_created_record_from_ax_client(self) -> None:
+        ax_client = AxClient()
+        ax_client.create_experiment(
+            name="test_experiment",
+            parameters=[
+                {"name": "x", "type": "range", "bounds": [-5.0, 10.0]},
+                {"name": "y", "type": "range", "bounds": [0.0, 15.0]},
+            ],
+            objectives={"branin": ObjectiveProperties(minimize=True)},
+            is_test=True,
+        )
+
+        record = OptimizationCreatedRecord.from_ax_client(
+            ax_client=ax_client,
+            product_surface="Axolotl",
+            launch_surface="web",
+            deployed_job_id=1118,
+            trial_evaluation_identifier="train",
+            is_manual_generation_strategy=True,
+            warm_started_from=None,
+            num_custom_trials=0,
+        )
+
+        expected_dict = {
+            **AxClientCreatedRecord.from_ax_client(ax_client=ax_client).flatten(),
+            "product_surface": "Axolotl",
+            "launch_surface": "web",
+            "deployed_job_id": 1118,
+            "trial_evaluation_identifier": "train",
+            "is_manual_generation_strategy": True,
+            "warm_started_from": None,
+            "num_custom_trials": 0,
+            # Extra fields
+            "scheduler_max_pending_trials": -1,
+            "scheduler_total_trials": None,
+        }
+
+        self.assertEqual(asdict(record), expected_dict)
+
+    def test_optimization_completed_record_from_scheduler(self) -> None:
         scheduler = Scheduler(
             experiment=get_branin_experiment(),
             generation_strategy=get_generation_strategy(),
@@ -65,16 +105,13 @@ class TestOptimization(TestCase):
             ),
         )
 
-        record = OptimizationCompletedRecord(
-            scheduler_completed_record=SchedulerCompletedRecord.from_scheduler(
-                scheduler=scheduler
-            ),
+        record = OptimizationCompletedRecord.from_scheduler(
+            scheduler=scheduler,
             deployed_job_id=1118,
             estimated_early_stopping_savings=19,
             estimated_global_stopping_savings=98,
         )
 
-        flat = record.flatten()
         expected_dict = {
             **SchedulerCompletedRecord.from_scheduler(scheduler=scheduler).flatten(),
             "deployed_job_id": 1118,
@@ -82,4 +119,35 @@ class TestOptimization(TestCase):
             "estimated_global_stopping_savings": 98,
         }
 
-        self.assertEqual(flat, expected_dict)
+        self.assertEqual(asdict(record), expected_dict)
+
+    def test_optimization_completed_record_from_ax_client(self) -> None:
+        ax_client = AxClient()
+        ax_client.create_experiment(
+            name="test_experiment",
+            parameters=[
+                {"name": "x", "type": "range", "bounds": [-5.0, 10.0]},
+                {"name": "y", "type": "range", "bounds": [0.0, 15.0]},
+            ],
+            objectives={"branin": ObjectiveProperties(minimize=True)},
+            is_test=True,
+        )
+
+        record = OptimizationCompletedRecord.from_ax_client(
+            ax_client=ax_client,
+            deployed_job_id=1118,
+            estimated_early_stopping_savings=19,
+            estimated_global_stopping_savings=98,
+        )
+
+        expected_dict = {
+            **AxClientCompletedRecord.from_ax_client(ax_client=ax_client).flatten(),
+            "deployed_job_id": 1118,
+            "estimated_early_stopping_savings": 19,
+            "estimated_global_stopping_savings": 98,
+            # Extra fields
+            "num_metric_fetch_e_encountered": -1,
+            "num_trials_bad_due_to_err": -1,
+        }
+
+        self.assertEqual(asdict(record), expected_dict)
