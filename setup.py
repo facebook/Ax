847b54c329cacc61282c22750f50cc3ec35321d0
Sait Cakmak <saitcakmak@outlook.com>
2023-07-25 16:30:38 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ def setup_package() -> None:
         ],
         long_description=long_description,
         long_description_content_type="text/markdown",
-        python_requires=">=3.8",
+        python_requires=">=3.9",
         install_requires=REQUIRES,
         packages=find_packages(),
         package_data={
