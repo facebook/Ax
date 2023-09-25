4a14425db232fda9cc90a0dbddba89c03d7edd57
Eric Onofrey <eonofrey@meta.com>
2023-09-25 13:18:57 -0700

    Expose reason for trial failure in AxSweep outputs (#1868)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/1868
    
    Update ax storage to include 'failed_reason' column
    
    Reviewed By: mpolson64
    
    Differential Revision: D48992271
    
    fbshipit-source-id: 2d2e508648d628bd397941dc042494aa07abf4a2

--- ax/storage/sqa_store/decoder.py
+++ ax/storage/sqa_store/decoder.py
@@ -890,6 +890,7 @@ class Decoder:
         trial._time_staged = trial_sqa.time_staged
         trial._time_run_started = trial_sqa.time_run_started
         trial._abandoned_reason = trial_sqa.abandoned_reason
+        trial._failed_reason = trial_sqa.failed_reason
         # pyre-fixme[9]: _run_metadata has type `Dict[str, Any]`; used as
         #  `Optional[Dict[str, Any]]`.
         # pyre-fixme[8]: Attribute has type `Dict[str, typing.Any]`; used as
