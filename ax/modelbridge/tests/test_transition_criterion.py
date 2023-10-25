d5b423fc7db4d57512818f5340948d703981b3a5
Mia Garrard <mgarrard@meta.com>
2023-10-25 09:51:25 -0700

    Fix TransitionCriterion to be based on Trials from a specific node instead of entire experiment (#1928)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/1928
    
    During the dev process we noticed that the current implementation of completion criteria relied on full experiment trials, instead we only want to pass transition criteria for Max and Min based on trials that were generated from the current node. This updates those criterion is_met methods to follow this logic.
    
    We also add unit tests and repr method for printing the classes, this helps with debugging
    
    Things in the pipeline:
    (1) Use the transition criterion to determine if a node is complete
    (2) add is_complete to generationNode and then use that in generation Strategy for moving forward
    (3) [Mby] skip max trial criterion addition if numtrials == -1
    (4) add transition criterion to the repr string + some of the other fields that havent made it yet on GeneratinoNode
    (5) Do a final pass of the generationStrategy/GenerationNode files to see what else can be migrated/condensed
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D50295579
    
    fbshipit-source-id: 2a29726a0c1b3043a6773ce6784104670417c6e4

--- ax/modelbridge/tests/test_transition_criterion.py
+++ ax/modelbridge/tests/test_transition_criterion.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+import logging
+from logging import Logger
 from unittest.mock import patch
 
 import pandas as pd
@@ -14,9 +16,13 @@ from ax.modelbridge.transition_criterion import (
     MaxTrials,
     MinimumPreferenceOccurances,
     MinimumTrialsInStatus,
+    TransitionCriterion,
 )
+from ax.utils.common.logger import get_logger
 from ax.utils.common.testutils import TestCase
-from ax.utils.testing.core_stubs import get_experiment
+from ax.utils.testing.core_stubs import get_branin_experiment, get_experiment
+
+logger: Logger = get_logger(__name__)
 
 
 class TestTransitionCriterion(TestCase):
@@ -25,7 +31,7 @@ class TestTransitionCriterion(TestCase):
         criterion = MinimumPreferenceOccurances(metric_name="m1", threshold=3)
         experiment = get_experiment()
         generation_strategy = GenerationStrategy(
-            name="SOBOL+GPEI::default",
+            name="SOBOL::default",
             steps=[
                 GenerationStep(
                     model=Models.SOBOL,
@@ -84,13 +90,13 @@ class TestTransitionCriterion(TestCase):
                 GenerationStep(
                     model=Models.SOBOL,
                     num_trials=3,
-                    enforce_num_trials=False,
                 ),
                 GenerationStep(
                     model=Models.GPEI,
                     num_trials=4,
                     max_parallelism=1,
                     min_trials_observed=2,
+                    enforce_num_trials=False,
                 ),
                 GenerationStep(
                     model=Models.GPEI,
@@ -102,7 +108,7 @@ class TestTransitionCriterion(TestCase):
         gs.experiment = experiment
 
         step_0_expected_transition_criteria = [
-            MaxTrials(threshold=3, enforce=False, transition_to="GenerationStep_1"),
+            MaxTrials(threshold=3, enforce=True, transition_to="GenerationStep_1"),
             MinimumTrialsInStatus(
                 status=TrialStatus.COMPLETED,
                 threshold=0,
@@ -110,7 +116,7 @@ class TestTransitionCriterion(TestCase):
             ),
         ]
         step_1_expected_transition_criteria = [
-            MaxTrials(threshold=4, enforce=True, transition_to="GenerationStep_2"),
+            MaxTrials(threshold=4, enforce=False, transition_to="GenerationStep_2"),
             MinimumTrialsInStatus(
                 status=TrialStatus.COMPLETED,
                 threshold=2,
@@ -135,11 +141,123 @@ class TestTransitionCriterion(TestCase):
             gs._steps[2].transition_criteria, step_2_expected_transition_criteria
         )
 
-        # Check default results for `is_met` call
-        self.assertTrue(gs._steps[0].transition_criteria[0].is_met(experiment))
-        self.assertTrue(gs._steps[0].transition_criteria[1].is_met(experiment))
-        self.assertFalse(gs._steps[1].transition_criteria[0].is_met(experiment))
-        self.assertFalse(gs._steps[1].transition_criteria[1].is_met(experiment))
+    def test_minimum_trials_in_status_is_met(self) -> None:
+        """Test that the is_met method in MinimumTrialsInStatus works"""
+        experiment = get_branin_experiment()
+        gs = GenerationStrategy(
+            name="SOBOL::default",
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=4,
+                    min_trials_observed=2,
+                    enforce_num_trials=True,
+                ),
+                GenerationStep(
+                    Models.SOBOL,
+                    num_trials=-1,
+                    max_parallelism=1,
+                ),
+            ],
+        )
+        gs.experiment = experiment
+
+        # Need to add trials to test the transition criteria `is_met` method
+        for _i in range(4):
+            experiment.new_trial(gs.gen(experiment=experiment))
+
+        # TODO: @mgarrard More comphrensive test of trials_from_node
+        node_0_trials = gs._steps[0].trials_from_node
+        node_1_trials = gs._steps[1].trials_from_node
+
+        self.assertEqual(len(node_0_trials), 4)
+        self.assertEqual(len(node_1_trials), 0)
+
+        # MinimumTrialsInStatus is met should not pass yet, becasue no trials
+        # are marked completed
+        self.assertFalse(
+            gs._steps[0]
+            .transition_criteria[1]
+            .is_met(experiment, gs._steps[0].trials_from_node)
+        )
+
+        # Should pass after two trials are marked completed
+        for idx, trial in experiment.trials.items():
+            trial.mark_running(no_runner_required=True).mark_completed()
+            if idx == 1:
+                break
+        self.assertTrue(
+            gs._steps[0]
+            .transition_criteria[1]
+            .is_met(experiment, gs._steps[0].trials_from_node)
+        )
+
+    def test_max_trials_is_met(self) -> None:
+        """Test that the is_met method in MaxTrials works"""
+        experiment = get_branin_experiment()
+        gs = GenerationStrategy(
+            name="SOBOL::default",
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=4,
+                    min_trials_observed=0,
+                    enforce_num_trials=True,
+                ),
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=4,
+                    min_trials_observed=0,
+                    enforce_num_trials=False,
+                ),
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=-1,
+                    max_parallelism=1,
+                ),
+            ],
+        )
+        gs.experiment = experiment
+
+        # If enforce_num_trials == False, should automatically pass
+        self.assertTrue(
+            gs._steps[1]
+            .transition_criteria[0]
+            .is_met(
+                experiment=experiment, trials_from_node=gs._steps[1].trials_from_node
+            )
+        )
+
+        # No trials yet, first step should fail
+        self.assertFalse(
+            gs._steps[0]
+            .transition_criteria[0]
+            .is_met(
+                experiment=experiment,
+                trials_from_node=gs._steps[0].trials_from_node,
+            )
+        )
+
+        # After adding trials, should pass
+        for _i in range(4):
+            experiment.new_trial(gs.gen(experiment=experiment))
+        self.assertTrue(
+            gs._steps[0]
+            .transition_criteria[0]
+            .is_met(
+                experiment=experiment,
+                trials_from_node=gs._steps[0].trials_from_node,
+            )
+        )
+
+        # if num_trials == -1, should always pass
+        self.assertTrue(
+            gs._steps[2]
+            .transition_criteria[0]
+            .is_met(
+                experiment=experiment, trials_from_node=gs._steps[2].trials_from_node
+            )
+        )
 
     def test_max_trials_status_arg(self) -> None:
         """Tests the `only_in_status` argument checks the threshold based on the
@@ -150,4 +268,101 @@ class TestTransitionCriterion(TestCase):
         criterion = MaxTrials(
             threshold=5, only_in_status=TrialStatus.RUNNING, enforce=True
         )
-        self.assertFalse(criterion.is_met(experiment))
+        self.assertFalse(criterion.is_met(experiment, trials_from_node={2, 3}))
+
+    def test_trials_from_node_none(self) -> None:
+        """Tests MinimumTrialsInStatus and MaxTrials default to experiment
+        level trials when trials_from_node is None.
+        """
+        # TODO: @mgarrard replace with assertion checks that `trials_from_node`
+        # cannot be none
+        experiment = get_experiment()
+        gs = GenerationStrategy(
+            name="SOBOL::default",
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,
+                    num_trials=4,
+                    min_trials_observed=2,
+                    enforce_num_trials=True,
+                ),
+            ],
+        )
+        max_criterion_with_status = MaxTrials(
+            threshold=2, enforce=True, only_in_status=TrialStatus.COMPLETED
+        )
+        max_criterion = MaxTrials(threshold=2, enforce=True)
+        warning_msg = (
+            "trials_from_node is None, will check threshold on experiment level"
+        )
+
+        # no trials so criterion should be false, then add trials to pass criterion
+        with self.assertLogs(TransitionCriterion.__module__, logging.WARNING) as logger:
+            self.assertFalse(max_criterion.is_met(experiment, trials_from_node=None))
+            self.assertTrue(
+                any(warning_msg in output for output in logger.output),
+                logger.output,
+            )
+        for _i in range(3):
+            experiment.new_trial(gs.gen(experiment=experiment))
+        self.assertTrue(max_criterion.is_met(experiment, trials_from_node=None))
+
+        # Before marking trial status it should be false, until trials are completed
+        self.assertFalse(
+            max_criterion_with_status.is_met(experiment, trials_from_node=None)
+        )
+        for idx, trial in experiment.trials.items():
+            trial._status = TrialStatus.COMPLETED
+            if idx == 1:
+                break
+        self.assertTrue(
+            max_criterion_with_status.is_met(experiment, trials_from_node=None)
+        )
+
+        # Check MinimumTrialsInStatus
+        min_criterion = MinimumTrialsInStatus(threshold=3, status=TrialStatus.COMPLETED)
+        with self.assertLogs(TransitionCriterion.__module__, logging.WARNING) as logger:
+            self.assertFalse(min_criterion.is_met(experiment, trials_from_node=None))
+            self.assertTrue(
+                any(warning_msg in output for output in logger.output),
+                logger.output,
+            )
+        for _idx, trial in experiment.trials.items():
+            trial._status = TrialStatus.COMPLETED
+        self.assertTrue(min_criterion.is_met(experiment, trials_from_node=None))
+
+    def test_repr(self) -> None:
+        """Tests that the repr string is correctly formatted for all
+        TransitionCriterion child classes.
+        """
+        max_trials_criterion = MaxTrials(
+            threshold=5,
+            enforce=True,
+            transition_to="GenerationStep_1",
+            only_in_status=TrialStatus.COMPLETED,
+        )
+        self.assertEqual(
+            str(max_trials_criterion),
+            "MaxTrials(threshold=5, enforce=True, only_in_status="
+            + "TrialStatus.COMPLETED, transition_to='GenerationStep_1')",
+        )
+
+        minimum_trials_in_status_criterion = MinimumTrialsInStatus(
+            status=TrialStatus.COMPLETED,
+            threshold=0,
+            transition_to="GenerationStep_2",
+        )
+        self.assertEqual(
+            str(minimum_trials_in_status_criterion),
+            "MinimumTrialsInStatus(status=TrialStatus.COMPLETED, threshold=0,"
+            + " transition_to='GenerationStep_2')",
+        )
+
+        minimum_preference_occurances_criterion = MinimumPreferenceOccurances(
+            metric_name="m1", threshold=3
+        )
+        self.assertEqual(
+            str(minimum_preference_occurances_criterion),
+            "MinimumPreferenceOccurances(metric_name='m1', threshold=3,"
+            + " transition_to=None)",
+        )
