b3ee6c9a3404c3b600111fea3931484ab218455c
Mia Garrard <mgarrard@meta.com>
2023-11-15 13:04:46 -0800

    Finalize fix of not needing to store transition criterion on steps (#1985)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/1985
    
    This diff:
    Finalizes the fix to the storage by removing the need to unset transiton criteria and doesn't store transition criterion anymore. It can do so because in the decoder we reconstruct the generation step, which automatically fills in the relevant node fields during its init method.
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D50752054
    
    fbshipit-source-id: 28a6529cc20d0239c521a37fd58dd379b1dc9610

--- ax/service/tests/test_ax_client.py
+++ ax/service/tests/test_ax_client.py
@@ -614,7 +614,6 @@ class TestAxClient(TestCase):
         )
         second_client = AxClient(db_settings=db_settings)
         second_client.load_experiment_from_database("unique_test_experiment")
-        generation_strategy._unset_non_persistent_state_fields()
         self.assertEqual(second_client.generation_strategy, generation_strategy)
 
     @patch(
