fixing the derivative gp posterior for matern kernel (#2071)
Summary:
Pull Request resolved: https://github.com/facebook/Ax/pull/2071

The previous implementation had a distinction between Matern with l1 distance and l2 distance. This distinction is not accurate and is unnecessary.

Pull Request resolved: https://github.com/facebook/Ax/pull/1431

Test Plan: Existing unit tests

Reviewed By: saitcakmak

Differential Revision: D43467145

Pulled By: bletham

fbshipit-source-id: 71307485be8336be468dc0c9093860e195f3d855

ax/utils/sensitivity/tests/test_sensitivity.py
@@ -303,7 +303,7 @@ class SensitivityAnanlysisTest(TestCase):
 
     def test_DerivativeGp(self) -> None:
         test_x = torch.rand(2, 2)
-        posterior = posterior_derivative(self.model, test_x, kernel_type="matern_l1")
+        posterior = posterior_derivative(self.model, test_x, kernel_type="matern")
         self.assertIsInstance(posterior, MultivariateNormal)
 
         with self.assertRaises(ValueError):