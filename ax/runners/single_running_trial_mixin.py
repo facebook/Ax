f9cb349d2f06284fe585db3925a71a99de6b4e80
Sam Daulton <sdaulton@meta.com>
2024-01-23 12:04:49 -0800

    move SingleRunningTrialMixin to OSS (#2144)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2144
    
    see title. This is useful in a stacked diff Scheduler diff.
    
    Reviewed By: Balandat
    
    Differential Revision: D52970943
    
    fbshipit-source-id: 88e8e3e801ed77c5bb94ccea97de21c95fe26f0f

---
+++ ax/runners/single_running_trial_mixin.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from collections import defaultdict
+from typing import Dict, Iterable, Set
+
+from ax.core.base_trial import BaseTrial, TrialStatus
+
+
+class SingleRunningTrialMixin:
+    """Mixin for Runners with a single running trial.
+
+    This mixin implements a simple poll_trial_status method that
+    allows for a single running trial (the latest running trial).
+    The returned status of trials that currently are marked as
+    running is completed.
+    """
+
+    def poll_trial_status(
+        self, trials: Iterable[BaseTrial]
+    ) -> Dict[TrialStatus, Set[int]]:
+        """Checks the status of any non-terminal trials and returns their
+        indices as a mapping from TrialStatus to a list of indices. Required
+        for runners used with Ax ``Scheduler``.
+
+        NOTE: Does not need to handle waiting between polling calls while trials
+        are running; this function should just perform a single poll.
+
+        Args:
+            trials: Trials to poll.
+
+        Returns:
+            A dictionary mapping TrialStatus to a list of trial indices that have
+            the respective status at the time of the polling. This does not need to
+            include trials that at the time of polling already have a terminal
+            (ABANDONED, FAILED, COMPLETED) status (but it may).
+        """
+        trials = list(trials)
+        if len(trials) == 0:
+            return {}
+        trial_statuses = defaultdict(set)
+        running_trial_indices = trials[0].experiment.running_trial_indices
+        max_running_trial_index = (
+            -1 if len(running_trial_indices) == 0 else max(running_trial_indices)
+        )
+        for trial in trials:
+            if trial.status in (
+                TrialStatus.ABANDONED,
+                TrialStatus.FAILED,
+                TrialStatus.COMPLETED,
+            ):
+                continue
+            elif (trial.status == TrialStatus.RUNNING) and (
+                trial.index < max_running_trial_index
+            ):
+                trial_statuses[TrialStatus.COMPLETED].add(trial.index)
+            else:
+                trial_statuses[trial.status].add(trial.index)
+        return dict(trial_statuses)
---
+++ ax/runners/tests/test_single_running_trial_mixin.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from ax.core.base_trial import TrialStatus
+from ax.runners.single_running_trial_mixin import SingleRunningTrialMixin
+from ax.runners.synthetic import SyntheticRunner
+from ax.utils.common.testutils import TestCase
+from ax.utils.testing.core_stubs import get_branin_experiment
+
+
+class SyntheticRunnerWithSingleRunningTrial(SingleRunningTrialMixin, SyntheticRunner):
+    ...
+
+
+class SingleRunningTrialMixinTest(TestCase):
+    def test_single_running_trial_mixin(self) -> None:
+        runner = SyntheticRunnerWithSingleRunningTrial()
+        exp = get_branin_experiment(with_trial=True, with_batch=True)
+        exp.runner = runner
+        trials = exp.trials.values()
+        for trial in trials:
+            trial.assign_runner()
+            trial.run()
+        trial_statuses = runner.poll_trial_status(trials=trials)
+        self.assertEqual(trial_statuses[TrialStatus.COMPLETED], {0})
+        self.assertEqual(trial_statuses[TrialStatus.RUNNING], {1})
+
+    def test_no_trials(self) -> None:
+        runner = SyntheticRunnerWithSingleRunningTrial()
+        trial_statuses = runner.poll_trial_status(trials=[])
+        self.assertEqual(trial_statuses, {})
+
+    def test_abandoned_trial(self) -> None:
+        runner = SyntheticRunnerWithSingleRunningTrial()
+        exp = get_branin_experiment(with_trial=True)
+        exp.runner = runner
+        trial = exp.trials[0]
+        trial.assign_runner()
+        trial.mark_abandoned()
+        trial_statuses = runner.poll_trial_status(trials=[trial])
+        self.assertEqual(trial_statuses, {})
