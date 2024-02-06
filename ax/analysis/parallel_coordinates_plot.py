88628925d4d39525bf3bd9a4245d133be3ce8484
Matthew Grange <mgrange@meta.com>
2024-02-06 15:49:51 -0800

    ParallelCoordinatesPlot class (#2172)
    Summary:
    
    Added the ParallelCoordinatesPlot class which extends BasePlotlyVisualization
    
    Reviewed By: mpolson64
    
    Differential Revision: D53286537

---
+++ ax/analysis/parallel_coordinates_plot.py
@@ -0,0 +1,93 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import List, Optional, Tuple
+
+import pandas as pd
+
+from ax.core.arm import Arm
+from ax.core.base_trial import BaseTrial
+
+from ax.core.batch_trial import BatchTrial
+from ax.core.experiment import Experiment
+from ax.core.trial import Trial
+
+from plotly import express as px, graph_objs as go
+
+from .base_plotly_visualization import BasePlotlyVisualization
+
+
+class ParallelCoordinatesPlot(BasePlotlyVisualization):
+    def __init__(
+        self,
+        experiment: Experiment,
+        objective_name: Optional[str] = None,
+    ) -> None:
+        """
+        Args:
+        experiment: Experiment containing trials to plot
+        objective_name: Objective name used to color lines between
+            parallel plots
+        """
+
+        if not objective_name:
+            if not experiment.optimization_config:
+                raise ValueError("No objective specified for parallel coordinate")
+            objective_name = experiment.optimization_config.objective.metric.name
+
+        self.objective_name = objective_name
+
+        super().__init__(experiment=experiment)
+
+    def get_df(self) -> pd.DataFrame:
+        """Strip variables not desired in the final plot
+         and truncate names for readability
+
+        Returns:
+            df.DataFrame: data frame ready for ingestion by plotly
+        """
+
+        data_df = self.experiment.lookup_data().df
+        filtered_df = data_df.loc[data_df["metric_name"] == self.objective_name]
+
+        if filtered_df.empty:
+            raise ValueError(f"No data found for metric {self.objective_name}")
+
+        def map_trial_to_arms(trial: BaseTrial) -> List[Tuple[Arm, Optional[int]]]:
+            if isinstance(trial, BatchTrial):
+                return [(arm, trial.index) for arm in trial.arms]
+            if isinstance(trial, Trial):
+                if not trial.arm:
+                    raise ValueError("Trial does not contain an arm")
+                return [(trial.arm, trial.index)]
+            raise ValueError(f"Unsupported trial type {type(trial)}")
+
+        records = [
+            {
+                self.objective_name: filtered_df.loc[data_df["arm_name"] == arm.name][
+                    "mean"
+                ].item(),
+                "index": index,
+                **arm.parameters,
+            }
+            for t in self.experiment.trials.values()
+            for arm, index in map_trial_to_arms(t)
+        ]
+
+        return pd.DataFrame.from_records(records)
+
+    def get_fig(self) -> go.Figure:
+        """Plot trials as a parallel coordinates graph
+
+        Returns:
+            go.Figure: Parellel coordinates plot of all experiment trials
+        """
+        df = self.get_df()
+
+        return px.parallel_coordinates(
+            df,
+            color=self.objective_name,
+            dimensions=self.experiment.search_space.parameters.keys(),
+        )
---
+++ ax/analysis/tests/test_parallel_coordinates_plot.py
@@ -0,0 +1,84 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from ax.analysis.parallel_coordinates_plot import ParallelCoordinatesPlot
+from ax.core.batch_trial import BatchTrial
+
+from ax.utils.common.testutils import TestCase
+from ax.utils.testing.core_stubs import get_branin_data_batch, get_branin_experiment
+from plotly import graph_objs as go
+
+
+class TestParallelCoordinatesPlot(TestCase):
+    def test_parallel_coordinates_plot(self) -> None:
+        exp = get_branin_experiment(with_completed_trial=True)
+
+        parallel_coordinates_plot = ParallelCoordinatesPlot(
+            experiment=exp, objective_name="branin"
+        )
+
+        # Assert that plot can be constructed successfully
+        plot = parallel_coordinates_plot.get_fig()
+        self.assertIsInstance(plot, go.Figure)
+
+    def test_parallel_coordinates_plot_batch_trial(self) -> None:
+        exp = get_branin_experiment(with_batch=True)
+
+        batch_trial = exp.trials[0]
+        if not isinstance(batch_trial, BatchTrial):
+            raise TypeError("was not a batch trial")
+        batch_trial.mark_running(no_runner_required=True)
+
+        exp.attach_data(
+            get_branin_data_batch(batch=batch_trial)
+        )  # Add data for batch trial
+        batch_trial.mark_completed()
+
+        parallel_coordinates_plot = ParallelCoordinatesPlot(
+            experiment=exp, objective_name="branin"
+        )
+
+        # Assert that plot can be constructed successfully
+        plot = parallel_coordinates_plot.get_fig()
+        self.assertIsInstance(plot, go.Figure)
+
+    def test_parallel_coordinates_plot_objective_from_config(self) -> None:
+        exp = get_branin_experiment(
+            has_optimization_config=True, with_completed_trial=True
+        )
+
+        parallel_coordinates_plot = ParallelCoordinatesPlot(experiment=exp)
+
+        # Assert that plot can be constructed successfully
+        plot = parallel_coordinates_plot.get_fig()
+        self.assertIsInstance(plot, go.Figure)
+
+    def test_metric_not_found(self) -> None:
+        exp = get_branin_experiment(with_completed_trial=True)
+
+        parallel_coordinates_plot = ParallelCoordinatesPlot(
+            experiment=exp, objective_name="invalid_metric"
+        )
+
+        # Assert that plot can be constructed successfully
+
+        with self.assertRaises(ValueError):
+            _ = parallel_coordinates_plot.get_df()
+
+    def test_parallel_coordinates_get_df(self) -> None:
+        exp = get_branin_experiment(with_completed_trial=True)
+
+        parallel_coordinates_plot = ParallelCoordinatesPlot(
+            experiment=exp, objective_name="branin"
+        )
+
+        # Assert that dataframe can be constructed successfully
+        df = parallel_coordinates_plot.get_df()
+        # check objective in dataframe
+        self.assertIn("branin", df.columns)
+        # check parameter names in dataframe
+        self.assertIn("x1", df.columns)
+        self.assertIn("x2", df.columns)
