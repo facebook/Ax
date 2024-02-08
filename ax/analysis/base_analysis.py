23af65ea34619d5ef6889b547c3257b10851dd1f
Matthew Grange <mgrange@meta.com>
2024-02-08 07:45:46 -0800

    BaseAnalysis and BasePlotlyVisualization classes (#2170)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2170
    
    - Added BaseAnalysis and BasePlotlyVisualization classes
    
    Reviewed By: mpolson64
    
    Differential Revision: D53272588
    
    fbshipit-source-id: fbeae0fe34430aed1888d7600968243c0acaf830

---
+++ ax/analysis/base_analysis.py
@@ -0,0 +1,33 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from abc import ABC, abstractmethod
+
+import pandas as pd
+from ax.core.experiment import Experiment
+
+
+class BaseAnalysis(ABC):
+    """
+    Abstract Analysis class for ax.
+    This is an interface that defines the methods to be implemented by all analyses.
+    Computes an output dataframe for each analysis
+    """
+
+    def __init__(self, experiment: Experiment) -> None:
+        """
+        Initialize the analysis with the experiment object.
+        """
+        self._experiment = experiment
+
+    @property
+    def experiment(self) -> Experiment:
+        return self._experiment
+
+    @abstractmethod
+    def get_df(self) -> pd.DataFrame:
+        """
+        Return the output of the analysis of this class.
+        """
---
+++ ax/analysis/base_plotly_visualization.py
@@ -0,0 +1,24 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from abc import abstractmethod
+
+import plotly.graph_objects as go
+
+from ax.analysis.base_analysis import BaseAnalysis
+
+
+class BasePlotlyVisualization(BaseAnalysis):
+    """
+    Abstract PlotlyVisualization class for ax.
+    This is an interface that defines the method to be implemented by all ax plots.
+    Computes an output dataframe for each analysis
+    """
+
+    @abstractmethod
+    def get_fig(self) -> go.Figure:
+        """
+        Return the plotly figure of the analysis of this class.
+        """
