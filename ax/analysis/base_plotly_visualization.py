a65e847c22639c31a8d17d8f3accbedc12e42e34
Matthew Grange <mgrange@meta.com>
2024-07-02 08:28:30 -0700

    Adding df and fig properties to BaseAnalysis and BasePlotlyVisualization to support loading analyses back from database. (#2549)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2549
    
    Because Analyses need to execute analysis on experiment data to produce outputs, it does not make sense to load stored analysis back into their original class.
    Instead, we should have Analysis classes just store and serve the dataframe and plots.
    
    This change creates the BaseAnalysis and BasePlotlyVisualization classes to add "df" and "fig" properties respectively. Using these will first check for a saved df or fig and serve that, otherwise it will call "get_df" and "get_fig" respectively.
    
    So that these Base classes can be instantiated when loaded from databases, also removed the abstractclass decorator and instead raise "NotImplementedError" when the get_df / get_fig methods are called.
    
    Reviewed By: Cesar-Cardoso
    
    Differential Revision: D58031648
    
    fbshipit-source-id: 3984adc0bbce192ed6856010c253f71bd1e9bbf5

--- ax/analysis/base_plotly_visualization.py
+++ ax/analysis/base_plotly_visualization.py
@@ -5,11 +5,14 @@
 
 # pyre-strict
 
-from abc import abstractmethod
+from typing import Optional
+
+import pandas as pd
 
 import plotly.graph_objects as go
 
 from ax.analysis.base_analysis import BaseAnalysis
+from ax.core.experiment import Experiment
 
 
 class BasePlotlyVisualization(BaseAnalysis):
@@ -19,8 +22,32 @@ class BasePlotlyVisualization(BaseAnalysis):
     Computes an output dataframe for each analysis
     """
 
-    @abstractmethod
+    def __init__(
+        self,
+        experiment: Experiment,
+        df_input: Optional[pd.DataFrame] = None,
+        fig_input: Optional[go.Figure] = None,
+    ) -> None:
+        """
+        Initialize the analysis with the experiment object.
+        For scenarios where an analysis output is already available,
+        we can pass the dataframe as an input.
+        """
+        self._fig = fig_input
+        super().__init__(experiment=experiment, df_input=df_input)
+
+    @property
+    def fig(self) -> go.Figure:
+        """
+        Return the output of the analysis of this class.
+        """
+        if self._fig is None:
+            self._fig = self.get_fig()
+        return self._fig
+
     def get_fig(self) -> go.Figure:
         """
         Return the plotly figure of the analysis of this class.
+        Subclasses should overwrite this.
         """
+        raise NotImplementedError("get_fig must be implemented by subclass")
