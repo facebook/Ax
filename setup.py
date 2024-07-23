9cde57212221460c20e5d4e00bba3cd3a5349ebe
Sait Cakmak <saitcakmak@meta.com>
2024-07-23 08:48:28 -0700

    Pin BoTorch to 0.11.3 (#2594)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2594
    
    --
    
    Reviewed By: mgarrard
    
    Differential Revision: D60069260
    
    fbshipit-source-id: a01ceea35a7ef89ff2bfe166f0419a36932d512d

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 
 from setuptools import find_packages, setup
 
-PINNED_BOTORCH_VERSION = "0.11.0"
+PINNED_BOTORCH_VERSION = "0.11.3"
 
 if os.environ.get("ALLOW_BOTORCH_LATEST"):
     # allows a more recent previously installed version of botorch to remain
