2ef6646c36115b94d5559b623468934b7807a071
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-07-26 13:12:36 -0700

    Add missing Pyre mode headers] [batch:1/25] [shard:2/N]
    Differential Revision: D60283507
    
    fbshipit-source-id: 38172f74ee3ae62362f71f88ffab5ceb309e0056

--- ax/analysis/__init__.py
+++ ax/analysis/__init__.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 from ax.analysis.analysis import Analysis, AnalysisCard, AnalysisCardLevel
 from ax.analysis.markdown import *  # noqa
 from ax.analysis.plotly import *  # noqa
--- ax/analysis/plotly/__init__.py
+++ ax/analysis/plotly/__init__.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 from ax.analysis.plotly.plotly_analysis import PlotlyAnalysis, PlotlyAnalysisCard
 
 __all__ = ["PlotlyAnalysis", "PlotlyAnalysisCard"]
