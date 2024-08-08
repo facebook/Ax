a3debca9a59184abcb6c06239c9fed900c38f736
Cesar Cardoso <cesarcardoso@meta.com>
2024-08-08 12:29:52 -0700

    SQAAnalysisCard refactor (#2644)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2644
    
    * Refactor `SQAAnalysis` -> `SQAAnalysisCard`
    * Delete `SQAAnalysisReport` and related encoders/decoders
    
    Reviewed By: danielcohenlive
    
    Differential Revision: D60258359
    
    fbshipit-source-id: 4b1a96742f365b17c96f22ae9ee3211cf50019d6

--- ax/storage/sqa_store/sqa_config.py
+++ ax/storage/sqa_store/sqa_config.py
@@ -10,8 +10,7 @@ from dataclasses import dataclass, field
 from enum import Enum
 from typing import Any, Callable, Dict, Optional, Type, Union
 
-from ax.analysis.old.analysis_report import AnalysisReport
-from ax.analysis.old.base_analysis import BaseAnalysis
+from ax.analysis.analysis import AnalysisCard
 
 from ax.core.arm import Arm
 from ax.core.batch_trial import AbandonedArm
@@ -36,8 +35,7 @@ from ax.storage.runner_registry import CORE_RUNNER_REGISTRY
 from ax.storage.sqa_store.db import SQABase
 from ax.storage.sqa_store.sqa_classes import (
     SQAAbandonedArm,
-    SQAAnalysis,
-    SQAAnalysisReport,
+    SQAAnalysisCard,
     SQAArm,
     SQAData,
     SQAExperiment,
@@ -80,8 +78,7 @@ class SQAConfig:
             Metric: SQAMetric,
             Runner: SQARunner,
             Trial: SQATrial,
-            BaseAnalysis: SQAAnalysis,
-            AnalysisReport: SQAAnalysisReport,
+            AnalysisCard: SQAAnalysisCard,
         }
 
     class_to_sqa_class: Dict[Type[Base], Type[SQABase]] = field(
