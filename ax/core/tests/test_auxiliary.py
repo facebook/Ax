d1618c40a3408dc4bf69e2d515f0b777a692ec96
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2024-08-16 13:52:00 -0700

    Add missing Pyre mode headers] [batch:1/25] [shard:2/N]
    Differential Revision: D61381894
    
    fbshipit-source-id: 959883534bad917fba94f4d71d8cb6989076a4be

--- ax/core/tests/test_auxiliary.py
+++ ax/core/tests/test_auxiliary.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 from ax.core.auxiliary import AuxiliaryExperiment
 from ax.utils.common.testutils import TestCase
 from ax.utils.testing.core_stubs import get_experiment, get_experiment_with_data
