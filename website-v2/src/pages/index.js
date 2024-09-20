06f002113ec02fb401f11687176cf1abeb4e04c7
Cristian Lara <cristianlara@meta.com>
2024-09-19 17:11:03 -0700

    Docusaurus: Make rough draft for landing page

--- website-v2/src/pages/index.js
+++ website-v2/src/pages/index.js
@@ -8,12 +8,16 @@
  */
 
 const React = require('react');
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
 const CompLibrary = {
-  Container: props => <div {...props}></div>,
+  Container: props => <div {...props}>{props.children}</div>,
   GridBlock: props => <div {...props}></div>,
-  MarkdownBlock: props => <div {...props}></div>
+  MarkdownBlock: props => <div {...props}>{props.children}</div>
 };
+
+import CodeBlock from '@theme/CodeBlock';
 
 import Layout from "@theme/Layout";
 
@@ -54,7 +58,7 @@ class HomeSplash extends React.Component {
     const PromoSection = props => (
       <div className="section promoSection">
         <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
+          <div>{props.children}</div>
         </div>
       </div>
     );
@@ -97,6 +101,7 @@ class Index extends React.Component {
           align="center"
           contents={props.children}
           layout={props.layout}
+          children={props.children}
         />
       </Container>
     );
@@ -115,9 +120,7 @@ class Index extends React.Component {
       </Block>
     );
 
-    const pre = '```';
-
-    const codeExample = `${pre}python
+    const codeExample = `
 >>> from ax import optimize
 >>> best_parameters, best_values, experiment, model = optimize(
         parameters=[
@@ -145,18 +148,18 @@ class Index extends React.Component {
       <div
         className="productShowcaseSection"
         id="quickstart"
-        style={{textAlign: 'center'}}>
-        <h2>Get Started</h2>
+      >
+        <h2 style={{textAlign: 'center'}}>Get Started</h2>
         <Container>
           <ol>
             <li>
               Install Ax:
-              <MarkdownBlock>{bash`conda install pytorch torchvision -c pytorch  # OSX only`}</MarkdownBlock>
-              <MarkdownBlock>{bash`pip3 install ax-platform  # all systems`}</MarkdownBlock>
+              <CodeBlock language="bash">{`conda install pytorch torchvision -c pytorch  # OSX only`}</CodeBlock>
+              <CodeBlock languague="bash">{`pip3 install ax-platform  # all systems`}</CodeBlock>
             </li>
             <li>
               Run an optimization:
-              <MarkdownBlock>{codeExample}</MarkdownBlock>
+              <CodeBlock language="python">{codeExample}</CodeBlock>
             </li>
           </ol>
         </Container>
@@ -164,8 +167,8 @@ class Index extends React.Component {
     );
 
     const Features = () => (
-      <div className="productShowcaseSection" style={{textAlign: 'center'}}>
-        <h2>Key Features</h2>
+      <div className="productShowcaseSection">
+        <h2 style={{textAlign: 'center'}}>Key Features</h2>
         <Block layout="fourColumn">
           {[
             {
@@ -196,6 +199,59 @@ class Index extends React.Component {
         </Block>
       </div>
     );
+
+    const features = [
+      {
+        content:
+          'Easy to plug in new algorithms and use the library across ' +
+          'different domains.',
+        image: `${baseUrl}img/th-large-solid.svg`,
+        imageAlign: 'top',
+        title: 'Modular',
+      },
+      {
+        content:
+          'Field experiments require a range of considerations ' +
+          'beyond standard optimization problems.',
+        image: `${baseUrl}img/dice-solid.svg`,
+        imageAlign: 'top',
+        title: 'Supports A/B Tests',
+      },
+      {
+        content:
+          'Support for industry-grade experimentation ' +
+          'and optimization management, including MySQL storage.',
+        image: `${baseUrl}img/database-solid.svg`,
+        imageAlign: 'top',
+        title: 'Production-Ready',
+      },
+    ];
+
+    const featuresNode = features && features.length && (
+      <section className="productShowcaseSection">
+        <div className="container">
+        <h2 style={{textAlign: 'center'}}>Key Features</h2>
+          <div className="row">
+            {features.map(({image, title, content}, idx) => (
+              <div
+                key={idx}
+                className="col col--4">
+                {image && (
+                  <div className="text--center">
+                    <img
+                      src={image}
+                      alt={title}
+                    />
+                  </div>
+                )}
+                <h3>{title}</h3>
+                <p>{content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    )
 
     const Showcase = () => {
       if ((siteConfig.users || []).length === 0) {
@@ -230,7 +286,8 @@ class Index extends React.Component {
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="landingPage mainContainer">
-          <Features />
+          {/* <Features /> */}
+          {featuresNode}
           <QuickStart />
         </div>
       </div>
@@ -238,4 +295,97 @@ class Index extends React.Component {
   }
 }
 
-export default props => <Layout><Index {...props} /></Layout>;
+// export default props => <Layout><Index {...props} /></Layout>;
+
+
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+const MyPage = () => {
+  const {siteConfig} = useDocusaurusContext();
+
+  const codeExample = `>>> from ax import optimize
+>>> best_parameters, best_values, experiment, model = optimize(
+        parameters=[
+          {
+            "name": "x1",
+            "type": "range",
+            "bounds": [-10.0, 10.0],
+          },
+          {
+            "name": "x2",
+            "type": "range",
+            "bounds": [-10.0, 10.0],
+          },
+        ],
+        # Booth function
+        evaluation_function=lambda p: (p["x1"] + 2*p["x2"] - 7)**2 + (2*p["x1"] + p["x2"] - 5)**2,
+        minimize=True,
+    )
+
+>>> best_parameters
+{'x1': 1.02, 'x2': 2.97}  # true min is (1, 3)`;
+
+  const QuickStart = () => (
+    <div
+      className="get-started-section"
+      id="quickstart"
+    >
+      <h2 style={{textAlign: 'center'}}>Get Started</h2>
+      <Container>
+        <ol>
+          <li>
+            Install Ax:
+            <Tabs>
+              <TabItem value="linux" label="Linux" deafult>
+                  <CodeBlock languague="bash" showLineNumbers>{`pip3 install ax-platform`}</CodeBlock>
+              </TabItem>
+              <TabItem value="mac" label="Mac">
+                  <CodeBlock language="bash" showLineNumbers>{`conda install pytorch torchvision -c pytorch
+pip3 install ax-platform`}</CodeBlock>
+              </TabItem>
+            </Tabs>
+          </li>
+          <li>
+            Run an optimization:
+            <br/><br/>
+            <CodeBlock language="python" showLineNumbers>{codeExample}</CodeBlock>
+          </li>
+        </ol>
+      </Container>
+    </div>
+  );
+
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.tagline}>
+      <div className="hero text--center">
+        <div className="container ">
+          <div className="padding-vert--md">
+            <h1 className="hero__title">{siteConfig.title}</h1>
+            <p className="hero__subtitle">{siteConfig.tagline}</p>
+          </div>
+          <div>
+            <Link
+              to="/docs/why-ax"
+              className="button button--lg button--outline button--primary">
+              Why Ax?
+            </Link>
+            <Link
+              to="/docs/why-ax"
+              className="button button--lg button--outline button--primary">
+              Get started
+            </Link>
+            <Link
+              to="/docs/why-ax"
+              className="button button--lg button--outline button--primary">
+              Tutorials
+            </Link>
+          </div>
+        </div>
+      </div>
+      <QuickStart />
+    </Layout>
+  );
+};
+
+export default MyPage;
