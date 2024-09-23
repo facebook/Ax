d986caded779b3dc8271ca6a807b96c62de011e8
Mia Garrard <mgarrard@meta.com>
2024-09-23 13:16:49 -0700

    add remaining n input constructor (#2758)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2758
    
    This diff adds the input constructor for our default logic for repeat arms. See note in comments about how this new logic differs
    Follow up diffs:
    - update the transition logic to leverage this
    - storage --> let's do this once we all like the 3 input constructors
    - update the input constructors to handle the case where n isn't provided as a kwarg
    - add tests for real world scenario per Liz suggestion
    - add test to enforce signature across input constructors per Liz/Sait/Daniel suggestion
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D62467907

--- ax/modelbridge/tests/test_generation_node_input_constructors.py
+++ ax/modelbridge/tests/test_generation_node_input_constructors.py
@@ -5,11 +5,14 @@
 
 # pyre-strict
 
+from ax.core.arm import Arm
+from ax.core.generator_run import GeneratorRun
 from ax.modelbridge.generation_node import GenerationNode
 from ax.modelbridge.generation_node_input_constructors import NodeInputConstructors
 from ax.modelbridge.model_spec import ModelSpec
 from ax.modelbridge.registry import Models
 from ax.utils.common.testutils import TestCase
+from ax.utils.testing.core_stubs import get_branin_experiment
 
 
 class TestGenerationNodeInputConstructors(TestCase):
@@ -23,6 +26,13 @@ class TestGenerationNodeInputConstructors(TestCase):
         self.sobol_generation_node = GenerationNode(
             node_name="test", model_specs=[self.sobol_model_spec]
         )
+        self.experiment = get_branin_experiment(with_completed_trial=True)
+        # construct a list of grs that will mock a list of grs that would exist during
+        # a gs.gen call. This list has one single arm GR, and one 3-arm GR.
+        self.grs = [
+            GeneratorRun(arms=[Arm(parameters={"x1": 1, "x2": 5})]),
+            GeneratorRun(arms=[Arm(parameters={"x1": 1, "x2": y}) for y in range(3)]),
+        ]
 
     def test_consume_all_n_constructor(self) -> None:
         """Test that the consume_all_n_constructor returns full n."""
@@ -54,7 +64,37 @@ class TestGenerationNodeInputConstructors(TestCase):
         self.assertEqual(medium_n, 1)
         self.assertEqual(large_n, 2)
 
-    def test_no_n_provided_error(self) -> None:
+    def test_remaining_n_constructor_expect_1(self) -> None:
+        """Test that the remaining_n_constructor returns the remaining n."""
+        # should return 1 becuase 4 arms already exist and 5 are requested
+        expect_1 = NodeInputConstructors.REMAINING_N(
+            previous_node=None,
+            next_node=self.sobol_generation_node,
+            gs_gen_call_kwargs={"n": 5, "grs": self.grs},
+        )
+        self.assertEqual(expect_1, 1)
+
+    def test_remaining_n_constructor_expect_0(self) -> None:
+        # should return 0 becuase 4 arms already exist and 4 are requested
+        expect_0 = NodeInputConstructors.REMAINING_N(
+            previous_node=None,
+            next_node=self.sobol_generation_node,
+            gs_gen_call_kwargs={"n": 4, "grs": self.grs},
+        )
+        self.assertEqual(expect_0, 0)
+
+    def test_remaining_n_constructor_cap_at_zero(self) -> None:
+        # should return 0 becuase 4 arms already exist and 3 are requested
+        # this is a bad state that should never be hit, but ensuring proper
+        # handling here feels like a valid edge case
+        expect_0 = NodeInputConstructors.REMAINING_N(
+            previous_node=None,
+            next_node=self.sobol_generation_node,
+            gs_gen_call_kwargs={"n": 3, "grs": self.grs},
+        )
+        self.assertEqual(expect_0, 0)
+
+    def test_no_n_provided_error_all_n(self) -> None:
         """Test raise error if n is not specified."""
         with self.assertRaisesRegex(
             NotImplementedError,
@@ -65,11 +105,23 @@ class TestGenerationNodeInputConstructors(TestCase):
                 next_node=self.sobol_generation_node,
                 gs_gen_call_kwargs={},
             )
+
+    def test_no_n_provided_error_repeat_n(self) -> None:
         with self.assertRaisesRegex(
             NotImplementedError,
             " `repeat_arm_n` only supports cases where n is specified",
         ):
             _ = NodeInputConstructors.REPEAT_N(
+                previous_node=None,
+                next_node=self.sobol_generation_node,
+                gs_gen_call_kwargs={},
+            )
+
+    def test_no_n_provided_error_remaining_n(self) -> None:
+        with self.assertRaisesRegex(
+            NotImplementedError, "only supports cases where n is specified"
+        ):
+            _ = NodeInputConstructors.REMAINING_N(
                 previous_node=None,
                 next_node=self.sobol_generation_node,
                 gs_gen_call_kwargs={},
