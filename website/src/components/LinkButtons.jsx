98cf5d976740d7e67b62082ec8c2e89cbeae9087
Cristian Lara <cristianlara@meta.com>
2024-10-03 13:53:05 -0400

    Tutorials: Use ExternalLink icon from docusaurus theme
    instead of manually copying the svg and trying to reference the internal class. It's futile to reference the internal class anyway since the class is named dynamically at runtime.

--- website/src/components/LinkButtons.jsx
+++ website/src/components/LinkButtons.jsx
@@ -7,17 +7,18 @@
 
 import React from 'react';
 import Link from '@docusaurus/Link';
+import IconExternalLink from '@theme/Icon/ExternalLink'
 
 const LinkButtons = ({githubUrl, colabUrl}) => {
   return (
     <div>
       <Link to={githubUrl} className="button button--outline button--primary margin--xs">
         Open in GitHub
-        <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+        <IconExternalLink />
       </Link>
       <Link to={colabUrl} className="button button--outline button--primary margin--xs">
         Run in Google Colab
-        <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" class="iconExternalLink_node_modules-@docusaurus-theme-classic-lib-theme-Icon-ExternalLink-styles-module"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+        <IconExternalLink />
       </Link>
       <br/><br/>
     </div>
