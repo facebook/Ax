92f64f2e8e0c47ef938f8d7fba2276dbf027fc91
Sait Cakmak <saitcakmak@meta.com>
2024-10-08 08:45:36 -0700

    Make run_trials_with_gs into a standalone test helper (#2836)
    Summary:
    
    This is useful for testing generation strategies without having to write repetitive boilerplate to advance them to the node of interest. It runs the specified number of trials using with the given experiment & GS, and completes the trials with dummy observations.
    
    Reviewed By: esantorella
    
    Differential Revision: D64044818

--- ax/utils/testing/utils.py
+++ ax/utils/testing/utils.py
@@ -5,10 +5,17 @@
 
 # pyre-strict
 
+import random
 from typing import Any
 
 import numpy as np
+import pandas as pd
 import torch
+from ax.core.data import Data
+from ax.core.experiment import Experiment
+from ax.exceptions.core import UnsupportedError
+from ax.modelbridge.generation_strategy import GenerationStrategy
+from pyre_extensions import none_throws
 from torch import Tensor
 
 
@@ -32,3 +39,41 @@ def generic_equals(first: Any, second: Any) -> bool:
                 return False
         return True
     return first == second
+
+
+def run_trials_with_gs(
+    experiment: Experiment, gs: GenerationStrategy, num_trials: int
+) -> None:
+    r"""Runs and completes `num_trials` trials for the given experiment with the
+    given GS. The trials are completed with random metric values between 0 and 1.
+
+    Args:
+        experiment: The experiment to run trials on. Must have an optimization config.
+        gs: The generation strategy to use.
+        num_trials: The number of trials to run.
+    """
+    if experiment.optimization_config is None:
+        raise UnsupportedError(  # pragma: no cover
+            "`run_trials_with_gs` requires the experiment to have "
+            "an optimization config."
+        )
+    existing_trials = len(experiment.trials)
+    for i in range(existing_trials, existing_trials + num_trials):
+        trial = experiment.new_trial(generator_run=gs.gen(experiment=experiment))
+        data = Data(
+            df=pd.DataFrame.from_records(
+                [
+                    {
+                        "arm_name": arm.name,
+                        "metric_name": m,
+                        "mean": random.random(),
+                        "sem": None,
+                        "trial_index": i,
+                    }
+                    for m in none_throws(experiment.optimization_config).metrics
+                    for arm in trial.arms
+                ]
+            )
+        )
+        experiment.attach_data(data)
+        trial.run().complete()
