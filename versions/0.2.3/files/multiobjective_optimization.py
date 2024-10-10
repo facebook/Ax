497362e214c5cefee34e2fa7671b772676206210
Ax Website Deployment Script <docusaurus-bot@users.noreply.github.com>
2024-10-10 07:18:36 +0000

    Update latest version of site

---
+++ versions/0.2.3/files/multiobjective_optimization.py
@@ -0,0 +1,496 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Multi-Objective Optimization Ax API
+# ### Using the Service API
+# For Multi-objective optimization (MOO) in the `AxClient`, objectives are specified through the `ObjectiveProperties` dataclass.  An `ObjectiveProperties` requires a boolean `minimize`, and also accepts an optional floating point  `threshold`.  If a `threshold` is not specified, Ax will infer it through the use of heuristics.  If the user knows the region of interest (because they have specs or prior knowledge), then specifying the thresholds is preferable to inferring it. But if the user would need to guess, inferring is preferable.
+# 
+# 
+# To learn more about how to choose a threshold, see [Set Objective Thresholds to focus candidate generation in a region of interest](#Set-Objective-Thresholds-to-focus-candidate-generation-in-a-region-of-interest).  See the [Service API Tutorial](/tutorials/gpei_hartmann_service.html) for more infomation on running experiments with the Service API.
+
+# In[1]:
+
+
+from ax.service.ax_client import AxClient
+from ax.service.utils.instantiation import ObjectiveProperties
+
+import torch
+
+# Plotting imports and initialization
+from ax.utils.notebook.plotting import render, init_notebook_plotting
+from ax.plot.pareto_utils import compute_posterior_pareto_frontier
+from ax.plot.pareto_frontier import plot_pareto_frontier
+init_notebook_plotting()
+
+# Load our sample 2-objective problem
+from botorch.test_functions.multi_objective import BraninCurrin
+branin_currin = BraninCurrin(negate=True).to(
+    dtype=torch.double, 
+    device= torch.device("cuda" if torch.cuda.is_available() else "cpu"),
+)
+
+
+# In[2]:
+
+
+ax_client = AxClient()
+ax_client.create_experiment(
+    name="moo_experiment",
+    parameters=[
+        {
+            "name": f"x{i+1}",
+            "type": "range",
+            "bounds": [0.0, 1.0],
+        }
+        for i in range(2)
+    ],
+    objectives={
+        # `threshold` arguments are optional
+        "a": ObjectiveProperties(minimize=False, threshold=branin_currin.ref_point[0]), 
+        "b": ObjectiveProperties(minimize=False, threshold=branin_currin.ref_point[1])
+    },
+    overwrite_existing_experiment=True,
+    is_test=True,
+)
+
+
+# ### Create an Evaluation Function
+# In the case of MOO experiments, evaluation functions can be any arbitrary function that takes in a `dict` of parameter names mapped to values and returns a `dict` of objective names mapped to a `tuple` of mean and SEM values.
+
+# In[3]:
+
+
+def evaluate(parameters):
+    evaluation = branin_currin(torch.tensor([parameters.get("x1"), parameters.get("x2")]))
+    # In our case, standard error is 0, since we are computing a synthetic function.
+    # Set standard error to None if the noise level is unknown.
+    return {"a": (evaluation[0].item(), 0.0), "b": (evaluation[1].item(), 0.0)}
+
+
+# ### Run Optimization
+
+# In[4]:
+
+
+for i in range(25):
+    parameters, trial_index = ax_client.get_next_trial()
+    # Local evaluation here can be replaced with deployment to external system.
+    ax_client.complete_trial(trial_index=trial_index, raw_data=evaluate(parameters))
+
+
+# ### Plot Pareto Frontier
+
+# In[5]:
+
+
+objectives = ax_client.experiment.optimization_config.objective.objectives
+frontier = compute_posterior_pareto_frontier(
+    experiment=ax_client.experiment,
+    data=ax_client.experiment.fetch_data(),
+    primary_objective=objectives[1].metric,
+    secondary_objective=objectives[0].metric,
+    absolute_metrics=["a", "b"],
+    num_points=20,
+)
+render(plot_pareto_frontier(frontier, CI_level=0.90)) 
+
+
+# # Deep Dive
+# 
+# In the rest of this tutorial, we will show two algorithms available in Ax for multi-objective optimization
+# and visualize how they compare to eachother and to quasirandom search.
+# 
+# MOO covers the case where we care about multiple
+# outcomes in our experiment but we do not know before hand a specific weighting of those
+# objectives (covered by `ScalarizedObjective`) or a specific constraint on one objective 
+# (covered by `OutcomeConstraint`s) that will produce the best result.
+# 
+# The solution in this case is to find a whole Pareto frontier, a surface in outcome-space
+# containing points that can't be improved on in every outcome. This shows us the
+# tradeoffs between objectives that we can choose to make.
+
+# ### Problem Statement
+# 
+# Optimize a list of M objective functions $ \bigl(f^{(1)}( x),..., f^{(M)}( x) \bigr)$ over a bounded search space $\mathcal X \subset \mathbb R^d$.
+# 
+# We assume $f^{(i)}$ are expensive-to-evaluate black-box functions with no known analytical expression, and no observed gradients. For instance, a machine learning model where we're interested in maximizing accuracy and minimizing inference time, with $\mathcal X$ the set of possible configuration spaces
+
+# ### Pareto Optimality
+# 
+# In a multi-objective optimization problem, there typically is no single best solution. Rather, the *goal* is to identify the set of Pareto optimal solutions such that any improvement in one objective means deteriorating another. Provided with the Pareto set, decision-makers can select an objective trade-off according to their preferences. In the plot below, the red dots are the Pareto optimal solutions (assuming both objectives are to be minimized).
+# ![pareto front](attachment:pareto_front%20%281%29.png)
+
+# ### Evaluating the Quality of a Pareto Front (Hypervolume)
+# 
+# Given a reference point $ r \in \mathbb R^M$, which we represent as a list of M `ObjectiveThreshold`s, one for each coordinate, the hypervolume (HV) of a Pareto set $\mathcal P = \{ f(x_i)\}_{i=1}^{|\mathcal P|}$ is the volume of the space dominated (superior in every one of our M objectives) by $\mathcal P$ and bounded from above by a point $ r$. The reference point should be set to be slightly worse (10% is reasonable) than the worst value of each objective that a decision maker would tolerate. In the figure below, the grey area is the hypervolume in this 2-objective problem.
+# ![hv_figure](attachment:hv_figure%20%281%29.png)
+
+# ### Set Objective Thresholds to focus candidate generation in a region of interest
+# 
+# The below plots show three different sets of points generated by the qNEHVI [1] algorithm with different objective thresholds (aka reference points). Note that here we use absolute thresholds, but thresholds can also be relative to a status_quo arm.
+# 
+# The first plot shows the points without the `ObjectiveThreshold`s visible (they're set far below the origin of the graph).
+# 
+# The second shows the points generated with (-18, -6) as thresholds. The regions violating the thresholds are greyed out. Only the white region in the upper right exceeds both threshold, points in this region dominate the intersection of these thresholds (this intersection is the reference point). Only points in this region contribute to the hypervolume objective. A few exploration points are not in the valid region, but almost all the rest of the points are.
+# 
+# The third shows points generated with a very strict pair of thresholds, (-18, -2). Only the white region in the upper right exceeds both thresholds. Many points do not lie in the dominating region, but there are still more focused there than in the second examples.
+# ![objective_thresholds_comparison.png](attachment:objective_thresholds_comparison.png)
+
+# ### Further Information
+# A deeper explanation of our the qNEHVI [1] and qNParEGO [2] algorithms this notebook explores can be found at 
+# 
+# [1] [S. Daulton, M. Balandat, and E. Bakshy. Parallel Bayesian Optimization of Multiple Noisy Objectives with Expected Hypervolume Improvement. Advances in Neural Information Processing Systems 34, 2021.](https://arxiv.org/abs/2105.08195)
+# 
+# [2] [S. Daulton, M. Balandat, and E. Bakshy. Differentiable Expected Hypervolume Improvement for Parallel Multi-Objective Bayesian Optimization. Advances in Neural Information Processing Systems 33, 2020.](https://arxiv.org/abs/2006.05078)
+# 
+# In addition, the underlying BoTorch implementation has a researcher-oriented tutorial at https://botorch.org/tutorials/multi_objective_bo.
+
+# ## Setup
+
+# In[6]:
+
+
+import pandas as pd
+from ax import *
+
+import numpy as np
+
+from ax.metrics.noisy_function import NoisyFunctionMetric
+from ax.service.utils.report_utils import exp_to_df
+from ax.runners.synthetic import SyntheticRunner
+
+# Factory methods for creating multi-objective optimization modesl.
+from ax.modelbridge.factory import get_MOO_EHVI, get_MOO_PAREGO
+
+# Analysis utilities, including a method to evaluate hypervolumes
+from ax.modelbridge.modelbridge_utils import observed_hypervolume
+
+
+# ## Define experiment configurations
+
+# ### Search Space
+
+# In[7]:
+
+
+x1 = RangeParameter(name="x1", lower=0, upper=1, parameter_type=ParameterType.FLOAT)
+x2 = RangeParameter(name="x2", lower=0, upper=1, parameter_type=ParameterType.FLOAT)
+
+search_space = SearchSpace(
+    parameters=[x1, x2],
+)
+
+
+# ### MultiObjectiveOptimizationConfig
+# 
+# To optimize multiple objective we must create a `MultiObjective` containing the metrics we'll optimize and `MultiObjectiveOptimizationConfig` (which contains `ObjectiveThreshold`s) instead of our more typical `Objective` and `OptimizationConfig`
+# 
+# We define `NoisyFunctionMetric`s to wrap our synthetic Branin-Currin problem's outputs. Add noise to see how robust our different optimization algorithms are.
+
+# In[8]:
+
+
+class MetricA(NoisyFunctionMetric):
+    def f(self, x: np.ndarray) -> float:
+        return float(branin_currin(torch.tensor(x))[0])
+    
+class MetricB(NoisyFunctionMetric):
+    def f(self, x: np.ndarray) -> float:
+        return float(branin_currin(torch.tensor(x))[1])
+
+metric_a = MetricA("a", ["x1", "x2"], noise_sd=0.0, lower_is_better=False)
+metric_b = MetricB("b", ["x1", "x2"], noise_sd=0.0, lower_is_better=False)
+
+
+# In[9]:
+
+
+mo = MultiObjective(
+    objectives=[Objective(metric=metric_a), Objective(metric=metric_b)],
+)
+
+
+# In[10]:
+
+
+objective_thresholds = [
+    ObjectiveThreshold(metric=metric, bound=val, relative=False)
+    for metric, val in zip(mo.metrics, branin_currin.ref_point)
+]
+
+
+# In[11]:
+
+
+optimization_config = MultiObjectiveOptimizationConfig(
+    objective=mo,
+    objective_thresholds=objective_thresholds,
+)
+
+
+# ## Define experiment creation utilities
+# 
+# These construct our experiment, then initialize with Sobol points before we fit a Gaussian Process model to those initial points.
+
+# In[12]:
+
+
+# Reasonable defaults for number of quasi-random initialization points and for subsequent model-generated trials.
+N_INIT = 6
+N_BATCH = 25
+
+
+# In[13]:
+
+
+def build_experiment():
+    experiment = Experiment(
+        name="pareto_experiment",
+        search_space=search_space,
+        optimization_config=optimization_config,
+        runner=SyntheticRunner(),
+    )
+    return experiment
+
+
+# In[14]:
+
+
+## Initialize with Sobol samples
+
+def initialize_experiment(experiment):
+    sobol = Models.SOBOL(search_space=experiment.search_space, seed=1234)
+
+    for _ in range(N_INIT):
+        experiment.new_trial(sobol.gen(1)).run()
+
+    return experiment.fetch_data()
+
+
+# # Sobol
+# We use quasirandom points as a fast baseline for evaluating the quality of our multi-objective optimization algorithms.
+
+# In[15]:
+
+
+sobol_experiment = build_experiment()
+sobol_data = initialize_experiment(sobol_experiment)
+
+
+# In[16]:
+
+
+sobol_model = Models.SOBOL(
+    experiment=sobol_experiment, 
+    data=sobol_data,
+)
+sobol_hv_list = []
+for i in range(N_BATCH):
+    generator_run = sobol_model.gen(1)
+    trial = sobol_experiment.new_trial(generator_run=generator_run)
+    trial.run()
+    exp_df = exp_to_df(sobol_experiment)
+    outcomes = np.array(exp_df[['a', 'b']], dtype=np.double)
+    # Fit a GP-based model in order to calculate hypervolume.
+    # We will not use this model to generate new points.
+    dummy_model = get_MOO_EHVI(
+        experiment=sobol_experiment, 
+        data=sobol_experiment.fetch_data(),
+    )
+    try:
+        hv = observed_hypervolume(modelbridge=dummy_model)
+    except:
+        hv = 0
+        print("Failed to compute hv")
+    sobol_hv_list.append(hv)
+    print(f"Iteration: {i}, HV: {hv}")
+
+sobol_outcomes = np.array(exp_to_df(sobol_experiment)[['a', 'b']], dtype=np.double)
+
+
+# ## qNEHVI
+# Noisy Expected Hypervolume Improvement. This is our current recommended algorithm for multi-objective optimization.
+
+# In[17]:
+
+
+ehvi_experiment = build_experiment()
+ehvi_data = initialize_experiment(ehvi_experiment)
+
+
+# In[18]:
+
+
+ehvi_hv_list = []
+ehvi_model = None
+for i in range(N_BATCH):   
+    ehvi_model = get_MOO_EHVI(
+        experiment=ehvi_experiment, 
+        data=ehvi_data,
+    )
+    generator_run = ehvi_model.gen(1)
+    trial = ehvi_experiment.new_trial(generator_run=generator_run)
+    trial.run()
+    ehvi_data = Data.from_multiple_data([ehvi_data, trial.fetch_data()])
+    
+    exp_df = exp_to_df(ehvi_experiment)
+    outcomes = np.array(exp_df[['a', 'b']], dtype=np.double)
+    try:
+        hv = observed_hypervolume(modelbridge=ehvi_model)
+    except:
+        hv = 0
+        print("Failed to compute hv")
+    ehvi_hv_list.append(hv)
+    print(f"Iteration: {i}, HV: {hv}")
+
+ehvi_outcomes = np.array(exp_to_df(ehvi_experiment)[['a', 'b']], dtype=np.double)
+
+
+# ## Plot qNEHVI Pareto Frontier based on model posterior 
+# 
+# The plotted points are samples from the fitted model's posterior, not observed samples.
+
+# In[19]:
+
+
+frontier = compute_posterior_pareto_frontier(
+    experiment=ehvi_experiment,
+    data=ehvi_experiment.fetch_data(),
+    primary_objective=metric_b,
+    secondary_objective=metric_a,
+    absolute_metrics=["a", "b"],
+    num_points=20,
+)
+
+render(plot_pareto_frontier(frontier, CI_level=0.90)) 
+
+
+# ## qNParEGO
+# This is a good alternative algorithm for multi-objective optimization when qNEHVI runs too slowly.
+
+# In[20]:
+
+
+parego_experiment = build_experiment()
+parego_data = initialize_experiment(parego_experiment)
+
+
+# In[21]:
+
+
+parego_hv_list = []
+parego_model = None
+for i in range(N_BATCH):   
+    parego_model = get_MOO_PAREGO(
+        experiment=parego_experiment, 
+        data=parego_data,
+    )
+    generator_run = parego_model.gen(1)
+    trial = parego_experiment.new_trial(generator_run=generator_run)
+    trial.run()
+    parego_data = Data.from_multiple_data([parego_data, trial.fetch_data()])
+    
+    exp_df = exp_to_df(parego_experiment)
+    outcomes = np.array(exp_df[['a', 'b']], dtype=np.double)
+    try:
+        hv = observed_hypervolume(modelbridge=parego_model)
+    except:
+        hv = 0
+        print("Failed to compute hv")
+    parego_hv_list.append(hv)
+    print(f"Iteration: {i}, HV: {hv}")
+
+parego_outcomes = np.array(exp_to_df(parego_experiment)[['a', 'b']], dtype=np.double)
+
+
+# ## Plot qNParEGO Pareto Frontier based on model posterior 
+# 
+# The plotted points are samples from the fitted model's posterior, not observed samples.
+
+# In[22]:
+
+
+frontier = compute_posterior_pareto_frontier(
+    experiment=parego_experiment,
+    data=parego_experiment.fetch_data(),
+    primary_objective=metric_b,
+    secondary_objective=metric_a,
+    absolute_metrics=["a", "b"],
+    num_points=20,
+)
+
+render(plot_pareto_frontier(frontier, CI_level=0.90)) 
+
+
+# ## Plot empirical data
+
+# #### Plot observed hypervolume, with color representing the iteration that a point was generated on.
+# 
+# To examine optimization process from another perspective, we plot the collected observations under each algorithm where the color corresponds to the BO iteration at which the point was collected. The plot on the right for $q$NEHVI shows that the $q$NEHVI quickly identifies the Pareto frontier and most of its evaluations are very close to the Pareto frontier. $q$NParEGO also identifies has many observations close to the Pareto frontier, but relies on optimizing random scalarizations, which is a less principled way of optimizing the Pareto front compared to $q$NEHVI, which explicitly attempts focuses on improving the Pareto front. Sobol generates random points and has few points close to the Pareto front.
+
+# In[23]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+from matplotlib.cm import ScalarMappable
+fig, axes = plt.subplots(1, 3, figsize=(20,6))
+algos = ["Sobol", "qNParEGO", "qNEHVI"]
+outcomes_list = [sobol_outcomes, parego_outcomes, ehvi_outcomes]
+cm = plt.cm.get_cmap('viridis')
+BATCH_SIZE = 1
+
+n_results = N_BATCH*BATCH_SIZE + N_INIT
+batch_number = torch.cat([torch.zeros(N_INIT), torch.arange(1, N_BATCH+1).repeat(BATCH_SIZE, 1).t().reshape(-1)]).numpy()
+for i, train_obj in enumerate(outcomes_list):
+    x = i
+    sc = axes[x].scatter(train_obj[:n_results, 0], train_obj[:n_results,1], c=batch_number[:n_results], alpha=0.8)
+    axes[x].set_title(algos[i])
+    axes[x].set_xlabel("Objective 1")
+    axes[x].set_xlim(-150, 5)
+    axes[x].set_ylim(-15, 0)
+axes[0].set_ylabel("Objective 2")
+norm = plt.Normalize(batch_number.min(), batch_number.max())
+sm =  ScalarMappable(norm=norm, cmap=cm)
+sm.set_array([])
+fig.subplots_adjust(right=0.9)
+cbar_ax = fig.add_axes([0.93, 0.15, 0.01, 0.7])
+cbar = fig.colorbar(sm, cax=cbar_ax)
+cbar.ax.set_title("Iteration")
+
+
+# # Hypervolume statistics
+# The hypervolume of the space dominated by points that dominate the reference point.
+
+# #### Plot the results
+# The plot below shows a common metric of multi-objective optimization performance when the true Pareto frontier is known:  the log difference between the hypervolume of the true Pareto front and the hypervolume of the approximate Pareto front identified by each algorithm. The log hypervolume difference is plotted at each step of the optimization for each of the algorithms.
+# 
+# The plot show that $q$NEHVI vastly outperforms $q$NParEGO which outperforms the Sobol baseline.
+
+# In[24]:
+
+
+import numpy as np
+from matplotlib import pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+iters = np.arange(1, N_BATCH + 1)
+log_hv_difference_sobol = np.log10(branin_currin.max_hv - np.asarray(sobol_hv_list))[:N_BATCH + 1]
+log_hv_difference_parego = np.log10(branin_currin.max_hv - np.asarray(parego_hv_list))[:N_BATCH + 1]
+log_hv_difference_ehvi = np.log10(branin_currin.max_hv - np.asarray(ehvi_hv_list))[:N_BATCH + 1]
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+ax.plot(iters, log_hv_difference_sobol, label="Sobol", linewidth=1.5)
+ax.plot(iters, log_hv_difference_parego, label="qNParEGO", linewidth=1.5)
+ax.plot(iters, log_hv_difference_ehvi, label="qNEHVI", linewidth=1.5)
+ax.set(xlabel='number of observations (beyond initial points)', ylabel='Log Hypervolume Difference')
+ax.legend(loc="lower right")
+
+
+# In[ ]:
+
+
+
+
