6e96237544d43c509dc043fee1d453bf63f3df9a
Sait Cakmak <saitcakmak@meta.com>
2024-10-15 10:43:07 -0700

    Support `optimize_acqf_mixed_alternating` in `optimizer_argparse` (#2881)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2881
    
    Adds support for optimizer argument parsing for newly open-sourced `optimize_acqf_mixed_alternating` and converts some previously silently ignored args into exceptions.
    
    Reviewed By: esantorella
    
    Differential Revision: D64400034
    
    fbshipit-source-id: c5dcf19ea1e6e9621e7bb91ba3e32daa17190c28

--- ax/models/torch/botorch_modular/optimizer_argparse.py
+++ ax/models/torch/botorch_modular/optimizer_argparse.py
@@ -11,6 +11,7 @@ from __future__ import annotations
 from typing import Any, TypeVar, Union
 
 import torch
+from ax.exceptions.core import UnsupportedError
 from ax.utils.common.constants import Keys
 from ax.utils.common.typeutils import _argparse_type_encoder
 from botorch.acquisition.acquisition import AcquisitionFunction
@@ -55,12 +56,17 @@ def _argparse_base(
 
     Args:
         acqf: The acquisition function being optimized.
-        optimizer: one of "optimize_acqf",
-            "optimize_acqf_discrete_local_search", "optimize_acqf_discrete", or
-            "optimize_acqf_mixed". This is generally chosen by
-            `Acquisition.optimize`.
+        optimizer: The optimizer to parse args for. Typically chosen by
+            `Acquisition.optimize`. Must be one of:
+            - "optimize_acqf",
+            - "optimize_acqf_discrete_local_search",
+            - "optimize_acqf_discrete",
+            - "optimize_acqf_homotopy",
+            - "optimize_acqf_mixed",
+            - "optimize_acqf_mixed_alternating".
         sequential: Whether we choose one candidate at a time in a sequential
-            manner. Ignored unless the optimizer is `optimize_acqf`.
+            manner. `sequential=False` is not supported by optimizers other than
+            `optimize_acqf` and will lead to an error.
         num_restarts: The number of starting points for multistart acquisition
             function optimization. Ignored if the optimizer is
             `optimize_acqf_discrete`.
@@ -72,7 +78,7 @@ def _argparse_base(
         batch_limit: The size of mini-batches used while optimizing the `acqf`.
             This helps reduce peak memory usage. Ignored if the optimizer is
             `optimize_acqf_discrete` or `optimize_acqf_discrete_local_search`.
-    optimizer_options: An optional dictionary of optimizer options. This may
+        optimizer_options: An optional dictionary of optimizer options. This may
             include overrides for the above options (some of these under an `options`
             dictionary) or any other option that is accepted by the optimizer. See
             the docstrings in `botorch/optim/optimize.py` for supported options.
@@ -90,8 +96,9 @@ def _argparse_base(
         "optimize_acqf",
         "optimize_acqf_discrete_local_search",
         "optimize_acqf_discrete",
-        "optimize_acqf_mixed",
         "optimize_acqf_homotopy",
+        "optimize_acqf_mixed",
+        "optimize_acqf_mixed_alternating",
     ]
     if optimizer not in supported_optimizers:
         raise ValueError(
@@ -100,25 +107,37 @@ def _argparse_base(
         )
     provided_options = optimizer_options if optimizer_options is not None else {}
 
-    # optimize_acqf_discrete only accepts 'choices', 'max_batch_size', 'unique'
+    # Construct arguments from options that are not `provided_options`.
     if optimizer == "optimize_acqf_discrete":
-        return provided_options
+        # `optimize_acqf_discrete` only accepts 'choices', 'max_batch_size', 'unique'.
+        options = {}
+    else:
+        options = {
+            "num_restarts": num_restarts,
+            "raw_samples": raw_samples,
+        }
 
-    # construct arguments from options that are not `provided_options`
-    options = {
-        "num_restarts": num_restarts,
-        "raw_samples": raw_samples,
-    }
-    # if not, 'options' will be silently ignored
-    if optimizer in ["optimize_acqf", "optimize_acqf_mixed", "optimize_acqf_homotopy"]:
+    if optimizer in [
+        "optimize_acqf",
+        "optimize_acqf_homotopy",
+        "optimize_acqf_mixed",
+        "optimize_acqf_mixed_alternating",
+    ]:
         options["options"] = {
             "init_batch_limit": init_batch_limit,
             "batch_limit": batch_limit,
             **provided_options.get("options", {}),
         }
+    # Error out if options are specified for an optimizer that does not support the arg.
+    elif "options" in provided_options:
+        raise UnsupportedError(
+            f"`{optimizer=}` does not support the `options` argument."
+        )
 
     if optimizer == "optimize_acqf":
         options["sequential"] = sequential
+    elif sequential is False:
+        raise UnsupportedError(f"`{optimizer=}` does not support `sequential=False`.")
 
     options.update(**{k: v for k, v in provided_options.items() if k != "options"})
     return options
