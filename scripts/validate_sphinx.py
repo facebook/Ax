5e2b3c1b3669e117341875ff469b2bcb600d009f
Miles Olson <mpolson64@meta.com>
2024-10-23 07:50:35 -0700

    Reapply codemod pep585_imports
    Summary: I noticed some files weren't obeying this, seems like these probably had diffs in flight that improperly merged and undid some changes from D60976146. No matter, just reran the codemod.
    
    Differential Revision: D64833627

--- scripts/validate_sphinx.py
+++ scripts/validate_sphinx.py
@@ -8,7 +8,6 @@ import argparse
 import os
 import pkgutil
 import re
-from typing import Set
 
 
 # Paths are relative to top-level Ax directory (which is passed into fxn below)
@@ -31,7 +30,7 @@ EXCLUDE_MODULES = {
 
 # NOTE: Can't use set[str] here due to internal call site of this module
 # on an ancient (<py3.9) python version.
-def parse_rst(rst_filename: str) -> Set[str]:
+def parse_rst(rst_filename: str) -> set[str]:
     """Extract automodule directives from rst."""
     ret = set()
     with open(rst_filename, "r") as f:
