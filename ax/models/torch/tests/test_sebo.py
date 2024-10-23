02c0ce52b7eeb57e4352f3341a5ee7738edd7f4b
David Eriksson <deriksson@meta.com>
2024-10-23 05:32:01 -0700

    SEBO bug fixes (#2935)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/2935
    
    This fixes a bunch of bugs in SEBO
    
    Reviewed By: bletham
    
    Differential Revision: D60089765
    
    fbshipit-source-id: 231db9b67a0bce7ab9a99a2c03e8c617b488a408

--- ax/models/torch/tests/test_sebo.py
+++ ax/models/torch/tests/test_sebo.py
@@ -10,6 +10,7 @@ from __future__ import annotations
 
 import dataclasses
 import functools
+import warnings
 from typing import Any
 from unittest import mock
 from unittest.mock import Mock
@@ -17,15 +18,18 @@ from unittest.mock import Mock
 import torch
 from ax.core.search_space import SearchSpaceDigest
 from ax.models.torch.botorch_modular.acquisition import Acquisition
-from ax.models.torch.botorch_modular.sebo import L1_norm_func, SEBOAcquisition
+from ax.models.torch.botorch_modular.sebo import (
+    clamp_to_target,
+    get_batch_initial_conditions,
+    L1_norm_func,
+    SEBOAcquisition,
+)
 from ax.models.torch.botorch_modular.surrogate import Surrogate
 from ax.models.torch_base import TorchOptConfig
 from ax.utils.common.constants import Keys
 from ax.utils.common.testutils import TestCase
 from ax.utils.common.typeutils import not_none
 from ax.utils.testing.mock import fast_botorch_optimize
-from botorch.acquisition import PosteriorMean
-from botorch.acquisition.monte_carlo import qNoisyExpectedImprovement
 from botorch.acquisition.multi_objective.monte_carlo import (
     qNoisyExpectedHypervolumeImprovement,
 )
@@ -33,9 +37,7 @@ from botorch.acquisition.penalized import L0Approximation
 from botorch.models.deterministic import GenericDeterministicModel
 from botorch.models.gp_regression import SingleTaskGP
 from botorch.models.model import ModelList
-from botorch.optim import Homotopy, HomotopyParameter, LinearHomotopySchedule
 from botorch.utils.datasets import SupervisedDataset
-from torch.nn import Parameter
 
 
 SEBOACQUISITION_PATH: str = SEBOAcquisition.__module__
@@ -103,18 +105,25 @@ class TestSebo(TestCase):
             linear_constraints=self.linear_constraints,
             fixed_features=self.fixed_features,
         )
+        self.torch_opt_config_2 = TorchOptConfig(
+            objective_weights=self.objective_weights,
+            objective_thresholds=self.objective_thresholds,
+            pending_observations=self.pending_observations,
+        )
 
     def get_acquisition_function(
         self,
         fixed_features: dict[int, float] | None = None,
         options: dict[str, str | float] | None = None,
+        torch_opt_config: TorchOptConfig | None = None,
     ) -> SEBOAcquisition:
         return SEBOAcquisition(
             botorch_acqf_class=qNoisyExpectedHypervolumeImprovement,
             surrogates={Keys.ONLY_SURROGATE: self.surrogates},
             search_space_digest=self.search_space_digest,
             torch_opt_config=dataclasses.replace(
-                self.torch_opt_config, fixed_features=fixed_features or {}
+                torch_opt_config or self.torch_opt_config,
+                fixed_features=fixed_features or {},
             ),
             options=options or self.options,
         )
@@ -129,11 +138,12 @@ class TestSebo(TestCase):
         self.assertIsInstance(model_list, ModelList)
         self.assertIsInstance(model_list.models[0], SingleTaskGP)
         self.assertIsInstance(model_list.models[1], GenericDeterministicModel)
-        self.assertEqual(acquisition1.det_metric_indx, -1)
 
         # Check right penalty term is instantiated
         self.assertEqual(acquisition1.penalty_name, "L0_norm")
         self.assertIsInstance(model_list.models[1]._f, L0Approximation)
+        # `a` needs to be set to something small for the pruning to work as expected
+        self.assertEqual(model_list.models[-1]._f.a, 1e-6)
 
         # Check transformed objective threshold
         self.assertTrue(
@@ -141,7 +151,7 @@ class TestSebo(TestCase):
                 # pyre-fixme[6]: For 2nd argument expected `Tensor` but got `int`.
                 acquisition1.acqf.ref_point[-1],
                 # pyre-fixme[6]: For 2nd argument expected `Tensor` but got `int`.
-                -1 * self.objective_thresholds_sebo[-1],
+                -self.objective_thresholds_sebo[-1],
             )
         )
         self.assertTrue(
@@ -190,6 +200,24 @@ class TestSebo(TestCase):
         with self.assertRaisesRegex(ValueError, "please provide target point."):
             self.get_acquisition_function(options={"penalty": "L1_norm"})
 
+        # Cache root catches
+        with warnings.catch_warnings(record=True) as ws:
+            self.get_acquisition_function(
+                fixed_features=self.fixed_features,
+                options={"cache_root": True, "target_point": self.target_point},
+            )
+            self.assertEqual(len(ws), 1)
+            self.assertEqual(
+                "SEBO doesn't support `cache_root=True`. Changing it to `False`.",
+                str(ws[0].message),
+            )
+
+        # Test with no outcome constraints
+        self.get_acquisition_function(
+            options={"target_point": self.target_point},
+            torch_opt_config=self.torch_opt_config_2,
+        )
+
     @mock.patch(f"{ACQUISITION_PATH}.optimize_acqf")
     def test_optimize_l1(self, mock_optimize_acqf: Mock) -> None:
         mock_optimize_acqf.return_value = (
@@ -217,52 +245,6 @@ class TestSebo(TestCase):
         self.assertEqual(kwargs["num_restarts"], self.optimizer_options["num_restarts"])
         self.assertEqual(kwargs["raw_samples"], self.optimizer_options["raw_samples"])
 
-    @mock.patch(
-        f"{SEBOACQUISITION_PATH}.get_batch_initial_conditions", return_value=None
-    )
-    @mock.patch(f"{SEBOACQUISITION_PATH}.Homotopy")
-    def test_optimize_l0_homotopy(
-        self,
-        mock_homotopy: Mock,
-        mock_get_batch_initial_conditions: Mock,
-    ) -> None:
-        tkwargs: dict[str, Any] = {"dtype": torch.double}
-        acquisition = self.get_acquisition_function(
-            fixed_features=self.fixed_features,
-            options={"penalty": "L0_norm", "target_point": self.target_point},
-        )
-        # overwrite acqf to validate homotopy
-        # pyre-fixme[61]: `p` is undefined, or not always defined.
-        # pyre-fixme[6]: For 1st argument expected `(Tensor) -> Tensor` but got `(x:
-        #  Any) -> int`.
-        model = GenericDeterministicModel(f=lambda x: 5 - (x - p) ** 2)
-        acqf = PosteriorMean(model=model)
-        acquisition.acqf = acqf
-
-        p = Parameter(-2 * torch.ones(1, **tkwargs))
-        hp = HomotopyParameter(
-            parameter=p,
-            schedule=LinearHomotopySchedule(start=4, end=0, num_steps=5),
-        )
-        mock_homotopy.return_value = Homotopy(homotopy_parameters=[hp])
-
-        search_space_digest = SearchSpaceDigest(
-            feature_names=["a"],
-            bounds=[(-10.0, 5.0)],
-        )
-        candidate, acqf_val, weights = acquisition._optimize_with_homotopy(
-            n=1,
-            search_space_digest=search_space_digest,
-            optimizer_options={
-                "num_restarts": 2,
-                "sequential": True,
-                "raw_samples": 16,
-            },
-        )
-        self.assertTrue(torch.allclose(candidate, torch.zeros(1, **tkwargs)))
-        self.assertTrue(torch.allclose(acqf_val, 5 * torch.ones(1, **tkwargs)))
-        self.assertEqual(weights, torch.ones(1, **tkwargs))
-
     @mock.patch(f"{SEBOACQUISITION_PATH}.optimize_acqf_homotopy")
     def test_optimize_l0(self, mock_optimize_acqf_homotopy: Mock) -> None:
         mock_optimize_acqf_homotopy.return_value = (
@@ -276,8 +258,6 @@ class TestSebo(TestCase):
         acquisition.optimize(
             n=2,
             search_space_digest=self.search_space_digest,
-            # does not support in homotopy now
-            # inequality_constraints=self.inequality_constraints,
             fixed_features=self.fixed_features,
             rounding_func=self.rounding_func,
             optimizer_options=self.optimizer_options,
@@ -320,7 +300,7 @@ class TestSebo(TestCase):
         with self.assertRaisesRegex(
             NotImplementedError,
             "Homotopy does not support optimization with inequality "
-            + "constraints. Use L1 penalty norm instead.",
+            "constraints. Use L1 penalty norm instead.",
         ):
             acquisition.optimize(
                 n=2,
@@ -331,16 +311,58 @@ class TestSebo(TestCase):
                 optimizer_options=self.optimizer_options,
             )
 
-        # assert error when using a wrong botorch_acqf_class
-        with self.assertRaisesRegex(
-            ValueError, "botorch_acqf_class must be qEHVI to use SEBO"
-        ):
-            acquisition = SEBOAcquisition(
-                botorch_acqf_class=qNoisyExpectedImprovement,
-                surrogates={Keys.ONLY_SURROGATE: self.surrogates},
-                search_space_digest=self.search_space_digest,
-                torch_opt_config=dataclasses.replace(
-                    self.torch_opt_config, fixed_features=self.fixed_features
-                ),
-                options=self.options,
+    def test_clamp_to_target(self) -> None:
+        X = torch.tensor(
+            [[0.5, 0.01, 0.5], [0.05, 0.5, 0.95], [0.1, 0.02, 0.06]], **self.tkwargs
+        )
+        X_true = torch.tensor(
+            [[0.5, 0, 0.5], [0, 0.5, 0.95], [0.1, 0, 0.06]], **self.tkwargs
+        )
+        self.assertTrue(
+            torch.allclose(
+                clamp_to_target(X, torch.zeros(1, 3, **self.tkwargs), 0.05), X_true
             )
+        )
+
+    @mock.patch(f"{SEBOACQUISITION_PATH}.optimize_acqf_homotopy")
+    @mock.patch(
+        f"{SEBOACQUISITION_PATH}.get_batch_initial_conditions",
+        wraps=get_batch_initial_conditions,
+    )
+    def test_get_batch_initial_conditions(
+        self, mock_get_batch_initial_conditions: Mock, mock_optimize_acqf_homotopy: Mock
+    ) -> None:
+        mock_optimize_acqf_homotopy.return_value = (
+            torch.tensor([[1.0, 1.0, 1.0], [2.0, 2.0, 2.0]], dtype=torch.double),
+            torch.tensor([1.0, 2.0], dtype=torch.double),
+        )
+        acquisition = self.get_acquisition_function(
+            fixed_features=self.fixed_features,
+            options={"target_point": self.target_point},
+            torch_opt_config=self.torch_opt_config_2,
+        )
+        acquisition.optimize(
+            n=2,
+            search_space_digest=self.search_space_digest,
+            fixed_features=self.fixed_features,
+            rounding_func=self.rounding_func,
+            optimizer_options={Keys.NUM_RESTARTS: 3, Keys.RAW_SAMPLES: 32},
+        )
+        call_args = mock_get_batch_initial_conditions.call_args[1]
+        self.assertTrue(
+            torch.equal(
+                call_args["X_pareto"],
+                torch.tensor([[1.0, 2.0, 3.0]], dtype=torch.double),
+            )
+        )
+        self.assertTrue(torch.equal(call_args["target_point"], self.target_point))
+        self.assertEqual(call_args["raw_samples"], 32)
+        self.assertEqual(call_args["num_restarts"], 3)
+        # Check the batch initial conditions
+        batch_initial_conditions = mock_optimize_acqf_homotopy.call_args[1][
+            "batch_initial_conditions"
+        ]
+        self.assertEqual(batch_initial_conditions.shape, torch.Size([3, 1, 3]))
+        self.assertTrue(torch.all(batch_initial_conditions[:1] != 1.0))
+        self.assertTrue(torch.all(batch_initial_conditions[1:, :, 0] == 1.0))
+        self.assertTrue(torch.all(batch_initial_conditions[1:, :, 1:] != 1.0))
