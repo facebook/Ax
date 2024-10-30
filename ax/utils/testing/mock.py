d224b01f6cacf0bee5e71429d8e1ab027dc6390e
Sait Cakmak <saitcakmak@meta.com>
2024-10-30 07:48:44 -0700

    Add `optimize_acqf_mixed_alternating` to `mock_botorch_optimize_context_manager` & reduce duplication with `mock_optimize_context_manager` (#2973)
    Summary:
    
    A previous diff added mixed optimizer to MBM. This diff adds it to optimizer mocks.
    
    `mock_botorch_optimize_context_manager` had a good bit of overlap with BoTorch's `mock_optimize_context_manager`, which is also cleaned up in this diff. It now uses `mock_optimize_context_manager` and adds additional mocks on top of that.
    
    Differential Revision: D65067691

--- ax/utils/testing/mock.py
+++ ax/utils/testing/mock.py
@@ -12,12 +12,8 @@ from typing import Any
 from unittest import mock
 
 from botorch.fit import fit_fully_bayesian_model_nuts
-from botorch.generation.gen import minimize_with_timeout
-from botorch.optim.initializers import (
-    gen_batch_initial_conditions,
-    gen_one_shot_kg_initial_conditions,
-)
-from scipy.optimize import OptimizeResult
+from botorch.optim.optimize_mixed import optimize_acqf_mixed_alternating
+from botorch.test_utils.mock import mock_optimize_context_manager
 from torch import Tensor
 
 
@@ -29,62 +25,34 @@ def mock_botorch_optimize_context_manager(
     Currently, the primary tactic is to force the underlying scipy methods to
     stop after just one iteration.
 
+    This context manager uses BoTorch's `mock_optimize_context_manager`, and
+    adds some additional mocks that are not possible to cover in BoTorch due to
+    the need to mock the functions where they are used.
+
     Args:
         force: If True will not raise an AssertionError if no mocks are called.
             USE RESPONSIBLY.
     """
 
-    def one_iteration_minimize(*args: Any, **kwargs: Any) -> OptimizeResult:
-        if kwargs["options"] is None:
-            kwargs["options"] = {}
-
-        kwargs["options"]["maxiter"] = 1
-        return minimize_with_timeout(*args, **kwargs)
-
-    def minimal_gen_ics(*args: Any, **kwargs: Any) -> Tensor:
-        kwargs["num_restarts"] = 2
-        kwargs["raw_samples"] = 4
-
-        return gen_batch_initial_conditions(*args, **kwargs)
-
-    def minimal_gen_os_ics(*args: Any, **kwargs: Any) -> Tensor | None:
-        kwargs["num_restarts"] = 2
-        kwargs["raw_samples"] = 4
-
-        return gen_one_shot_kg_initial_conditions(*args, **kwargs)
-
     def minimal_fit_fully_bayesian(*args: Any, **kwargs: Any) -> None:
         fit_fully_bayesian_model_nuts(*args, **_get_minimal_mcmc_kwargs(**kwargs))
 
+    def minimal_mixed_optimizer(*args: Any, **kwargs: Any) -> tuple[Tensor, Tensor]:
+        # BoTorch's `mock_optimize_context_manager` also has some mocks for this,
+        # but the full set of mocks applied here cannot be covered by that.
+        kwargs["raw_samples"] = 2
+        kwargs["num_restarts"] = 1
+        kwargs["options"].update(
+            {
+                "maxiter_alternating": 1,
+                "maxiter_continuous": 1,
+                "maxiter_init": 1,
+                "maxiter_discrete": 1,
+            }
+        )
+        return optimize_acqf_mixed_alternating(*args, **kwargs)
+
     with ExitStack() as es:
-        mock_generation = es.enter_context(
-            mock.patch(
-                "botorch.generation.gen.minimize_with_timeout",
-                wraps=one_iteration_minimize,
-            )
-        )
-
-        mock_fit = es.enter_context(
-            mock.patch(
-                "botorch.optim.core.minimize_with_timeout",
-                wraps=one_iteration_minimize,
-            )
-        )
-
-        mock_gen_ics = es.enter_context(
-            mock.patch(
-                "botorch.optim.optimize.gen_batch_initial_conditions",
-                wraps=minimal_gen_ics,
-            )
-        )
-
-        mock_gen_os_ics = es.enter_context(
-            mock.patch(
-                "botorch.optim.optimize.gen_one_shot_kg_initial_conditions",
-                wraps=minimal_gen_os_ics,
-            )
-        )
-
         mock_mcmc_mbm = es.enter_context(
             mock.patch(
                 "ax.models.torch.botorch_modular.utils.fit_fully_bayesian_model_nuts",
@@ -92,17 +60,42 @@ def mock_botorch_optimize_context_manager(
             )
         )
 
+        mock_mixed_optimizer = es.enter_context(
+            mock.patch(
+                "ax.models.torch.botorch_modular.acquisition."
+                "optimize_acqf_mixed_alternating",
+                wraps=minimal_mixed_optimizer,
+            )
+        )
+
+        es.enter_context(mock_optimize_context_manager())
+
         yield
 
-    if (not force) and all(
-        mock_.call_count < 1
-        for mock_ in [
-            mock_generation,
-            mock_fit,
-            mock_gen_ics,
-            mock_gen_os_ics,
-            mock_mcmc_mbm,
-        ]
+        # Only raise if none of the BoTorch or Ax side mocks were called.
+        # We do this by catching the error that could be raised by the BoTorch
+        # context manager, and combining it with the signals from Ax side mocks.
+        try:
+            es.close()
+        except AssertionError as e:
+            # Check if the error is due to no BoTorch mocks being called.
+            if "No mocks were called" in str(e):
+                botorch_mocks_called = False
+            else:
+                raise
+        else:
+            botorch_mocks_called = True
+
+    if (
+        not force
+        and all(
+            mock_.call_count < 1
+            for mock_ in [
+                mock_mcmc_mbm,
+                mock_mixed_optimizer,
+            ]
+        )
+        and botorch_mocks_called is False
     ):
         raise AssertionError(
             "No mocks were called in the context manager. Please remove unused "
