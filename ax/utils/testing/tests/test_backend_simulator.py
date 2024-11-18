7526bcdcd46308b580e0f9300fa6ea958d1aeaaa
Elizabeth Santorella <santorella@meta.com>
2024-11-18 12:12:09 -0800

    Remove unused `_init_state` and `reset()` from `BackendSimulator` (#3075)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/3075
    
    Context: The `BackendSimulator` can be reset to its initial state. This functionality is not used. I would like to be able to serialize and deserialize `BackendSimulator`s in order to permit analysis of past simulations. The presence of `_init_state` makes this much harder, since there are then two states that need to be considered, the initial state and the current state.
    
    This PR:
    * Removes the attribute `_init_state` and the method `reset` from `BackendSimulator`
    
    Reviewed By: saitcakmak
    
    Differential Revision: D66100116
    
    fbshipit-source-id: 733fab50a7a082b11a149097f3f0d3e35e9ed556

--- ax/utils/testing/tests/test_backend_simulator.py
+++ ax/utils/testing/tests/test_backend_simulator.py
@@ -65,19 +65,6 @@ class BackendSimulatorTest(TestCase):
         self.assertEqual(sim.num_failed, 0)
         self.assertEqual(sim.num_completed, 3)
 
-        # test reset
-        sim.max_concurrency = 3
-        sim.time_scaling = 2.0
-        sim.failure_rate, 0.5
-        sim.reset()
-        self.assertEqual(sim.max_concurrency, 2)
-        self.assertEqual(sim.time_scaling, 1.0)
-        self.assertEqual(sim.failure_rate, 0.0)
-        self.assertEqual(sim.num_queued, 0)
-        self.assertEqual(sim.num_running, 0)
-        self.assertEqual(sim.num_failed, 0)
-        self.assertEqual(sim.num_completed, 0)
-
         # test load state
         sim2 = BackendSimulator.from_state(state)
         self.assertEqual(sim2.max_concurrency, 2)
