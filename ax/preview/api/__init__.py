ed3ba5c1ee0e1d46c8c8e42063d55ee350185242
Miles Olson <mpolson64@meta.com>
2024-11-18 14:12:03 -0800

    Ax API entry point class (#3054)
    Summary:
    
    This replaces AxClient, offering a much more streamlined and opinionated vocabulary for conducting experiments as well as a unification of our ask-tell and Metric/Runner style experimentation APIs.
    
    The methods provided here provide are complete in that using just these an Ax user is able to successfully utilize all Ax 1.0 launch features without having to access any implementation details, ax.core classes, etc directly.
    
    Note: There are a few methods prefixed with "set_" that we will not consider API (because we do not want to expose their arguments to the API) but will leave as "breadcrumbs" for contributors or those otherwise using Ax as a research platform and interested in extending the library.
    
    Differential Revision: D63836188

--- ax/preview/api/__init__.py
+++ ax/preview/api/__init__.py
@@ -3,3 +3,32 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from ax.preview.api.client import Client
+from ax.preview.api.configs import (
+    ChoiceParameterConfig,
+    DatabaseConfig,
+    ExperimentConfig,
+    GenerationStrategyConfig,
+    OrchestrationConfig,
+    ParameterScaling,
+    ParameterType,
+    RangeParameterConfig,
+)
+from ax.preview.api.types import TOutcome, TParameterization
+
+__all__ = [
+    "Client",
+    "ChoiceParameterConfig",
+    "DatabaseConfig",
+    "ExperimentConfig",
+    "GenerationStrategyConfig",
+    "OrchestrationConfig",
+    "ParameterScaling",
+    "ParameterType",
+    "RangeParameterConfig",
+    "TOutcome",
+    "TParameterization",
+]
--- ax/preview/api/protocols/__init__.py
+++ ax/preview/api/protocols/__init__.py
@@ -3,3 +3,13 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from ax.preview.api.protocols.metric import IMetric
+from ax.preview.api.protocols.runner import IRunner
+
+__all__ = [
+    "IMetric",
+    "IRunner",
+]
