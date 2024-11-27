326700da5794c008fc209c5b696340812c327a51
Elizabeth Santorella <santorella@meta.com>
2024-11-27 14:33:56 -0800

    Reap legacy `recommend_best_out_of_sample_point` (#3126)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/3126
    
    Context: `recommend_best_out_of_sample_point` was used with legacy models and would error with any model that did not have an attribute `_get_best_point_acqf`. Currently, no model has that attribute.
    
    This PR: Removes the function
    
    Reviewed By: saitcakmak
    
    Differential Revision: D66551384
    
    fbshipit-source-id: 5376fb54aad1e9d3ed9b552eb29b5aaaafdc3c17

--- ax/models/torch/botorch_defaults.py
+++ ax/models/torch/botorch_defaults.py
@@ -13,13 +13,11 @@ from random import randint
 from typing import Any, Protocol
 
 import torch
-from ax.models.model_utils import best_observed_point, get_observed
-from ax.models.torch.utils import _to_inequality_constraints
+from ax.models.model_utils import best_observed_point
 from ax.models.torch_base import TorchModel
 from ax.models.types import TConfig
 from botorch.acquisition import get_acquisition_function
 from botorch.acquisition.acquisition import AcquisitionFunction
-from botorch.acquisition.fixed_feature import FixedFeatureAcquisitionFunction
 from botorch.acquisition.objective import ConstrainedMCObjective, GenericMCObjective
 from botorch.acquisition.utils import get_infeasible_cost
 from botorch.exceptions.errors import UnsupportedError
@@ -577,107 +575,6 @@ def recommend_best_observed_point(
     # pyre-fixme[16]: Item `ndarray` of `Union[ndarray[typing.Any, typing.Any],
     #  Tensor]` has no attribute `to`.
     return x_best.to(dtype=model.dtype, device=torch.device("cpu"))
-
-
-def recommend_best_out_of_sample_point(
-    model: TorchModel,
-    bounds: list[tuple[float, float]],
-    objective_weights: Tensor,
-    outcome_constraints: tuple[Tensor, Tensor] | None = None,
-    linear_constraints: tuple[Tensor, Tensor] | None = None,
-    fixed_features: dict[int, float] | None = None,
-    model_gen_options: TConfig | None = None,
-    target_fidelities: dict[int, float] | None = None,
-) -> Tensor | None:
-    """
-    Identify the current best point by optimizing the posterior mean of the model.
-    This is "out-of-sample" because it considers un-observed designs as well.
-
-    Return None if no such point can be identified.
-
-    Args:
-        model: A TorchModel.
-        bounds: A list of (lower, upper) tuples for each column of X.
-        objective_weights: The objective is to maximize a weighted sum of
-            the columns of f(x). These are the weights.
-        outcome_constraints: A tuple of (A, b). For k outcome constraints
-            and m outputs at f(x), A is (k x m) and b is (k x 1) such that
-            A f(x) <= b.
-        linear_constraints: A tuple of (A, b). For k linear constraints on
-            d-dimensional x, A is (k x d) and b is (k x 1) such that
-            A x <= b.
-        fixed_features: A map {feature_index: value} for features that
-            should be fixed to a particular value in the best point.
-        model_gen_options: A config dictionary that can contain
-            model-specific options. See `TorchOptConfig` for details.
-        target_fidelities: A map {feature_index: value} of fidelity feature
-            column indices to their respective target fidelities. Used for
-            multi-fidelity optimization.
-
-    Returns:
-        A d-array of the best point, or None if no feasible point exists.
-    """
-    options = model_gen_options or {}
-    fixed_features = fixed_features or {}
-    acf_options = options.get("acquisition_function_kwargs", {})
-    optimizer_options = options.get("optimizer_kwargs", {})
-
-    X_observed = get_observed(
-        Xs=model.Xs,  # pyre-ignore: [16]
-        objective_weights=objective_weights,
-        outcome_constraints=outcome_constraints,
-    )
-
-    if hasattr(model, "_get_best_point_acqf"):
-        acq_function, non_fixed_idcs = model._get_best_point_acqf(  # pyre-ignore: [16]
-            X_observed=X_observed,
-            objective_weights=objective_weights,
-            mc_samples=acf_options.get("mc_samples", 512),
-            fixed_features=fixed_features,
-            target_fidelities=target_fidelities,
-            outcome_constraints=outcome_constraints,
-            seed_inner=acf_options.get("seed_inner", None),
-            qmc=acf_options.get("qmc", True),
-        )
-    else:
-        raise RuntimeError("The model should implement _get_best_point_acqf.")
-
-    inequality_constraints = _to_inequality_constraints(linear_constraints)
-    # TODO: update optimizers to handle inequality_constraints
-    # (including transforming constraints b/c of fixed features)
-    if inequality_constraints is not None:
-        raise UnsupportedError("Inequality constraints are not supported!")
-
-    return_best_only = optimizer_options.get("return_best_only", True)
-    bounds_ = torch.tensor(bounds, dtype=model.dtype, device=model.device)
-    bounds_ = bounds_.transpose(-1, -2)
-    if non_fixed_idcs is not None:
-        bounds_ = bounds_[..., non_fixed_idcs]
-
-    opt_options: dict[str, bool | float | int | str] = {
-        "batch_limit": 8,
-        "maxiter": 200,
-        "method": "L-BFGS-B",
-        "nonnegative": False,
-    }
-    opt_options.update(optimizer_options.get("options", {}))
-    candidates, _ = optimize_acqf(
-        acq_function=acq_function,
-        bounds=bounds_,
-        q=1,
-        num_restarts=optimizer_options.get("num_restarts", 60),
-        raw_samples=optimizer_options.get("raw_samples", 1024),
-        inequality_constraints=inequality_constraints,
-        fixed_features=None,  # handled inside the acquisition function
-        options=opt_options,
-        return_best_only=return_best_only,
-    )
-    rec_point = candidates.detach().cpu()
-    if isinstance(acq_function, FixedFeatureAcquisitionFunction):
-        rec_point = acq_function._construct_X_full(rec_point)
-    if return_best_only:
-        rec_point = rec_point.view(-1)
-    return rec_point
 
 
 def _get_model(
