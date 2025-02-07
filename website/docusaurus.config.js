2d0d2efbc65c5a874c3544a7df9c5d014c8882e4
Cristian Lara <cristianlara@meta.com>
2025-02-07 12:12:38 -0800

    Disable experimental optimizations (#3328)
    Summary:
    I previously added this to resolve issues with building when we have many versions of our docs. While this works locally we've been seeing [failures to build in GHA](https://github.com/facebook/Ax/actions/runs/13164146992/job/36740052914). Some debugging pointed to this being the issue, so let's disable for now. The Docusaurus team is actively working on speeding up build time and plans to roll out these optimizations as they become more stable.
    
    I tested this by adding a test workflow to build the website using these changes in a separate PR for debugging
    - PR: https://github.com/facebook/Ax/pull/3303
    - Successful workflow run: https://github.com/facebook/Ax/actions/runs/13206354540/job/36870297699
    
    See this commit for when I added these options: https://github.com/CristianLara/Ax/commit/d04a3b202a48f900e9cb7b6ff5849b8ecdbb5d6e
    
    See Docusaurus's "faster" initiative: https://github.com/facebook/docusaurus/issues/10556
    
    Pull Request resolved: https://github.com/facebook/Ax/pull/3328
    
    Reviewed By: saitcakmak
    
    Differential Revision: D69320211
    
    Pulled By: CristianLara
    
    fbshipit-source-id: ce72b3c389dd8694746fbdc106c1f1e43c7c1ca3

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -30,9 +30,6 @@ module.exports={
   },
   "onBrokenLinks": "throw",
   "onBrokenMarkdownLinks": "warn",
-  "future": {
-    "experimental_faster": true,
-  },
   "presets": [
     [
       "@docusaurus/preset-classic",
