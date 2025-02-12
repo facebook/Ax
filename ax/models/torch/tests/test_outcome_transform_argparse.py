4f0d86e7f354c95f8698d8a446e7ca1fa8e00eaf
Sam Daulton <sdaulton@meta.com>
2025-02-12 09:44:09 -0800

    add argparse for StratifiedStandardize (#3343)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/3343
    
    See title.
    
    Reviewed By: saitcakmak
    
    Differential Revision: D69466499
    
    fbshipit-source-id: ca58f6419ac52c36fc0a473126f361b6fb4ae134

--- ax/models/torch/tests/test_outcome_transform_argparse.py
+++ ax/models/torch/tests/test_outcome_transform_argparse.py
@@ -10,8 +10,13 @@ from ax.models.torch.botorch_modular.input_constructors.outcome_transform import
     outcome_transform_argparse,
 )
 from ax.utils.common.testutils import TestCase
-from botorch.models.transforms.outcome import OutcomeTransform, Standardize
-from botorch.utils.datasets import SupervisedDataset
+from botorch.models.transforms.outcome import (
+    OutcomeTransform,
+    Standardize,
+    StratifiedStandardize,
+)
+from botorch.utils.datasets import MultiTaskDataset, SupervisedDataset
+from pyre_extensions import assert_is_instance
 
 
 class DummyOutcomeTransform(OutcomeTransform):
@@ -60,3 +65,44 @@ class OutcomeTransformArgparseTest(TestCase):
         )
         self.assertEqual(outcome_transform_kwargs_a, {"m": 1})
         self.assertEqual(outcome_transform_kwargs_b, {"m": 10})
+
+    def test_argparse_stratified_standardize(self) -> None:
+        X = self.dataset.X
+        X[:5, 3] = 0
+        X[5:, 3] = 1
+        mt_dataset = MultiTaskDataset.from_joint_dataset(
+            dataset=self.dataset,
+            task_feature_index=3,
+            target_task_value=1,
+        )
+        outcome_transform_kwargs_a = outcome_transform_argparse(
+            StratifiedStandardize, dataset=mt_dataset
+        )
+        options_b = {
+            "stratification_idx": 2,
+            "task_values": torch.tensor([0, 3]),
+        }
+        outcome_transform_kwargs_b = outcome_transform_argparse(
+            StratifiedStandardize,
+            dataset=mt_dataset,
+            outcome_transform_options=options_b,
+        )
+        expected_options_a = {
+            "stratification_idx": 3,
+            "task_values": torch.tensor([0, 1]),
+        }
+        for expected_options, actual_options in zip(
+            (expected_options_a, options_b),
+            (outcome_transform_kwargs_a, outcome_transform_kwargs_b),
+        ):
+            self.assertEqual(len(actual_options), 2)
+            self.assertEqual(
+                actual_options["stratification_idx"],
+                expected_options["stratification_idx"],
+            )
+            self.assertTrue(
+                torch.equal(
+                    actual_options["task_values"],
+                    assert_is_instance(expected_options["task_values"], torch.Tensor),
+                )
+            )
