8f5ccd09bb6f5952d6a5b224396a0f044d4e5215
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2025-04-10 10:02:58 -0700

    Add missing Pyre mode headers] [batch:35/1680] [shard:6/N]
    Differential Revision: D72700687
    
    fbshipit-source-id: 216072c1742db3d79cf36241b995594b28a9e649

--- ax/api/utils/storage.py
+++ ax/api/utils/storage.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-unsafe
+
 from ax.api.configs import StorageConfig
 
 # Allow Ax to be used without SQLAlchemy.
