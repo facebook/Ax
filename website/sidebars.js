6b5bf218c350c824d37a09282538270df7f73b15
Paschal Igusti <paschali@meta.com>
2025-04-29 06:57:07 -0700

    Add MOO recipe (#3528)
    Summary:
    
    as titled - any edits are greatly appreciated!
    
    Reviewed By: mpolson64
    
    Differential Revision: D71397358

--- website/sidebars.js
+++ website/sidebars.js
@@ -56,6 +56,7 @@ export default {
       'recipes/tracking-metrics',
       'recipes/experiment-to-json',
       'recipes/experiment-to-sqlite',
+      'recipes/multi-objective-optimization',
     ],
   },
 };
