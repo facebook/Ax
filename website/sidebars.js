7d04cdcecc8a1358d12bf477bab62db2a0b11183
Miles Olson <mpolson64@fb.com>
2025-05-06 09:44:03 -0700

    Add Internal Organization of Ax: Experiment
    Summary:
    Originally written by Lena here https://docs.google.com/document/d/1fApiUgGx8TG58T2v4DM-ngAkXvQHi-eOX_h_LZliMWM/edit?tab=t.0
    
    WIP: Still need to add images
    
    Made some edits to the SS and Constraints section, and added paragraph about equality constraints
    
    Differential Revision: D74204968

--- website/sidebars.js
+++ website/sidebars.js
@@ -43,7 +43,7 @@ export default {
       type: 'category',
       label: 'Extending Ax',
       collapsed: false,
-      items: ['analyses'],
+      items: ['analyses', 'experiment'],
     },
     {
       type: 'category',
