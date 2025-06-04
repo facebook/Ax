993e7c57556e6bb6b9d260e1a6ea06ac2c116222
Cesar Cardoso <cesarcardoso@meta.com>
2025-06-04 06:22:45 -0700

    Refactor auxiliary experiment storage (again) (#3829)
    Summary: Pull Request resolved: https://github.com/facebook/Ax/pull/3829
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D74902347
    
    fbshipit-source-id: 9876ab3d27c040130ca4b3b0acf85a2e6b531747

--- ax/storage/sqa_store/decoder.py
+++ ax/storage/sqa_store/decoder.py
@@ -11,7 +11,7 @@ from collections import defaultdict, OrderedDict
 from enum import Enum
 from io import StringIO
 from logging import Logger
-from typing import Any, cast, Union
+from typing import cast, Union
 
 import pandas as pd
 from ax.analysis.analysis import AnalysisCard
@@ -105,9 +105,10 @@ class Decoder:
     def _auxiliary_experiments_by_purpose_from_experiment_sqa(
         self, experiment_sqa: SQAExperiment, reduced_state: bool = False
     ) -> dict[AuxiliaryExperimentPurpose, list[AuxiliaryExperiment]] | None:
-        auxiliary_experiments_by_purpose = None
+        auxiliary_experiments_by_purpose = {}
+
+        # Legacy logic
         if experiment_sqa.auxiliary_experiments_by_purpose:
-            auxiliary_experiments_by_purpose = {}
             aux_exps_dict = none_throws(experiment_sqa.auxiliary_experiments_by_purpose)
             for aux_exp_purpose_str, aux_exps_json in aux_exps_dict.items():
                 aux_exp_purpose = next(
@@ -115,20 +116,46 @@ class Decoder:
                     for member in self.config.auxiliary_experiment_purpose_enum
                     if member.value == aux_exp_purpose_str
                 )
-                auxiliary_experiments_by_purpose[aux_exp_purpose] = []
+                if aux_exp_purpose not in auxiliary_experiments_by_purpose:
+                    auxiliary_experiments_by_purpose[aux_exp_purpose] = []
                 for aux_exp_json in aux_exps_json:
                     # keeping this for backward compatibility since previously
                     # we used to save only the experiment name
                     if isinstance(aux_exp_json, str):
                         aux_exp_json = {"experiment_name": aux_exp_json}
-                    aux_experiment = auxiliary_experiment_from_json(
-                        json=aux_exp_json,
+                    aux_experiment = auxiliary_experiment_from_name(
+                        experiment_name=aux_exp_json["experiment_name"],
                         config=self.config,
+                        is_active=True,
                         reduced_state=reduced_state,
                     )
                     auxiliary_experiments_by_purpose[aux_exp_purpose].append(
                         aux_experiment
                     )
+
+        # New logic
+        if experiment_sqa.auxiliary_experiments:
+            for auxiliary_experiment_sqa in experiment_sqa.auxiliary_experiments:
+                purpose = self.config.auxiliary_experiment_purpose_enum(
+                    auxiliary_experiment_sqa.purpose
+                )
+                if purpose not in auxiliary_experiments_by_purpose:
+                    auxiliary_experiments_by_purpose[purpose] = []
+                # If the auxiliary experiment is already loaded, we don't need to
+                # load it again.
+                if any(
+                    auxiliary_experiment_sqa.source_experiment_id
+                    == aux_exp.experiment.db_id
+                    for aux_exp in auxiliary_experiments_by_purpose[purpose]
+                ):
+                    continue
+                aux_experiment = auxiliary_experiment_from_name(
+                    experiment_name=auxiliary_experiment_sqa.source_experiment.name,
+                    config=self.config,
+                    is_active=auxiliary_experiment_sqa.is_active,
+                    reduced_state=reduced_state,
+                )
+                auxiliary_experiments_by_purpose[purpose].append(aux_experiment)
         return auxiliary_experiments_by_purpose
 
     def _init_experiment_from_sqa(
@@ -1343,29 +1370,30 @@ def _get_scalarized_outcome_constraint_children_metrics(
     return metrics_sqa
 
 
-def auxiliary_experiment_from_json(
-    json: dict[str, Any],
+def auxiliary_experiment_from_name(
+    experiment_name: str,
     config: SQAConfig,
+    is_active: bool,
     reduced_state: bool = False,
 ) -> AuxiliaryExperiment:
     """
-    Load an ``AuxiliaryExperiment`` from JSON.
+    Load an ``AuxiliaryExperiment`` by name.
 
     Args:
-        json: A dictionary containing the JSON representation of an AuxiliaryExperiment.
+        experiment_name: Name of the auxiliary experiment.
         config: The SQAConfig object used to load the experiment.
 
     Returns:
-        An AuxiliaryExperiment object constructed from the JSON representation.
+        An AuxiliaryExperiment object constructed from the given experiment.
     """
 
     from ax.storage.sqa_store.load import load_experiment
 
     experiment = load_experiment(
-        json.get("experiment_name"),
+        experiment_name,
         config=config,
         skip_runners_and_metrics=True,
         load_auxiliary_experiments=False,
         reduced_state=reduced_state,
     )
-    return AuxiliaryExperiment(experiment)
+    return AuxiliaryExperiment(experiment, is_active=is_active)
