46cb9c15f42aaae8970edd242e939c17fe817b91
Cesar Cardoso <cesarcardoso@meta.com>
2025-06-17 22:39:59 -0700

    OverviewAnalysis (#3937)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/3937
    
    Create a top level analysis which is relevant on ALL Ax experiments. Dispatches to appropriate sub-analyses and combines cards into "Results", "Insights", and "Diagnostics" groups.
    
    Health checks to be added in a following diff.
    Diff after will replace all instances of flow-control based analysis dispatch (ie if/else choose_analysis_... methods) with the OverviewAnalysis
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D75984682
    
    fbshipit-source-id: 8de03409afd6888141c61386b2afbe149a07f290

---
+++ ax/analysis/tests/test_overview.py
@@ -0,0 +1,94 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from ax.analysis.overview import OverviewAnalysis
+from ax.api.client import Client
+from ax.api.configs import RangeParameterConfig
+from ax.utils.common.testutils import TestCase
+from ax.utils.testing.core_stubs import get_offline_experiments, get_online_experiments
+from ax.utils.testing.mock import mock_botorch_optimize
+from ax.utils.testing.modeling_stubs import get_default_generation_strategy_at_MBM_node
+
+
+class TestOverview(TestCase):
+    @mock_botorch_optimize
+    def test_compute(self) -> None:
+        # Set up a basic optimization with the Client
+        client = Client()
+
+        client.configure_experiment(
+            name="booth_function",
+            parameters=[
+                RangeParameterConfig(
+                    name="x1",
+                    bounds=(-10.0, 10.0),
+                    parameter_type="float",
+                ),
+                RangeParameterConfig(
+                    name="x2",
+                    bounds=(-10.0, 10.0),
+                    parameter_type="float",
+                ),
+            ],
+        )
+        client.configure_optimization(objective="-1 * booth")
+        # Iterate well into the BO phase
+        for _ in range(10):
+            for trial_index, parameters in client.get_next_trials(max_trials=1).items():
+                client.complete_trial(
+                    trial_index=trial_index,
+                    raw_data={
+                        # pyre-ignore[58]
+                        "booth": (parameters["x1"] + 2 * parameters["x2"] - 7) ** 2
+                        # pyre-ignore[58]
+                        + (2 * parameters["x1"] + parameters["x2"] - 5) ** 2
+                    },
+                )
+
+        card = OverviewAnalysis().compute(
+            experiment=client._experiment,
+            generation_strategy=client._generation_strategy,
+        )
+
+        self.assertEqual(len(card.flatten()), 9)
+
+        children_names = [child.name for child in card.children]
+        self.assertIn("ResultsAnalysis", children_names)
+        self.assertIn("InsightsAnalysis", children_names)
+        self.assertIn("DiagnosticAnalysis", children_names)
+
+    @mock_botorch_optimize
+    def test_online(self) -> None:
+        # Test MetricSummary can be computed for a variety of experiments which
+        # resemble those we see in an online setting.
+
+        analysis = OverviewAnalysis()
+
+        for experiment in get_online_experiments():
+            generation_strategy = get_default_generation_strategy_at_MBM_node(
+                experiment=experiment
+            )
+
+            _ = analysis.compute(
+                experiment=experiment, generation_strategy=generation_strategy
+            )
+
+    @mock_botorch_optimize
+    def test_offline(self) -> None:
+        # Test MetricSummary can be computed for a variety of experiments which
+        # resemble those we see in an offline setting.
+
+        analysis = OverviewAnalysis()
+
+        for experiment in get_offline_experiments():
+            generation_strategy = get_default_generation_strategy_at_MBM_node(
+                experiment=experiment
+            )
+
+            _ = analysis.compute(
+                experiment=experiment, generation_strategy=generation_strategy
+            )
