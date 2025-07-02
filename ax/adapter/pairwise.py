9bfe0054ad8eafa5c8a0832f2aa6bd5b0da4d68c
Jerry Lin <zylin@meta.com>
2025-07-02 16:58:10 -0700

    Replace PairwiseAdapter with TorchAdapter (#3986)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/3986
    
    Now we support handling of (pairwise) ranking datasets in `TorchAdapter` and can safely deprecate `PairwiseAdapter` in favor of `TorchAdapter` everywhere.
    
    Reviewed By: saitcakmak
    
    Differential Revision: D77683806
    
    fbshipit-source-id: 5c5247e92cad70dabe6b52b7205fc7214652947e

--- ax/adapter/pairwise.py
+++ ax/adapter/pairwise.py
@@ -6,91 +6,12 @@
 
 # pyre-strict
 
-from __future__ import annotations
-
-import numpy as np
-import torch
-from ax.adapter.adapter_utils import prep_pairwise_data
 from ax.adapter.torch import TorchAdapter
-from ax.core.observation import ObservationData, ObservationFeatures
-from ax.core.search_space import SearchSpaceDigest
-from ax.core.types import TCandidateMetadata
-from ax.utils.common.constants import Keys
-from botorch.utils.containers import DenseContainer
-from botorch.utils.datasets import SupervisedDataset
-from pyre_extensions import none_throws
 
 
 class PairwiseAdapter(TorchAdapter):
-    def _convert_observations(
-        self,
-        observation_data: list[ObservationData],
-        observation_features: list[ObservationFeatures],
-        outcomes: list[str],
-        parameters: list[str],
-        search_space_digest: SearchSpaceDigest | None,
-    ) -> tuple[
-        list[SupervisedDataset], list[str], list[list[TCandidateMetadata]] | None
-    ]:
-        """Converts observations to a dictionary of `Dataset` containers and (optional)
-        candidate metadata.
-        """
-        if len(observation_features) != len(observation_data):
-            raise ValueError("Observation features and data must have the same length!")
-        # pyre-fixme[6]: For 1st argument expected `Union[_SupportsArray[dtype[typing...
-        ordered_idx = np.argsort([od.trial_index for od in observation_features])
-        observation_features = [observation_features[i] for i in ordered_idx]
-        observation_data = [observation_data[i] for i in ordered_idx]
-
-        (
-            Xs,
-            Ys,
-            _,  # Yvars is not used here.
-            candidate_metadata_dict,
-            any_candidate_metadata_is_not_none,
-            trial_indices,
-        ) = self._extract_observation_data(
-            observation_data, observation_features, parameters
+    # pyre-ignore[2]: Missing parameter annotationa
+    def __init__(self, **kwargs) -> None:
+        raise DeprecationWarning(
+            "PairwiseAdapter is deprecated. Use TorchAdapter instead."
         )
-
-        datasets: list[SupervisedDataset] = []
-        candidate_metadata = []
-        for outcome in outcomes:
-            X = torch.stack(Xs[outcome], dim=0)
-            Y = torch.tensor(Ys[outcome], dtype=torch.long).unsqueeze(-1)
-            if outcome == Keys.PAIRWISE_PREFERENCE_QUERY.value:
-                dataset = prep_pairwise_data(
-                    X=X,
-                    Y=Y,
-                    group_indices=torch.tensor(none_throws(trial_indices)[outcome]),
-                    outcome=outcome,
-                    parameters=parameters,
-                )
-            else:  # pragma: no cover
-                event_shape = torch.Size([X.shape[-1]])
-                dataset_X = DenseContainer(X, event_shape=event_shape)
-                dataset = SupervisedDataset(
-                    X=dataset_X,
-                    Y=Y,
-                    feature_names=parameters,
-                    outcome_names=[outcome],
-                    group_indices=torch.tensor(trial_indices[outcome])
-                    if trial_indices is not None
-                    else None,
-                )
-
-            datasets.append(dataset)
-            candidate_metadata.append(candidate_metadata_dict[outcome])
-
-        if not any_candidate_metadata_is_not_none:
-            return datasets, outcomes, None
-
-        return datasets, outcomes, candidate_metadata
-
-    def _predict(
-        self,
-        observation_features: list[ObservationFeatures],
-        use_posterior_predictive: bool = False,
-    ) -> list[ObservationData]:
-        # TODO: Implement `_predict` to enable examining predicted effects
-        raise NotImplementedError
--- ax/adapter/tests/test_torch_adapter.py
+++ ax/adapter/tests/test_torch_adapter.py
@@ -13,6 +13,7 @@ from unittest import mock
 
 import numpy as np
 import torch
+from ax.adapter.adapter_utils import _binary_pref_to_comp_pair, _consolidate_comparisons
 from ax.adapter.base import Adapter
 from ax.adapter.cross_validation import cross_validate
 from ax.adapter.registry import MBM_X_trans
@@ -40,7 +41,7 @@ from ax.core.search_space import SearchSpace, SearchSpaceDigest
 from ax.core.types import ComparisonOp
 from ax.exceptions.core import DataRequiredError
 from ax.generators.torch.botorch_modular.generator import BoTorchGenerator
-from ax.generators.torch.botorch_modular.surrogate import SurrogateSpec
+from ax.generators.torch.botorch_modular.surrogate import Surrogate, SurrogateSpec
 from ax.generators.torch.botorch_modular.utils import ModelConfig
 from ax.generators.torch_base import TorchGenerator, TorchGenResults
 from ax.utils.common.constants import Keys
@@ -57,6 +58,11 @@ from ax.utils.testing.core_stubs import (
 from ax.utils.testing.mock import mock_botorch_optimize
 from ax.utils.testing.modeling_stubs import transform_1, transform_2
 from ax.utils.testing.preference_stubs import get_pbo_experiment
+from botorch.acquisition.logei import qLogNoisyExpectedImprovement
+from botorch.acquisition.preference import (
+    AnalyticExpectedUtilityOfBestOption,
+    qExpectedUtilityOfBestOption,
+)
 from botorch.models import SingleTaskGP
 from botorch.models.map_saas import AdditiveMapSaasSingleTaskGP
 from botorch.models.model_list_gp_regression import ModelListGP
@@ -66,6 +72,7 @@ from botorch.models.transforms.outcome import Standardize
 from botorch.utils.datasets import (
     ContextualDataset,
     MultiTaskDataset,
+    RankingDataset,
     SupervisedDataset,
 )
 from pandas import DataFrame
@@ -1108,3 +1115,132 @@ class TorchAdapterTest(TestCase):
                         surrogate_spec=surrogate_spec,
                     ),
                 )
+
+    @mock_botorch_optimize
+    def test_pairwise_preference_model(self) -> None:
+        experiment = get_pbo_experiment()
+        data = experiment.lookup_data()
+
+        surrogate = Surrogate(
+            surrogate_spec=SurrogateSpec(
+                model_configs=[
+                    ModelConfig(
+                        botorch_model_class=PairwiseGP,
+                        mll_class=PairwiseLaplaceMarginalLogLikelihood,
+                        input_transform_classes=[Normalize],
+                        input_transform_options={
+                            "Normalize": {"d": len(experiment.parameters)}
+                        },
+                    )
+                ]
+            )
+        )
+
+        cases = [
+            (qLogNoisyExpectedImprovement, None, 3),
+            (qExpectedUtilityOfBestOption, None, 3),
+            (
+                AnalyticExpectedUtilityOfBestOption,
+                # Analytic Acqfs do not support pending points and sequential opt
+                {"optimizer_kwargs": {"sequential": False}},
+                2,  # analytic EUBO only supports n=2
+            ),
+        ]
+        for botorch_acqf_class, model_gen_options, n in cases:
+            pmb = TorchAdapter(
+                experiment=experiment,
+                search_space=experiment.search_space,
+                data=data,
+                generator=BoTorchGenerator(
+                    botorch_acqf_class=botorch_acqf_class,
+                    surrogate=surrogate,
+                ),
+                transforms=[],
+                optimization_config=OptimizationConfig(
+                    Objective(
+                        Metric(Keys.PAIRWISE_PREFERENCE_QUERY.value), minimize=False
+                    )
+                ),
+                fit_tracking_metrics=False,
+            )
+            # Can generate candidates correctly
+            # pyre-ignore: Incompatible parameter type [6]
+            generator_run = pmb.gen(n=n, model_gen_options=model_gen_options)
+            self.assertEqual(len(generator_run.arms), n)
+
+        observation_data = [
+            ObservationData(
+                metric_names=[Keys.PAIRWISE_PREFERENCE_QUERY.value],
+                means=np.array([0]),
+                covariance=np.array([[np.nan]]),
+            ),
+            ObservationData(
+                metric_names=[Keys.PAIRWISE_PREFERENCE_QUERY.value],
+                means=np.array([1]),
+                covariance=np.array([[np.nan]]),
+            ),
+        ]
+        observation_features = [
+            ObservationFeatures(parameters={"x1": 0.1, "x2": 0.2}, trial_index=0),
+            ObservationFeatures(parameters={"x1": 0.3, "x2": 0.4}, trial_index=0),
+        ]
+        observation_features_with_metadata = [
+            ObservationFeatures(parameters={"x1": 0.1, "x2": 0.2}, trial_index=0),
+            ObservationFeatures(
+                parameters={"x1": 0.3, "x2": 0.4},
+                trial_index=0,
+                metadata={"metadata_key": "metadata_val"},
+            ),
+        ]
+        parameter_names = list(experiment.parameters.keys())
+        outcomes = [assert_is_instance(Keys.PAIRWISE_PREFERENCE_QUERY.value, str)]
+
+        datasets, _, candidate_metadata = pmb._convert_observations(
+            observation_data=observation_data,
+            observation_features=observation_features,
+            outcomes=outcomes,
+            parameters=parameter_names,
+            search_space_digest=None,
+        )
+        self.assertTrue(len(datasets) == 1)
+        self.assertTrue(isinstance(datasets[0], RankingDataset))
+        self.assertTrue(candidate_metadata is None)
+
+        datasets, _, candidate_metadata = pmb._convert_observations(
+            observation_data=observation_data,
+            observation_features=observation_features_with_metadata,
+            outcomes=outcomes,
+            parameters=parameter_names,
+            search_space_digest=None,
+        )
+        self.assertTrue(len(datasets) == 1)
+        self.assertTrue(isinstance(datasets[0], RankingDataset))
+        self.assertTrue(candidate_metadata is not None)
+
+        # Test individual helper methods
+        X = torch.tensor(
+            [[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [1.0, 2.0, 3.0], [2.1, 3.1, 4.1]]
+        )
+        Y = torch.tensor([[1, 0, 0, 1]])
+        expected_X = torch.tensor([[1.0, 2.0, 3.0], [2.0, 3.0, 4.0], [2.1, 3.1, 4.1]])
+        ordered_Y = torch.tensor([[0, 1], [3, 2]])
+        expected_Y = torch.tensor([[0, 1], [2, 0]])
+
+        # `_binary_pref_to_comp_pair`.
+        comp_pair_Y = _binary_pref_to_comp_pair(Y=Y)
+        self.assertTrue(torch.equal(comp_pair_Y, ordered_Y))
+
+        # test `_binary_pref_to_comp_pair` with invalid data
+        bad_Y = torch.tensor([[1, 1, 0, 0]])
+        with self.assertRaises(ValueError):
+            _binary_pref_to_comp_pair(Y=bad_Y)
+
+        # `_consolidate_comparisons`.
+        consolidated_X, consolidated_Y = _consolidate_comparisons(X=X, Y=comp_pair_Y)
+        self.assertTrue(torch.equal(consolidated_X, expected_X))
+        self.assertTrue(torch.equal(consolidated_Y, expected_Y))
+
+        with self.assertRaises(ValueError):
+            _consolidate_comparisons(
+                X=X.expand(2, *X.shape), Y=comp_pair_Y.expand(2, *comp_pair_Y.shape)
+            )
