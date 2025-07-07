d9f33d2652e15f3a56b00c8ef6585b123cd8811f
Miles Olson <mpolson64@meta.com>
2025-07-07 15:45:39 -0700

    Add `display` to AnalysisBase (#3999)
    Summary: Pull Request resolved: https://github.com/facebook/Ax/pull/3999
    
    Reviewed By: eonofrey
    
    Differential Revision: D77883850
    
    fbshipit-source-id: 8a64777e1f527086e5865dae5bb308e44b2b505e

--- ax/service/utils/analysis_base.py
+++ ax/service/utils/analysis_base.py
@@ -7,9 +7,9 @@
 
 from collections.abc import Iterable
 
-from ax.analysis.analysis import Analysis
-from ax.analysis.analysis_card import AnalysisCard, AnalysisCardGroup
-from ax.analysis.plotly.parallel_coordinates import ParallelCoordinatesPlot
+from ax.analysis.analysis import Analysis, display_cards
+from ax.analysis.analysis_card import AnalysisCard
+from ax.analysis.overview import OverviewAnalysis
 from ax.core.experiment import Experiment
 from ax.generation_strategy.generation_strategy import GenerationStrategy
 from ax.service.utils.with_db_settings_base import WithDBSettingsBase
@@ -28,17 +28,10 @@ class AnalysisBase(WithDBSettingsBase):
     # is never initialized
     generation_strategy: GenerationStrategy
 
-    def _choose_analyses(self) -> list[Analysis]:
-        """
-        Choose Analyses to compute based on the Experiment, GenerationStrategy, etc.
-        """
-
-        # TODO Create a useful heuristic for choosing analyses
-        return [ParallelCoordinatesPlot()]
-
     def compute_analyses(
         self,
         analyses: Iterable[Analysis] | None = None,
+        display: bool = True,
     ) -> list[AnalysisCard]:
         """
         Compute AnalysisCards (data about the optimization for end-user consumption)
@@ -55,15 +48,18 @@ class AnalysisBase(WithDBSettingsBase):
         Args:
             analyses: A list of Analysis classes to run. If None Ax will choose which
                 analyses to run based on the state of the experiment.
+            display: Whether to display the AnalysisCards if executed in an interactive
+                environment (e.g. Jupyter). Defaults to True. If not in an interactive
+                environment this setting has no effect.
         Returns:
             A list of AnalysisCards.
         """
 
-        analyses = analyses if analyses is not None else self._choose_analyses()
+        analyses = analyses if analyses is not None else [OverviewAnalysis()]
 
         # Compute Analyses one by one and accumulate Results holding either the
         # AnalysisCard or an Exception and some metadata
-        children = [
+        cards = [
             analysis.compute_or_error_card(
                 experiment=self.experiment,
                 generation_strategy=self.generation_strategy,
@@ -71,7 +67,8 @@ class AnalysisBase(WithDBSettingsBase):
             for analysis in analyses
         ]
 
-        return AnalysisCardGroup(
-            name="",
-            children=children,
-        ).flatten()
+        # Display the AnalysisCards if requested and if the user is in a notebook
+        if display:
+            display_cards(cards=cards)
+
+        return [leaf for card in cards for leaf in card.flatten()]
