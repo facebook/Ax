448e15a305c2d020c80ceaec030028b95b3bad9c
Miles Olson <mpolson64@meta.com>
2025-07-10 07:57:31 -0700

    Remove unused utilities (#4018)
    Summary:
    
    As titled. These functions are not used anywhere and are likely leftover from InsampleEffects and PredictedEffects plots
    
    Differential Revision: D77951334

--- ax/analysis/plotly/tests/test_utils.py
+++ ax/analysis/plotly/tests/test_utils.py
@@ -6,15 +6,8 @@
 # pyre-strict
 
 import pandas as pd
-from ax.analysis.plotly.utils import (
-    get_arm_tooltip,
-    get_constraint_violated_probabilities,
-    trial_index_to_color,
-)
-from ax.core.metric import Metric
-from ax.core.outcome_constraint import ComparisonOp, OutcomeConstraint
+from ax.analysis.plotly.utils import get_arm_tooltip, trial_index_to_color
 from ax.core.trial_status import TrialStatus
-from ax.exceptions.core import UserInputError
 from ax.utils.common.testutils import TestCase
 
 
@@ -99,98 +92,4 @@ class TestUtils(TestCase):
             bar_tooltip,
             "Trial: 5<br />Arm: 5_0<br />Status: COMPLETED<br />"
             "Generation Node: MBM<br />bar: -0.22247±1.25131<br />",
-        )
-
-    def test_no_constraints_violates_none(self) -> None:
-        constraint_violated_probabilities = get_constraint_violated_probabilities(
-            # predictions for 2 observations on metrics a and b
-            predictions=[
-                (
-                    {"a": 1.0, "b": 2.0},
-                    {"a": 0.1, "b": 0.2},
-                ),
-                (
-                    {"a": 1.1, "b": 2.1},
-                    {"a": 0.1, "b": 0.2},
-                ),
-            ],
-            outcome_constraints=[],
-        )
-        self.assertEqual(
-            constraint_violated_probabilities, {"any_constraint_violated": [0.0, 0.0]}
-        )
-
-    def test_relative_constraints_are_not_accepted(self) -> None:
-        with self.assertRaisesRegex(
-            UserInputError,
-            "does not support relative outcome constraints",
-        ):
-            get_constraint_violated_probabilities(
-                predictions=[],
-                outcome_constraints=[
-                    OutcomeConstraint(
-                        metric=Metric("a"),
-                        op=ComparisonOp.GEQ,
-                        bound=0.0,
-                        relative=True,
-                    )
-                ],
-            )
-
-    def test_it_gives_a_result_per_constraint_plus_overall(self) -> None:
-        constraint_violated_probabilities = get_constraint_violated_probabilities(
-            # predictions for 2 observations on metrics a and b
-            predictions=[
-                (
-                    {"a": 1.0, "b": 2.0},
-                    {"a": 0.1, "b": 0.2},
-                ),
-                (
-                    {"a": 1.1, "b": 2.1},
-                    {"a": 0.1, "b": 0.2},
-                ),
-            ],
-            outcome_constraints=[
-                OutcomeConstraint(
-                    metric=Metric("a"),
-                    op=ComparisonOp.GEQ,
-                    bound=0.9,
-                    relative=False,
-                ),
-                OutcomeConstraint(
-                    metric=Metric("b"),
-                    op=ComparisonOp.LEQ,
-                    bound=2.2,
-                    relative=False,
-                ),
-            ],
-        )
-        self.assertEqual(
-            len(constraint_violated_probabilities.keys()),
-            3,
-        )
-        self.assertIn("any_constraint_violated", constraint_violated_probabilities)
-        self.assertIn("a", constraint_violated_probabilities)
-        self.assertIn("b", constraint_violated_probabilities)
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["any_constraint_violated"][0],
-            0.292,
-            places=2,
-        )
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["any_constraint_violated"][1],
-            0.324,
-            places=2,
-        )
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["a"][0], 0.158, places=2
-        )
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["a"][1], 0.022, places=2
-        )
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["b"][0], 0.158, places=2
-        )
-        self.assertAlmostEqual(
-            constraint_violated_probabilities["b"][1], 0.308, places=2
         )
