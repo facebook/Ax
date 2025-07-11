38c42ce99bc8629650d7ab12a53246935195e10c
Matthew Grange <mgrange@meta.com>
2025-07-11 10:40:12 -0700

    Allow for importing "save_analysis_card" to fail in non sqlalchemy environments (#4028)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/4028
    
    Nightly cron jobs are failing "Tests with latest BoTorch & minimal dependencies / tests-and-coverage (3.10)"
    https://github.com/facebook/Ax/actions/runs/16212247566/job/45774446848
    
    ```
    > Install dependencies with latest Botorch (minimal dependencies true)
    
    > Run python scripts/import_ax.py
      ...
        from ax.storage.sqa_store.save import save_analysis_card
      File "/home/runner/work/Ax/Ax/ax/storage/sqa_store/__init__.py", line 11, in <module>
        from ax.storage.sqa_store import validation
      File "/home/runner/work/Ax/Ax/ax/storage/sqa_store/validation.py", line 13, in <module>
        from ax.storage.sqa_store.db import SQABase
      File "/home/runner/work/Ax/Ax/ax/storage/sqa_store/db.py", line 16, in <module>
        from sqlalchemy import create_engine
    ModuleNotFoundError: No module named 'sqlalchemy'
    Error: Process completed with exit code 1.
    ```
    
    # Fix
    
    Move the import of "save_analysis_card" into the try block that allows for sqlalchemy to not be included.
    
    Reviewed By: saitcakmak, mpolson64
    
    Differential Revision: D78163052
    
    fbshipit-source-id: f5bdfe4f926b0c7e7b42518b06bed109cb3c1ebf

---
+++ ax/service/tests/test_with_db_settings_base_minimal_dependencies.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-strict
+
+from unittest.mock import patch
+
+from ax.utils.common.testutils import TestCase
+
+
+class TestWithDbSetingsBaseMinimalDependencies(TestCase):
+    @patch.dict("sys.modules", {"sqlalchemy": None})
+    def test_with_db_settings_base_no_sql_alchemy(self) -> None:
+        from ax.service.utils.with_db_settings_base import WithDBSettingsBase  # noqa
