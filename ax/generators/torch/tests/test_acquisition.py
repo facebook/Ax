b2a8be91c8d7bed6d7e71ae0cec919213a578032
Samuel Müller <sammuller@meta.com>
2025-08-07 02:39:44 -0700

    Increase Batch Limit Default To 20 (Again)
    Summary: After re-evaluating Ax's behavior with different default batch limits, it seems the batch limit did not detariorate performance, but instead our benchmarks were noisy.
    
    Differential Revision: D79788850

--- ax/generators/torch/tests/test_acquisition.py
+++ ax/generators/torch/tests/test_acquisition.py
@@ -340,7 +340,7 @@ class AcquisitionTest(TestCase):
             q=n,
             options={
                 "init_batch_limit": 32,
-                "batch_limit": 5,
+                "batch_limit": 20,
                 "max_optimization_problem_aggregation_size": 5,
             },
             inequality_constraints=self.inequality_constraints,
@@ -686,7 +686,7 @@ class AcquisitionTest(TestCase):
             acq_function=acquisition.acqf,
             bounds=mock.ANY,
             q=3,
-            options={"init_batch_limit": 32, "batch_limit": 5},
+            options={"init_batch_limit": 32, "batch_limit": 20},
             fixed_features_list=[{1: 0}, {1: 1}, {1: 2}],
             inequality_constraints=self.inequality_constraints,
             post_processing_func=self.rounding_func,
@@ -736,7 +736,7 @@ class AcquisitionTest(TestCase):
             q=3,
             options={
                 "init_batch_limit": 32,
-                "batch_limit": 5,
+                "batch_limit": 20,
                 "maxiter_alternating": 2,
             },
             inequality_constraints=self.inequality_constraints,
@@ -775,7 +775,7 @@ class AcquisitionTest(TestCase):
             q=3,
             options={
                 "init_batch_limit": 32,
-                "batch_limit": 5,
+                "batch_limit": 20,
                 "maxiter_alternating": 2,
             },
             inequality_constraints=self.inequality_constraints,
