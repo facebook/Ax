c2e6c014361a398b094f2ea3c1da1ef5cc13ac6b
Elizabeth Santorella <santorella@meta.com>
2025-08-12 06:52:51 -0700

    Remove unused argument `excluded_columns` from `Data.column_data_types` (#4132)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/4132
    
    It isn't used except in tests.
    
    Reviewed By: Balandat
    
    Differential Revision: D80040604
    
    fbshipit-source-id: 4de1d3e84b27c511a973a2ca30bd865aeb841bb1

--- ax/core/tests/test_data.py
+++ ax/core/tests/test_data.py
@@ -358,26 +358,3 @@ class DataTest(TestCase):
         for c, t in Data.COLUMN_DATA_TYPES.items():
             self.assertEqual(columns[c], t)
         self.assertEqual(columns["foo"], bartype)
-
-    def test_data_column_data_types_with_removed_columns(self) -> None:
-        columns = Data.column_data_types(excluded_columns=["fidelities"])
-        self.assertNotIn("fidelities", columns)
-        for c, t in Data.COLUMN_DATA_TYPES.items():
-            if c != "fidelities":
-                self.assertEqual(columns[c], t)
-
-    # there isn't really a point in doing this
-    # this test just documents expected behavior
-    # that excluded_columns wins out
-    def test_data_column_data_types_with_extra_columns_also_deleted(self) -> None:
-        bartype = random.choice([str, int, float])
-        excluded_columns = ["fidelities", "foo"]
-        columns = Data.column_data_types(
-            extra_column_types={"foo": bartype},
-            excluded_columns=excluded_columns,
-        )
-        self.assertNotIn("fidelities", columns)
-        self.assertNotIn("foo", columns)
-        for c, t in Data.COLUMN_DATA_TYPES.items():
-            if c not in excluded_columns:
-                self.assertEqual(columns[c], t)
