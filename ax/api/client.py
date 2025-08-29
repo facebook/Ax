13ed9970a7dd668feb6c22d61d40d45145c59a26
Elizabeth Santorella <santorella@meta.com>
2025-08-29 15:12:22 -0700

    Do not require map key in map evaluations (#4226)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/4226
    
    **Context**:
    * This diff is one of several that will remove the ability to customize map keys so that they are always the default "step." This is already the case with the API, and other metrics currently in use seem to use "step."
    * Data can be attached via AxClient with map evaluations. Map evaluations take the format [({map_key: map_value}, {metric_name: (mean, sem)})]. The map key is no longer needed if the map key is to always be "step."
    
    **This PR**:
    * Moves DEFAULT_MAP_KEY ("step") from MapMetric's module to MapData's module to avoid circular imports and renames it to MAP_KEY
    * Change MapEvaluations to the format `[(map_value, {metric_name: (mean, sem)})]`, removing the map key. This mostly affects internal data movement within Ax; the one potentially user-affecting changes are in AxClient, e.g. `ax_client.complete_trial(trial_index=0, raw_data=some_t_map_evaluation)`.
    * Adds validation that the map value must always be able to be cast to a float, which is what MapData expects.
    
    Differential Revision: D81335900

--- ax/api/client.py
+++ ax/api/client.py
@@ -490,7 +490,7 @@ class Client(WithDBSettingsBase):
         # If no progression is provided assume the data is not timeseries-like and
         # set step=NaN
         data_with_progression = [
-            ({"step": progression if progression is not None else np.nan}, raw_data)
+            (progression if progression is not None else np.nan, raw_data)
         ]
 
         trial = assert_is_instance(self._experiment.trials[trial_index], Trial)
