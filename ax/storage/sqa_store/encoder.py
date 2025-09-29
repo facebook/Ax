870d52adc45af24c931c8e12fd400c352af3aacd
Cesar Cardoso <cesarcardoso@meta.com>
2025-09-29 14:06:51 -0700

    Add backfill_value and default_value fields to Parameter (#4177)
    Summary:
    
    Add two properties to `Parameter`:
    1. `backfill_value` - used for parameters added to experiments that already have run trials. Specifies the backfill value to use for trials that have already run.
    2. `default_value` - used for parameters disabled in experiments that already have run trials. Specified the default value to use in modeling for future trials.
    
    Reviewed By: lena-kashtelyan
    
    Differential Revision: D79260975

--- ax/storage/sqa_store/encoder.py
+++ ax/storage/sqa_store/encoder.py
@@ -278,6 +278,8 @@ class Encoder:
                 is_fidelity=parameter.is_fidelity,
                 target_value=parameter.target_value,
                 dependents=parameter.dependents if parameter.is_hierarchical else None,
+                backfill_value=parameter.backfill_value,
+                default_value=parameter.default_value,
             )
         elif isinstance(parameter, ChoiceParameter):
             if parameter._bypass_cardinality_check:
@@ -298,6 +300,8 @@ class Encoder:
                 is_fidelity=parameter.is_fidelity,
                 target_value=parameter.target_value,
                 dependents=parameter.dependents if parameter.is_hierarchical else None,
+                backfill_value=parameter.backfill_value,
+                default_value=parameter.default_value,
             )
         elif isinstance(parameter, FixedParameter):
             # pyre-fixme[29]: `SQAParameter` is not a function.
@@ -310,6 +314,8 @@ class Encoder:
                 is_fidelity=parameter.is_fidelity,
                 target_value=parameter.target_value,
                 dependents=parameter.dependents if parameter.is_hierarchical else None,
+                backfill_value=parameter.backfill_value,
+                default_value=parameter.default_value,
             )
         elif isinstance(parameter, DerivedParameter):
             # pyre-fixme[29]: `SQAParameter` is not a function.
@@ -431,6 +437,8 @@ class Encoder:
                 digits=parameter.digits,
                 is_fidelity=parameter.is_fidelity,
                 target_value=parameter.target_value,
+                backfill_value=parameter.backfill_value,
+                default_value=parameter.default_value,
             )
         else:
             raise SQAEncodeError(
