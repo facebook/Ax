3c4870cf63ffd841fa381b843854257995e4c3cb
Mia Garrard <mgarrard@meta.com>
2025-10-01 15:15:56 -0700

    Expose method to bulk configure metrics of same class on experiment
    Summary: QOL improvement to be able to mutate all metrics of a specific class in the same way without manually mutating each metric
    
    Differential Revision: D83629776

--- ax/core/tests/test_experiment.py
+++ ax/core/tests/test_experiment.py
@@ -43,6 +43,8 @@ from ax.exceptions.core import (
     UserInputError,
 )
 from ax.metrics.branin import BraninMetric
+from ax.metrics.hartmann6 import Hartmann6Metric
+from ax.metrics.noisy_function import NoisyFunctionMetric
 from ax.runners.synthetic import SyntheticRunner
 from ax.service.ax_client import AxClient
 from ax.service.utils.instantiation import ObjectiveProperties
@@ -668,6 +670,39 @@ class ExperimentTest(TestCase):
         self.assertEqual(
             exp.fetch_data(metrics=[Metric(name="not_on_experiment")]), Data()
         )
+
+    def test_bulk_configure_metrics(self) -> None:
+        exp = get_branin_experiment_with_multi_objective()
+        exp.add_tracking_metric(
+            metric=Hartmann6Metric(name="no update", param_names=["x"])
+        )
+        # apply update to metric properties manually
+        inital_metrics = exp.metrics
+        for metric in inital_metrics.values():
+            if isinstance(metric, BraninMetric):
+                metric.param_names = ["foo", "bar"]
+
+        with self.subTest("updates both metrics"):
+            exp.bulk_configure_metrics(
+                metric_class_to_update_dict={
+                    BraninMetric: {"param_names": ["foo", "bar"]}
+                }
+            )
+            self.assertEqual(inital_metrics, exp.metrics)
+            # double check the change is populated in opt config too
+            inital_metrics.pop("no update")
+            self.assertIsNotNone(exp.optimization_config)
+            self.assertEqual(inital_metrics, exp.optimization_config.metrics)
+        with self.subTest("parameter not in initialization"):
+            with self.assertRaisesRegex(UserInputError, "does not have the following"):
+                exp.bulk_configure_metrics(
+                    metric_class_to_update_dict={BraninMetric: {"fake": 1}}
+                )
+        with self.subTest("no metrics to update"):
+            with self.assertRaisesRegex(UserInputError, "No metrics of class"):
+                exp.bulk_configure_metrics(
+                    metric_class_to_update_dict={NoisyFunctionMetric: {"fake": 1}}
+                )
 
     def test_OverwriteExistingData(self) -> None:
         n = 10
