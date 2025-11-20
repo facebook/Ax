d4fe7c330f775f9edff775c0aba4a939dd6d488d
Eric Onofrey <eonofrey@meta.com>
2025-11-20 15:15:01 -0800

    Replace `ConstraintsFeasibilityAnalysis` with `IndividualConstraintsFeasibilityAnalysis` (#4527)
    Summary:
    
    As titled, we believe `IndividualConstraintsFeasibilityAnalysis` is strictly better than `ConstraintsFeasibilityAnalysis` in all aspects but name, so we are naming the former `ConstraintsFeasibilityAnalysis` and deleting the latter
    
    Differential Revision: D86808454

--- ax/analysis/healthcheck/__init__.py
+++ ax/analysis/healthcheck/__init__.py
@@ -11,14 +11,12 @@ from ax.analysis.healthcheck.can_generate_candidates import (
 
 from ax.analysis.healthcheck.constraints_feasibility import (
     ConstraintsFeasibilityAnalysis,
+    RESTRICTIVE_P_FEAS_THRESHOLD,
 )
 from ax.analysis.healthcheck.healthcheck_analysis import (
     create_healthcheck_analysis_card,
     HealthcheckAnalysisCard,
     HealthcheckStatus,
-)
-from ax.analysis.healthcheck.individual_constraints_feasibility import (
-    IndividualConstraintsFeasibilityAnalysis,
 )
 from ax.analysis.healthcheck.regression_analysis import RegressionAnalysis
 
@@ -28,7 +26,7 @@ from ax.analysis.healthcheck.should_generate_candidates import ShouldGenerateCan
 __all__ = [
     "create_healthcheck_analysis_card",
     "ConstraintsFeasibilityAnalysis",
-    "IndividualConstraintsFeasibilityAnalysis",
+    "RESTRICTIVE_P_FEAS_THRESHOLD",
     "CanGenerateCandidatesAnalysis",
     "HealthcheckAnalysisCard",
     "HealthcheckStatus",
