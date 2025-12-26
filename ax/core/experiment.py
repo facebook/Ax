9e60db9ef1c6665c560be8f5ac73da1ea38b3fde
Elizabeth Santorella <santorella@meta.com>
2025-12-26 07:38:59 -0800

    (Temporary step) Give Experiment a property `Data` (#4686)
    Summary:
    Pull Request resolved: https://github.com/facebook/Ax/pull/4686
    
    **Context**: This diff exists to split up a more complex change. The next diff, D86452716, will remove `Experiment`'s attribute `_data_by_trial` and give it an attribute `Data`. That is quite a complex change; in addition to the changes on `Experiment`, it requires storage changes and updating many callsites. As a first step, this diff gives `Experiment` a property `data` (which will become an attribute in D86452716) and updates call sites. It should not be landed alone.
    
    **Changes**:
    * Gives `Experiment` a property `Data` that is equivalent to `.lookup_data()`
    * Updates many call sites to reference `data` rather than `_data_by_trial`
    
    Differential Revision: D87004539

--- ax/core/experiment.py
+++ ax/core/experiment.py
@@ -1061,6 +1061,11 @@ class Experiment(Base):
         data_type = MapData if has_map_data else Data
         return data_type.from_multiple_data(data_by_trial)
 
+    # Temporary. This will become an attribute in the next diff.
+    @property
+    def data(self) -> Data:
+        return self.lookup_data()
+
     @property
     def num_trials(self) -> int:
         """How many trials are associated with this experiment."""
