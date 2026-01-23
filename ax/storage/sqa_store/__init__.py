b887eae86fb3be0e4b6124b42d8d694b9038a712
Cesar Cardoso <cesarcardoso@meta.com>
2026-01-23 09:35:09 -0800

    Remove imports from ax/storage/sqa_store/__init__.py to resolve an accidentally-added required SQA dep (#4812)
    Summary: Pull Request resolved: https://github.com/facebook/Ax/pull/4812
    
    Reviewed By: saitcakmak
    
    Differential Revision: D91274573

--- ax/storage/sqa_store/__init__.py
+++ ax/storage/sqa_store/__init__.py
@@ -5,14 +5,3 @@
 # LICENSE file in the root directory of this source tree.
 
 # pyre-strict
-
-# necessary to import this file so SQLAlchemy knows about the event listeners
-# see https://fburl.com/8mn7yjt2
-from ax.storage.sqa_store import validation
-from ax.storage.sqa_store.load import load_experiment as sqa_load
-from ax.storage.sqa_store.save import save_experiment as sqa_save
-
-
-__all__ = ["sqa_load", "sqa_save"]
-
-del validation
